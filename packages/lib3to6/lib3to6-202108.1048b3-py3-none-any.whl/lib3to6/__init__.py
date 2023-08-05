53712ff4156860f07bf888378deaf2d5012ede4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:22 +0000

    Add package lib3to6/202108.1048b3/lib3to6-202108.1048b3-py3-none-any.whl

---
+++ packages/lib3to6/lib3to6-202108.1048b3-py3-none-any.whl/lib3to6/__init__.py
@@ -0,0 +1,16 @@
+# This file is part of the lib3to6 project
+# https://github.com/mbarkhau/lib3to6
+#
+# Copyright (c) 2019-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+from __future__ import generator_stop
+from .utils import parsedump_ast
+from .utils import parsedump_source
+from .packaging import Distribution
+from .packaging import fix
+from .packaging import build_py
+from .transpile import transpile_module
+__version__ = 'v202108.1048-b3'
+__all__ = ['fix', 'transpile_module', 'parsedump_ast', 'parsedump_source',
+    'build_py', 'Distribution']
