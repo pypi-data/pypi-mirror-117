be6c92bea9d092110bb31af97747c09504a936a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:00 +0000

    Add package ospyata/1.0.2/ospyata-1.0.2-py3-none-any.whl

---
+++ packages/ospyata/ospyata-1.0.2-py3-none-any.whl/ospyata/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 aerocyber
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from . import loadOMIO, manageOsmation, writeOMIO, __version__
+
+__version__ = __version__.__version__
