a69d11f8ada0a5e718eddd01e6cde3fd863726f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:30 +0000

    Add package webrcon/1.1.0/webrcon-1.1.0.tar.gz

---
+++ packages/webrcon/webrcon-1.1.0.tar.gz/webrcon-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fp:
+    long = fp.read()
+
+setup(name='webrcon',
+      author='lewdneko',
+      url='https://github.com/lewdneko/webrcon',
+      version='1.1.0',
+      packages=['webrcon'],
+      setup_requires=['wheel'],
+      python_requires='>=3.6',
+      platforms=['Windows', 'Linux', 'OSX'],
+      zip_safe=True,
+      license='BlueOak-1.0.0',
+      description='Basic async interface for Rust\'s WebRcon protocol',
+      long_description=long,
+      long_description_content_type='text/markdown',
+      keywords='rust webrcon rcon websockets ws rust_game async'
+)
