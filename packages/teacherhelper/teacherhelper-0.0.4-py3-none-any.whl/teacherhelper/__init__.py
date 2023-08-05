f946ce6ab17a3e4bffe01f7d44159401585f0215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:06 +0000

    Add package teacherhelper/0.0.4/teacherhelper-0.0.4-py3-none-any.whl

---
+++ packages/teacherhelper/teacherhelper-0.0.4-py3-none-any.whl/teacherhelper/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from .helper import Helper
+
+REQUIRED_VARS = [
+    'EMAIL_USERNAME',
+    'EMAIL_PASSWORD',
+]
+
+for var in REQUIRED_VARS:
+    if not os.getenv(var):
+        print(
+            f'Environment variable {var} is missing, which is necessary for '
+            'certain functions within this module.'
+        )
