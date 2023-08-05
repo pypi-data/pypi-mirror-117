0b8658f89addcf0bcec0adda775599a63553df1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package sgtpy/0.0.13/sgtpy-0.0.13.tar.gz

---
+++ packages/sgtpy/sgtpy-0.0.13.tar.gz/sgtpy-0.0.13/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, Extension
+# from Cython.Distutils import build_ext
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = {}
+ext_modules = []
+
+if use_cython:
+    ext_modules += [Extension('SGTPy.coloc_cy',
+                              ['SGTPy/src/coloc_cy.pyx']),
+                    Extension('SGTPy.sgt.cijmix_cy',
+                              ['SGTPy/src/cijmix_cy.pyx'])]
+    cmdclass.update({'build_ext': build_ext})
+else:
+    ext_modules += [Extension('SGTPy.coloc_cy', ['SGTPy/src/coloc_cy.c']),
+                    Extension('SGTPy.sgt.cijmix_cy',
+                    ['SGTPy/src/cijmix_cy.c'])]
+
+"""
+cmdclass = {}
+ext_modules = []
+
+
+ext_modules += [Extension('SGTPy.coloc_cy',
+                          ['SGTPy/src/coloc_cy.pyx']),
+                Extension('SGTPy.sgt.cijmix_cy',
+                          ['SGTPy/src/cijmix_cy.pyx'])]
+cmdclass.update({'build_ext': build_ext})
+"""
+
+setup(
+  name='sgtpy',
+  license='MIT',
+  version='0.0.13',
+  description='SAFT-VR-MIE EOS and SGT',
+  author='Gustavo Chaparro Maldonado, Andres Mejia Matallana, Erich A. Muller',
+  author_email='gustavochaparro@udec.cl',
+  url='https://github.com/gustavochm/SGTPy',
+  download_url='https://github.com/gustavochm/SGTPy.git',
+  long_description=open('long_description.rst').read(),
+  packages=['SGTPy', 'SGTPy.vrmie_mixtures',  'SGTPy.vrmie_pure',
+            'SGTPy.gammamie_mixtures', 'SGTPy.gammamie_pure',
+            'SGTPy.sgt', 'SGTPy.equilibrium', 'SGTPy.fit'],
+  cmdclass=cmdclass,
+  ext_modules=ext_modules,
+  package_data={'SGTPy': ['database/*']},
+  install_requires=['numpy', 'scipy', 'cython', 'numba', 'pandas', 'openpyxl'],
+  platforms=["Windows", "Linux", "Mac OS", "Unix"],
+  keywords=['SAFT-VR-Mie', 'SGT'],
+  zip_safe=False
+)
