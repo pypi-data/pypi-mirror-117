f220284a961a5e50bae09c0206474f361f0fe770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:14 +0000

    Add package Flask-GitHubApplication/1.2.0/Flask-GitHubApplication-1.2.0.tar.gz

---
+++ packages/Flask-GitHubApplication/Flask-GitHubApplication-1.2.0.tar.gz/Flask-GitHubApplication-1.2.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='Flask-GitHubApplication',
+    url='https://github.com/ffalor/flask-githubapplication',
+    project_urls={
+        "Bug Tracker": "https://github.com/ffalor/flask-githubapplicatio/issues",
+    },
+    license='GNU3',
+    author='Frank Falor',
+    author_email='ffalorjr@outlook.com',
+    description='Flask extension for creating Github Apps',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    zip_safe=False,
+    install_requires=[
+        'flask==2.0.1',
+        'ghapi==0.1.19',
+        'requests==2.22.0',
+        'pyjwt[crypto]==2.1.0'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Flask',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
