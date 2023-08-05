768077b136c69ecea286f9e4a53e8ff4a3960066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:49 +0000

    Add package pyderman/2.0.2/pyderman-2.0.2.tar.gz

---
+++ packages/pyderman/pyderman-2.0.2.tar.gz/pyderman-2.0.2/pyderman/drivers/chrome.py
@@ -0,0 +1,20 @@
+from pyderman.util import downloader
+
+_base_version = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+_base_download = 'https://chromedriver.storage.googleapis.com/%s/chromedriver_%s%s.zip'
+
+
+def get_url(version='latest', _os=None, _os_bit=None):
+	if version == 'latest':
+		version = downloader.raw(_base_version)
+	if not version:
+		raise Exception("Unable to locate latest ChromeDriver version!")
+	if _os == 'mac-sur':
+		_os = 'mac' # chromedriver_mac64_m1
+		_os_bit = _os_bit + '_m1'
+	download = _base_download % (version, _os, _os_bit)
+	return 'chromedriver', download, version
+
+
+if __name__ == "__main__":
+	print([u for u in get_url('latest', 'win', '32')])
