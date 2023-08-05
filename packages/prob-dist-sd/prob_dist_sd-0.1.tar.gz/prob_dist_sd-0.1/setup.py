fae3f430dd139e629abd6a25885cbebb10a0f778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:47 +0000

    Add package prob-dist-sd/0.1/prob_dist_sd-0.1.tar.gz

---
+++ packages/prob-dist-sd/prob_dist_sd-0.1.tar.gz/prob_dist_sd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_dist_sd',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dist_sd'],
+      author = 'Swastik Dashore',
+      author_email = 'swastik01tc@gmail.com',
+      zip_safe=False)
