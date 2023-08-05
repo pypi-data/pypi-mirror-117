4d49a907aa17ec19f48417c2148619b8d3a0b8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:56 +0000

    Add package cc-py-commons/0.2.32/cc_py_commons-0.2.32.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.2.32.tar.gz/cc_py_commons-0.2.32/cc_py_commons/transactions/map_transaction.py
@@ -0,0 +1,40 @@
+from dataclasses import fields
+from cc_py_commons.transactions.transaction import Transaction
+
+def from_load(load):
+	"""Takes a freight-hub load and maps it to a pricing load (Transaction)"""
+	trans_data = {}
+
+	for field in fields(Transaction):
+		value = getattr(load, field.name, None)
+		
+		if value:
+			trans_data[field.name] = value
+			
+	trans_data['equipment'] = load.equipment_description
+	trans_data['all_in_cost'] = load.rate
+	trans_data['multi_stop'] = load.stops
+	
+	if isinstance(load.origin, dict):
+		trans_data['origin_city'] = load.origin['city']
+		trans_data['origin_state'] = load.origin['state']
+		trans_data['origin_postcode'] = load.origin['postcode']
+	else:
+		trans_data['origin_city'] = load.origin.city
+		trans_data['origin_state'] = load.origin.state
+		trans_data['origin_postcode'] = load.origin.postcode
+
+	if isinstance(load.destination, dict):
+		trans_data['destination_city'] = load.destination['city']
+		trans_data['destination_state'] = load.destination['state']
+		trans_data['destination_postcode'] = load.destination['postcode']
+	else:
+		trans_data['destination_city'] = load.destination.city
+		trans_data['destination_state'] = load.destination.state
+		trans_data['destination_postcode'] = load.destination.postcode
+
+	del trans_data['customer_id']
+	trans_data['client_id'] = None
+	trans_data['equipment_class'] = None
+	transaction = Transaction(**trans_data)
+	return transaction
