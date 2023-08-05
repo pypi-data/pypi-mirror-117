9a59b836bfafa196c817320370ef2684b9fade53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:25 +0000

    Add package imate/0.6.1/imate-0.6.1-cp37-cp37m-win_amd64.whl

---
+++ packages/imate/imate-0.6.1-cp37-cp37m-win_amd64.whl/imate/sample_matrices/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Copyright 2021, Siavash Ameli <sameli@berkeley.edu>
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the license found in the LICENSE.txt file in the root
+# directory of this source tree.
+
+
+from .correlation_matrix import correlation_matrix
+from .band_matrix import band_matrix
+
+__all__ = ['correlation_matrix', 'band_matrix']
