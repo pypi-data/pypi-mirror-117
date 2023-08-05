ffd35cfad1f73028c78861c046196921f811934d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:29 +0000

    Add package rule34Py/1.3.38/rule34Py-1.3.38.tar.gz

---
+++ packages/rule34Py/rule34Py-1.3.38.tar.gz/rule34Py-1.3.38/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open('README.md', 'r') as rmdf:
+    long_description = rmdf.read()
+
+setup(
+    name='rule34Py',
+    version='1.3.38',
+    description='API wraper for rule34.xxx',
+    url="https://github.com/b3yc0d3/rule34Py",
+    author="b3yc0d3",
+    author_email="mc25.studio@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['rule34Py'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    install_requires = [
+        "beautifulsoup4 ~= 4.9.3",
+        "requests ~= 2.25.1"
+    ],
+    project_urls={
+        "Issue tracker": "https://github.com/b3yc0d3/rule34Py/issues"
+    }
+)
