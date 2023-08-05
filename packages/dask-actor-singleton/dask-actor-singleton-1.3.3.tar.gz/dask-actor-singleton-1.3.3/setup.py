7c25e73681c676ee8f7c6745f6d6ae692995a0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package dask-actor-singleton/1.3.3/dask-actor-singleton-1.3.3.tar.gz

---
+++ packages/dask-actor-singleton/dask-actor-singleton-1.3.3.tar.gz/dask-actor-singleton-1.3.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+        name='dask-actor-singleton',
+        version='1.3.3',
+        author='Walt Woods',
+        author_email='woodswalben@gmail.com',
+        description='Helper library to allocate and retrieve singleton actors in Dask',
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        url='https://github.com/wwoods/dask-actor-singleton',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'dask >= 2017.7.1',
+            'distributed >= 2017.7.1',
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)
+
