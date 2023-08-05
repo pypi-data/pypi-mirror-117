1d121ff8485aca5bab8423e44c82c96644676f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:34 +0000

    Add package LeProHQ/0.2.0/LeProHQ-0.2.0-py3-none-any.whl

---
+++ packages/LeProHQ/LeProHQ-0.2.0-py3-none-any.whl/LeProHQ/color.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import numba as nb
+
+NC = 3.0
+CA = NC
+CF = (NC ** 2 - 1.0) / (2.0 * NC)
+
+Kqph = 1.0 / NC
+Kgph = 1.0 / (NC ** 2 - 1.0)
+
+
+@nb.njit("f8(u1)", cache=True)
+def beta0_lf(nlf):
+    return (11.0 * CA - 2.0 * nlf) / 3.0
