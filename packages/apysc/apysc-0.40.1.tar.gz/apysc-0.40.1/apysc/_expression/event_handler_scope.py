50313e4cf1a52c05a857ecde9599046675c351b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:58 +0000

    Add package apysc/0.40.1/apysc-0.40.1.tar.gz

---
+++ packages/apysc/apysc-0.40.1.tar.gz/apysc-0.40.1/apysc/_expression/event_handler_scope.py
@@ -0,0 +1,128 @@
+"""Implementations for the event handler's expression scope
+interfaces.
+"""
+
+import os
+from typing import Any
+
+
+class HandlerScope:
+    """
+    Class for a handler scope. This is used at a with statement.
+    """
+
+    def __init__(self) -> None:
+        """
+        Class for a handler scope. This is used at a with statement.
+        """
+
+    def __enter__(self) -> None:
+        """
+        Enter and set an event handler scope setting.
+        """
+        _increment_scope_count()
+
+    def __exit__(self, *args: Any) -> None:
+        """
+        Exit and remove an event handler scope setting.
+
+        Parameters
+        ----------
+        *args : list
+            Positional arguments.
+        """
+        _decrement_scope_count()
+
+
+class TemporaryNotHandlerScope:
+    """
+    Class temporarily sets up a scope that is not a handler.
+    This is used at a with statement.
+    """
+
+    _original_scope_count: int
+
+    def __init__(self) -> None:
+        """
+        Class temporarily sets up a scope that is not a handler.
+        This is used at a with statement.
+        """
+        self._original_scope_count = get_current_event_handler_scope_count()
+
+    def __enter__(self) -> None:
+        """
+        Enter and set the scope count to zero.
+        """
+        _save_current_scope_count(count=0)
+
+    def __exit__(self, *args: Any) -> None:
+        """
+        Exit and revert the scope count.
+
+        Parameters
+        ----------
+        *args : list
+            Positional arguments.
+        """
+        _save_current_scope_count(count=self._original_scope_count)
+
+
+def _increment_scope_count() -> None:
+    """
+    Increment current scope count.
+    """
+    scope_count: int = get_current_event_handler_scope_count()
+    scope_count += 1
+    _save_current_scope_count(count=scope_count)
+
+
+def _decrement_scope_count() -> None:
+    """
+    Decrement current scope count.
+    """
+    scope_count: int = get_current_event_handler_scope_count()
+    scope_count -= 1
+    scope_count = max(scope_count, 0)
+    _save_current_scope_count(count=scope_count)
+
+
+def _save_current_scope_count(count: int) -> None:
+    """
+    Save current scope count.
+
+    Parameters
+    ----------
+    count : int
+        Scope count ot save.
+    """
+    from apysc._expression.expression_file_util import \
+        EVENT_HANDLER_SCOPE_COUNT_FILE_PATH
+    from apysc._file import file_util
+    file_util.save_plain_txt(
+        txt=str(count),
+        file_path=EVENT_HANDLER_SCOPE_COUNT_FILE_PATH)
+
+
+def get_current_event_handler_scope_count() -> int:
+    """
+    Get a current event handler's scope count.
+
+    Returns
+    -------
+    scope_count : int
+        Current event handler's scope count.
+        If normal handler's call, then 1 will be returned,
+        or call other handler in handler's function, then
+        2 or more count will be returned.
+    """
+    from apysc._expression import expression_file_util
+    from apysc._file import file_util
+    file_path: str = expression_file_util.EVENT_HANDLER_SCOPE_COUNT_FILE_PATH
+    if not os.path.isfile(file_path):
+        return 0
+    txt: str = file_util.read_txt(file_path=file_path)
+    txt = txt.strip()
+    if not txt.isdigit():
+        return 0
+    scope_count: int = int(txt)
+    return scope_count
---
+++ packages/apysc/apysc-0.40.1.tar.gz/apysc-0.40.1/apysc/_expression/indent_num.py
@@ -0,0 +1,101 @@
+"""Implementations of expression's indent number related interfaces.
+
+Mainly following interfaces are defined:
+
+- get_current_indent_num: Get a current indent number.
+- Indent: Class implementation for increment and decrement
+    indentation number. Basically use this class at with statement.
+- reset: Reset current indent number.
+"""
+
+import os
+from typing import Any
+
+
+def get_current_indent_num() -> int:
+    """
+    Get a current indent number.
+
+    Returns
+    -------
+    current_indent_num : int
+        Current indent number.
+    """
+    from apysc._file import file_util
+    file_path: str = _get_indent_num_file_path()
+    if not os.path.isfile(file_path):
+        return 0
+    indent_num_txt: str = file_util.read_txt(file_path=file_path)
+    indent_num_txt = indent_num_txt.strip()
+    if indent_num_txt == '':
+        return 0
+    current_indent_num: int = int(indent_num_txt)
+    return current_indent_num
+
+
+def _get_indent_num_file_path() -> str:
+    """
+    Get a indent number file path. This value will switch by
+    scope condition (e.g., event handler's scope or not).
+
+    Returns
+    -------
+    file_path : str
+        Indent number file path.
+    """
+    from apysc._expression import event_handler_scope
+    from apysc._expression import expression_file_util
+    event_handler_scope_count: int = \
+        event_handler_scope.get_current_event_handler_scope_count()
+    if event_handler_scope_count == 0:
+        return expression_file_util.INDENT_NUM_FILE_PATH
+    return expression_file_util.EVENT_HANDLER_INDENT_NUM_FILE_PATH
+
+
+class Indent:
+    """Class implementation for increment and decrement
+    indentation number. Basically use this class at with statement.
+    """
+
+    def __enter__(self) -> None:
+        """
+        Method to be used by with statement.
+        This method will increment indentation number.
+        """
+        from apysc._file import file_util
+        file_path: str = _get_indent_num_file_path()
+        current_indent_num: int = get_current_indent_num()
+        current_indent_num += 1
+        file_util.save_plain_txt(
+            txt=str(current_indent_num), file_path=file_path)
+
+    def __exit__(self, *args: Any) -> None:
+        """
+        Method to be used by with statement.
+        This method will decrement indentation number.
+
+        Parameters
+        ----------
+        *args : list
+            Any positional arguments.
+        """
+        from apysc._file import file_util
+        file_path: str = _get_indent_num_file_path()
+        current_indent_num: int = get_current_indent_num()
+        current_indent_num -= 1
+        if current_indent_num < 0:
+            current_indent_num = 0
+        file_util.save_plain_txt(
+            txt=str(current_indent_num), file_path=file_path)
+
+
+def reset() -> None:
+    """
+    Reset current indent number.
+    """
+    from apysc._expression import expression_file_util
+    from apysc._file import file_util
+    file_util.remove_file_if_exists(
+        file_path=expression_file_util.INDENT_NUM_FILE_PATH)
+    file_util.remove_file_if_exists(
+        file_path=expression_file_util.EVENT_HANDLER_INDENT_NUM_FILE_PATH)
