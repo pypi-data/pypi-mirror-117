be3ee5c35dd60f1c0dc784c7bca38914be0df865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package serial-info/0.7/serial_info-0.7.tar.gz

---
+++ packages/serial-info/serial_info-0.7.tar.gz/serial_info-0.7/serial_info/get.py
@@ -0,0 +1,21 @@
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+
+for i in ports:
+    print("******************************************************************************************")
+    print(f"Description: {i.description}")
+    print(f"Device: {i.device}")
+    print(f"HWID: {i.hwid}")
+    print(f"Interface: {i.interface}")
+    print(f"Manufacturer: {i.manufacturer}")
+    print(f"Name: {i.name}")
+    print(f"PID: {i.pid}")
+    print(f"Product: {i.product}")
+    print(f"Serial Number: {i.serial_number}")
+    print(f"VID: {i.vid}")
+
+if len(ports) == 0:
+    print("There are no devices connected to com ports.")
+else:
+    print("******************************************************************************************")
