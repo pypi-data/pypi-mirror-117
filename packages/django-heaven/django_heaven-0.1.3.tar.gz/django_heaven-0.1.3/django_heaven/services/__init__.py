8d948e94a93d4b4051140674c4c4851e37568a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:33 +0000

    Add package django-heaven/0.1.3/django_heaven-0.1.3.tar.gz

---
+++ packages/django-heaven/django_heaven-0.1.3.tar.gz/django_heaven-0.1.3/django_heaven/services/__init__.py
@@ -0,0 +1,10 @@
+from services.services import Service
+
+__all__ = [
+    'Service',
+    'service_function_for_write',
+    'service_function_decorator',
+    'objects_or_kwargs_decorator',
+    'service_function_default_arguments',
+]
+
---
+++ packages/django-heaven/django_heaven-0.1.3.tar.gz/django_heaven-0.1.3/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+with open(os.path.join(BASE_DIR, 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django_heaven',
+    version='0.1.3',
+    packages=['django_heaven/responses', 'django_heaven/services'],
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.2.4',
+    ],
+    license='MIT License',
+    description='django_heaven brings structure and order to your django projects',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/knucklesuganda/django-heaven/',
+    author='Andrey Ivanov',
+    author_email='python.on.papyrus@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
