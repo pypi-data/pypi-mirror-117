9724a6bbc80f981c1cd3e2830b0ae1c16b5b3dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:42 +0000

    Add package pfhedge/0.10.3/pfhedge-0.10.3-py3-none-any.whl

---
+++ packages/pfhedge/pfhedge-0.10.3-py3-none-any.whl/pfhedge/features/_base.py
@@ -0,0 +1,44 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import TypeVar
+
+from torch import Tensor
+
+T = TypeVar("T")
+
+
+class Feature(ABC):
+    """Base class for all features.
+
+    All features should subclass this class.
+    """
+
+    @abstractmethod
+    def __getitem__(self, i: int) -> Tensor:
+        """Return feature tensor.
+
+        Returned tensor should have a shape :math:`(N, 1)`, where :math:`N` is
+        the number of simulated paths.
+
+        Args:
+            i (int): The index of the time step to get the feature.
+
+        Returns:
+            torch.Tensor
+        """
+
+    def of(self: T, derivative=None, hedger=None) -> T:
+        """Set ``derivative`` and ``hedger`` to the attributes of ``self``.
+
+        Args:
+            derivative (Derivative, optional): The derivative to compute features.
+            hedger (Hedger, optional): The hedger to compute features.
+
+        Returns:
+            self
+        """
+        if not hasattr(self, "derivative") or derivative is not None:
+            self.derivative = derivative
+        if not hasattr(self, "hedger") or derivative is not None:
+            self.hedger = hedger
+        return self
---
+++ packages/pfhedge/pfhedge-0.10.3-py3-none-any.whl/pfhedge/nn/functional.py
@@ -0,0 +1,299 @@
+from math import ceil
+from typing import Optional
+from typing import Union
+
+import torch
+import torch.nn.functional as fn
+from torch import Tensor
+
+
+def european_payoff(input: Tensor, call: bool = True, strike: float = 1.0) -> Tensor:
+    """Returns the payoff of a European option.
+
+    Args:
+        input (torch.Tensor): The input tensor representing the price trajectory.
+        call (bool, default=True): Specifies whether the option is call or put.
+        strike (float, default=1.0): The strike price of the option.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    if call:
+        return fn.relu(input[..., -1] - strike)
+    else:
+        return fn.relu(strike - input[..., -1])
+
+
+def lookback_payoff(input: Tensor, call: bool = True, strike: float = 1.0) -> Tensor:
+    """Returns the payoff of a lookback option with a fixed strike.
+
+    Args:
+        input (torch.Tensor): The input tensor representing the price trajectory.
+        call (bool, default=True): Specifies whether the option is call or put.
+        strike (float, default=1.0): The strike price of the option.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    if call:
+        return fn.relu(input.max(dim=-1).values - strike)
+    else:
+        return fn.relu(strike - input.min(dim=-1).values)
+
+
+def american_binary_payoff(
+    input: Tensor, call: bool = True, strike: float = 1.0
+) -> Tensor:
+    """Returns the payoff of an American binary option.
+
+    Args:
+        input (torch.Tensor): The input tensor representing the price trajectory.
+        call (bool, default=True): Specifies whether the option is call or put.
+        strike (float, default=1.0): The strike price of the option.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    if call:
+        return (input.max(dim=-1).values >= strike).to(input)
+    else:
+        return (input.min(dim=-1).values <= strike).to(input)
+
+
+def european_binary_payoff(
+    input: Tensor, call: bool = True, strike: float = 1.0
+) -> Tensor:
+    """Returns the payoff of a European binary option.
+
+    Args:
+        input (torch.Tensor): The input tensor representing the price trajectory.
+        call (bool, default=True): Specifies whether the option is call or put.
+        strike (float, default=1.0): The strike price of the option.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    if call:
+        return (input[..., -1] >= strike).to(input)
+    else:
+        return (input[..., -1] <= strike).to(input)
+
+
+def exp_utility(input: Tensor, a: float = 1.0) -> Tensor:
+    """Applies an exponential utility function.
+
+    An exponential utility function is defined as:
+
+    .. math ::
+
+        u(x) = -\\exp(-a x) \\,.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        a (float, default=1.0): The risk aversion coefficient of the exponential
+            utility.
+
+    Returns:
+        torch.Tensor
+    """
+    return -(-a * input).exp()
+
+
+def isoelastic_utility(input: Tensor, a: float) -> Tensor:
+    """Applies an isoelastic utility function.
+
+    An isoelastic utility function is defined as:
+
+    .. math ::
+
+        u(x) = \\begin{cases}
+        x^{1 - a} & a \\neq 1 \\\\
+        \\log{x} & a = 1
+        \\end{cases} \\,.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        a (float): Relative risk aversion coefficient of the isoelastic
+            utility.
+
+    Returns:
+        torch.Tensor
+    """
+    if a == 1.0:
+        return input.log()
+    else:
+        return input ** (1.0 - a)
+
+
+def topp(input: Tensor, p: float, dim: Optional[int] = None, largest: bool = True):
+    """Returns the largest :math:`p * N` elements of the given input tensor,
+    where :math:`N` stands for the total number of elements in the input tensor.
+
+    If ``dim`` is not given, the last dimension of the ``input`` is chosen.
+
+    If ``largest`` is ``False`` then the smallest elements are returned.
+
+    A namedtuple of ``(values, indices)`` is returned, where the ``indices``
+    are the indices of the elements in the original ``input`` tensor.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        p (float): Quantile level.
+        dim (int, optional): The dimension to sort along.
+        largest (bool, default=True): Controls whether to return largest or smallest
+            elements.
+
+    Returns:
+        torch.Tensor
+
+    Examples:
+
+        >>> from pfhedge.nn.functional import topp
+        >>>
+        >>> input = torch.arange(1.0, 6.0)
+        >>> input
+        tensor([1., 2., 3., 4., 5.])
+        >>> topp(input, 3 / 5)
+        torch.return_types.topk(
+        values=tensor([5., 4., 3.]),
+        indices=tensor([4, 3, 2]))
+    """
+    if dim is None:
+        return input.topk(ceil(p * input.numel()), largest=largest)
+    else:
+        return input.topk(ceil(p * input.size()[dim]), dim=dim, largest=largest)
+
+
+def expected_shortfall(input: Tensor, p: float, dim: Optional[int] = None) -> Tensor:
+    """Returns the expected shortfall of the given input tensor.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        p (float): Quantile level.
+        dim (int, optional): The dimension to sort along.
+
+    Examples:
+
+        >>> from pfhedge.nn.functional import expected_shortfall
+        >>>
+        >>> input = -torch.arange(1., 6.)
+        >>> expected_shortfall(input, 3 / 5)
+        tensor(4.)
+
+    Returns:
+        torch.Tensor
+    """
+    if dim is None:
+        return -topp(input, p=p, largest=False).values.mean()
+    else:
+        return -topp(input, p=p, largest=False, dim=dim).values.mean(dim=dim)
+
+
+def leaky_clamp(
+    input: Tensor,
+    min: Optional[Tensor] = None,
+    max: Optional[Tensor] = None,
+    clamped_slope: float = 0.01,
+) -> Tensor:
+    """Leakily clamp all elements in ``input`` into the range :math:`[\\min, \\max]`.
+
+    The bounds :math:`\\min` and :math:`\\max` can be tensors.
+
+    See :class:`pfhedge.nn.LeakyClamp` for details.
+    """
+    x = input
+
+    if min is not None:
+        min = torch.as_tensor(min)
+        x = x.maximum(min + clamped_slope * (x - min))
+
+    if max is not None:
+        max = torch.as_tensor(max)
+        x = x.minimum(max + clamped_slope * (x - max))
+
+    if min is not None and max is not None:
+        x = x.where(min <= max, (min + max) / 2)
+
+    return x
+
+
+def clamp(
+    input: Tensor, min: Optional[Tensor] = None, max: Optional[Tensor] = None
+) -> Tensor:
+    """Clamp all elements in ``input`` into the range :math:`[\\min, \\max]`.
+
+    The bounds :math:`\\min` and :math:`\\max` can be tensors.
+
+    See :class:`pfhedge.nn.Clamp` for details.
+    """
+    return leaky_clamp(input, min=min, max=max, clamped_slope=0.0)
+
+
+def realized_variance(input: Tensor, dt: Union[Tensor, float]) -> Tensor:
+    """Returns the realized variance of the price.
+
+    Realized variance :math:`\\sigma^2` of the stock price :math:`S` is defined as:
+
+    .. math ::
+
+        \\sigma^2 = \\frac{1}{T - 1} \\sum_{i = 1}^{T - 1}
+        \\frac{1}{dt} \\log(S_{i + 1} / S_i)^2
+
+    where :math:`T` is the number of time steps.
+
+    .. note ::
+
+        The mean of log return is assumed to be zero.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        dt (torch.Tensor or float): The intervals of the time steps.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    return input.log().diff(dim=-1).square().mean(dim=-1) / dt
+
+
+def realized_volatility(input: Tensor, dt: Union[Tensor, float]) -> Tensor:
+    """Returns the realized volatility of the price.
+    It is square root of :func:`realized_variance`.
+
+    Args:
+        input (torch.Tensor): The input tensor.
+        dt (torch.Tensor or float): The intervals of the time steps.
+
+    Shape:
+        - input: :math:`(*, T)` where :math:`T` stands for the number of time steps
+          and :math:`*` means any number of additional dimensions.
+        - output: :math:`(*)`
+
+    Returns:
+        torch.Tensor
+    """
+    return realized_variance(input, dt=dt).sqrt()
