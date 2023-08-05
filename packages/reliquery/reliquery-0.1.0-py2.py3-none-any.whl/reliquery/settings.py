4ba741fe186d2e7a6167e5891e6cf9e4db266e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package reliquery/0.1.0/reliquery-0.1.0-py2.py3-none-any.whl

---
+++ packages/reliquery/reliquery-0.1.0-py2.py3-none-any.whl/reliquery/settings.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+def get_config(reliquery_dir):
+    # First we check the environment for the config
+    if "RELIQUERY_CONFIG" in os.environ:
+        return json.loads(os.environ["RELIQUERY_CONFIG"])
+
+    # Otherwise, fallback on a file.
+    config_path = os.path.join(reliquery_dir, "config")
+    
+    if not os.path.exists(config_path):
+        os.makedirs(reliquery_dir)
+        config_path = os.path.join(reliquery_dir, "config")
+        with open(config_path, mode="w+") as config_file:
+            config = {"storage": {"type": "File"}}
+            config_file.write(json.dumps(config, indent=4))
+
+    with open(config_path, mode="r") as config_file:
+        config = json.load(config_file)
+
+    return config
