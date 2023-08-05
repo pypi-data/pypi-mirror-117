588201255a226979cc0d664fad270434360ab069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:05 +0000

    Add package groclimateclient/0.0.0.dev1/groclimateclient-0.0.0.dev1.tar.gz

---
+++ packages/groclimateclient/groclimateclient-0.0.0.dev1.tar.gz/groclimateclient-0.0.0.dev1/groclimateclient/__init__.py
@@ -0,0 +1,4 @@
+from groclimateclient.client import GroClimateClient
+
+# Do a runtime lookup to get the groclient package version info.
+# __version__ = lib.get_version_info().get('api-client-version', 'unknown')
