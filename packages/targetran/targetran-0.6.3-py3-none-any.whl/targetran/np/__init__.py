cc865e065ef0cf87e2150a91b4b3fc74432fcdce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:47 +0000

    Add package targetran/0.6.3/targetran-0.6.3-py3-none-any.whl

---
+++ packages/targetran/targetran-0.6.3-py3-none-any.whl/targetran/np/__init__.py
@@ -0,0 +1,17 @@
+from ._np import (
+    flip_left_right,
+    flip_up_down,
+    rotate,
+    shear,
+    translate,
+    crop,
+    CombineAffine,
+    RandomTransform,
+    RandomFlipLeftRight,
+    RandomFlipUpDown,
+    RandomRotate,
+    RandomShear,
+    RandomTranslate,
+    RandomCrop,
+    Resize
+)
