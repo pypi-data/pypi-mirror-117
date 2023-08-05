fc6b817ca22e3000684f58bcee45270dfc3fc05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package puck-me/0.1.0/puck-me-0.1.0.tar.gz

---
+++ packages/puck-me/puck-me-0.1.0.tar.gz/puck-me-0.1.0/puck_me/skater/temp_tests.py
@@ -0,0 +1,28 @@
+from src.skater.skaters import Skaters
+
+year = "2009"
+
+nhl_players = Skaters.all_skaters("2009")
+
+print(f"Found {len(nhl_players)} NHL players.")
+
+for player in nhl_players[:1]:
+    print(f"Player name: {player.name}.")
+    print(player.games_played())
+    print(player.goals())
+    print(player.assists())
+    print(player.points())
+    print(player.plus_minus())
+    print(player.penalty_mins())
+    print(player.goals_even_strength())
+    print(player.goals_power_play())
+    print(player.goals_short_handed())
+    print(player.goals_game_winning())
+    print(player.shots())
+    print(player.shooting_percentage())
+    print(player.shifts())
+    print(player.time_on_ice_per_game())
+    print(player.time_on_ice_total())
+    pass
+
+print(len(nhl_players))
