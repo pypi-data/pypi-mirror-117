1561f7f1041bbc61b5ba8010744aefcb9f49f507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:58 +0000

    Add package quickshare/0.5/quickshare-0.5.tar.gz

---
+++ packages/quickshare/quickshare-0.5.tar.gz/quickshare-0.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+#with open('./quickshare/requirements.txt') as f:
+#    requirements = f.read().splitlines()
+
+setup(
+    name='quickshare',
+    version='0.5',
+    packages=['quickshare'],
+    #install_requires=requirements,
+    install_requires=[
+        'ipywidgets',
+        'ipython'
+    ]
+)
