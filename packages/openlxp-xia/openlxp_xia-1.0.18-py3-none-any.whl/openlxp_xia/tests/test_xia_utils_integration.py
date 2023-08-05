54f7556db1042f36652f2f11bee364600c37e62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:31 +0000

    Add package openlxp-xia/1.0.18/openlxp_xia-1.0.18-py3-none-any.whl

---
+++ packages/openlxp-xia/openlxp_xia-1.0.18-py3-none-any.whl/openlxp_xia/tests/test_xia_utils_integration.py
@@ -0,0 +1,26 @@
+
+import logging
+
+from ddt import ddt
+from django.test import tag
+
+from openlxp_xia.management.commands.transform_source_metadata import \
+    get_target_metadata_for_transformation
+from openlxp_xia.models import XIAConfiguration
+
+from .test_setup import TestSetUp
+
+logger = logging.getLogger('dict_config_logger')
+
+
+@tag('integration')
+@ddt
+class CommandIntegration(TestSetUp):
+    # globally accessible data sets
+    def test_get_target_metadata_for_transformation(self):
+        """Test that get target mapping_dictionary from XIAConfiguration """
+        xiaConfig = XIAConfiguration(
+            target_metadata_schema='AGENT_p2881_target_metadata_schema.json')
+        xiaConfig.save()
+        source_target_mapping = get_target_metadata_for_transformation()
+        self.assertTrue(source_target_mapping)
