7a8d9781b043e7f29f82252225157e520cc1f983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:07 +0000

    Add package d4-py/0.0.0.10a4/d4-py-0.0.0.10a4.tar.gz

---
+++ packages/d4-py/d4-py-0.0.0.10a4.tar.gz/d4-py-0.0.0.10a4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="d4-py",
+    version="0.0.0.10a4",
+    author="ast.08",
+    author_email="<ast.08e@gmail.com>",
+    description="d4.py pre-alpha v0.0.0.10a",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/namuKR/d4.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests', 'asyncio', 'aiohttp']
+)
