6d39b263f0dc22a8ca00b1e2d983ade3aede8bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:32 +0000

    Add package proai/0.0.2a0/proai-0.0.2a0.tar.gz

---
+++ packages/proai/proai-0.0.2a0.tar.gz/proai-0.0.2a0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    url='https://gitlab.com/prosocialai/proai',
+    author_email='hobson@proai.org',
+    name='proai',
+    packages=find_packages(),
+    version='0.0.2a',
+    description='Tools and AI for building prosocial AI algorithms.',
+    author='Hobson Lane (ProAI.org)',
+    license='Hippocratic License (MIT + Do No Harm)',
+)
