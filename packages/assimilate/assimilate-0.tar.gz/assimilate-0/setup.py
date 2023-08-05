6f00eda600e7afa84454543118590ca4741f7518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package assimilate/0/assimilate-0.tar.gz

---
+++ packages/assimilate/assimilate-0.tar.gz/assimilate-0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name = "assimilate",
+    version = "0",
+    long_description = 'In development',
+    long_description_content_type = 'text/x-rst',
+    python_requires = ">=3.6",
+    zip_safe = True,
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Utilities",
+    ],
+)
