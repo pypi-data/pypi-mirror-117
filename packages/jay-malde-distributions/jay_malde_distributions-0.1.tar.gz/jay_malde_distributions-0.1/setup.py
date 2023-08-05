975aa8f129c5e49f00489c80c1a370df934e02b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:59 +0000

    Add package jay-malde-distributions/0.1/jay_malde_distributions-0.1.tar.gz

---
+++ packages/jay-malde-distributions/jay_malde_distributions-0.1.tar.gz/jay_malde_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jay_malde_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jay_malde_distributions'],
+      author='Jay Malde',
+      author_email='maldejay77@gmail.com',
+      zip_safe=False)
