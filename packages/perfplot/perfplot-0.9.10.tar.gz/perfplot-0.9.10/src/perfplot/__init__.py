8511a8e7314f3dab84f1a4d2fa5ad2e4cb5bc712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:20 +0000

    Add package perfplot/0.9.10/perfplot-0.9.10.tar.gz

---
+++ packages/perfplot/perfplot-0.9.10.tar.gz/perfplot-0.9.10/src/perfplot/__init__.py
@@ -0,0 +1,11 @@
+from .__about__ import __version__
+from ._main import bench, live, plot, save, show
+
+__all__ = [
+    "__version__",
+    "bench",
+    "plot",
+    "show",
+    "save",
+    "live",
+]
