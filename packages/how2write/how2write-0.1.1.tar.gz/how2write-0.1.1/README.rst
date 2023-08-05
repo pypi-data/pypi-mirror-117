299f010622d0b9d36593b9c5a5fc559554f81b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:13 +0000

    Add package how2write/0.1.1/how2write-0.1.1.tar.gz

---
+++ packages/how2write/how2write-0.1.1.tar.gz/how2write-0.1.1/README.rst
@@ -0,0 +1,32 @@
+how2write
+=========
+
+.. image:: https://img.shields.io/pypi/v/how2write.svg
+    :target: https://pypi.python.org/pypi/how2write
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/kragniz/cookiecutter-pypackage-minimal.png
+   :target: https://travis-ci.org/kragniz/cookiecutter-pypackage-minimal
+   :alt: Latest Travis CI build status
+
+An english-learning helper
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`how2write` was written by `Christophe Prat <christopheprat@icloud.com>`_.
---
+++ packages/how2write/how2write-0.1.1.tar.gz/how2write-0.1.1/setup.py
@@ -0,0 +1,45 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="how2write",
+    version="0.1.1",
+    url="https://github.com/ChristophePRAT/How2Write",
+    license='MIT',
+
+    author="Christophe Prat",
+    author_email="christopheprat@icloud.com",
+
+    description="An english-learning helper",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    include_package_data=True,
+    install_requires=["requests", "gTTS", "pygame"],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        "console_scripts": [
+            "how2write=how2write.main:main"
+        ]
+    }
+)
