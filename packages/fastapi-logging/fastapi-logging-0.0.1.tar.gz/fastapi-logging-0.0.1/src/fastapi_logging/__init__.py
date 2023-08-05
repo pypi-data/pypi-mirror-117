e02436af2bcb2bedfb83a5dcbb7f788cb2006f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package fastapi-logging/0.0.1/fastapi-logging-0.0.1.tar.gz

---
+++ packages/fastapi-logging/fastapi-logging-0.0.1.tar.gz/fastapi-logging-0.0.1/src/fastapi_logging/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Santo Digital BigQuery."""
+
+from __future__ import absolute_import
+from fastapi_logging import logging
+
+
+
+__author__ = """Lucas Magalhaes"""
+__email__ = 'lucas.castro@santodigital.com.br'
+__version__ = '1.0.0'
