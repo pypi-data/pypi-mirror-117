3bbb05cbb6e39034a6fca293ed129d10147acdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:49 +0000

    Add package LightSim2Grid/0.5.4/LightSim2Grid-0.5.4-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/LightSim2Grid/LightSim2Grid-0.5.4-cp37-cp37m-macosx_10_14_x86_64.whl/lightsim2grid/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.5.4"
+
+__all__ = ["newtonpf", "SolverType", "solver"]
+
+# import directly from c++ module
+from lightsim2grid_cpp import SolverType
+import lightsim2grid.solver
+
+try:
+    from lightsim2grid.LightSimBackend import LightSimBackend
+    __all__.append("LightSimBackend")
+except ImportError:
+    # grid2op is not installed, the Backend will not be available
+    pass
+
+try:
+    from lightsim2grid.physical_law_checker import PhysicalLawChecker
+    __all__.append("PhysicalLawChecker")
+except ImportError as exc_:
+    # grid2op is not installed, the Backend will not be available
+    pass
+    print(exc_)
