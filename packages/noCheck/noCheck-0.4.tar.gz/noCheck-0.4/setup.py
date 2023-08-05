9387e0a88cc539f4b98def30ef55677ce84f58e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:06 +0000

    Add package noCheck/0.4/noCheck-0.4.tar.gz

---
+++ packages/noCheck/noCheck-0.4.tar.gz/noCheck-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='noCheck',
+      version='0.4',
+      description="find highest of two numbers",
+      author='Zubair Mushtaq',
+      author_email='zubair.mushtaq000@gmail.com',
+      packages=['noCheck'],
+      zip_safe=False)
