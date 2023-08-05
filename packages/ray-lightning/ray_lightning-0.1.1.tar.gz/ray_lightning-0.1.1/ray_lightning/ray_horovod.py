65097ba540a8a822a31a126c10027db0c810651f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:40 +0000

    Add package ray-lightning/0.1.1/ray_lightning-0.1.1.tar.gz

---
+++ packages/ray-lightning/ray_lightning-0.1.1.tar.gz/ray_lightning-0.1.1/ray_lightning/ray_horovod.py
@@ -0,0 +1,214 @@
+import torch
+from pytorch_lightning import LightningModule
+from pytorch_lightning.plugins import HorovodPlugin
+from pytorch_lightning.utilities import rank_zero_only
+
+import ray
+from ray import ObjectRef
+from ray.util.queue import Queue
+
+from ray_lightning.session import init_session
+from ray_lightning.util import process_results, Unavailable
+from ray_lightning.tune import TUNE_INSTALLED, is_session_enabled
+
+try:
+    import horovod.torch as hvd
+    from horovod.ray import RayExecutor
+except (ModuleNotFoundError, ImportError):
+    HOROVOD_AVAILABLE = False
+    RayExecutor = Unavailable
+    hvd = Unavailable
+else:
+    HOROVOD_AVAILABLE = True
+
+
+def get_executable_cls():
+    # Only used for testing purposes, currently.
+    # We need to override this in tests to ensure test path is set correctly.
+    return None
+
+
+class HorovodRayPlugin(HorovodPlugin):
+    """Pytorch Lightning Plugin for Horovod training on a Ray cluster.
+
+    This plugin is used to manage distributed training on a Ray cluster
+    via the Horovod training framework. Internally, the specified number of
+    Ray actors are launched in the cluster and are configured as part of the
+    Horovod ring. The Pytorch Lightning trainer is instantiated on the
+    driver and sent to each of these training workers where training is
+    executed. The distributed training protocol is handled by Horovod.
+
+    Each training worker is configured to reserve 1 CPU and if 1 GPU if
+    ``use_gpu`` is set to ``True``.
+
+    If using this plugin, you should run your code like a normal Python
+    script: ``python train.py``, and not with ``horovodrun``.
+
+    Args:
+        num_hosts (int): The number of nodes/machines to execute the job on.
+        num_slots (int): Number of workers to be placed on each machine.
+        use_gpu (bool): Whether to use GPU for allocation. For GPU to be
+            used, you must also set the ``gpus`` arg in your Pytorch Lightning
+            Trainer to a value > 0.
+
+    Example:
+
+        .. code-block:: python
+
+            import pytorch_lightning as ptl
+            from ray_lightning import HorovodRayPlugin
+
+            ptl_model = MNISTClassifier(...)
+            # 2 nodes, 4 workers per node, each using 1 CPU and 1 GPU.
+            plugin = HorovodRayPlugin(num_hosts=2, num_slots=4,
+                use_gpu=True)
+
+            # If using GPUs, set the ``gpus`` arg to a value > 0.
+            # The actual number of GPUs is determined by ``num_slots``.
+            trainer = pl.Trainer(..., gpus=1, plugins=[plugin])
+            trainer.fit(ptl_model)
+
+    """
+
+    def __init__(self,
+                 num_hosts: int = 1,
+                 num_slots: int = 1,
+                 use_gpu: bool = False):
+
+        if not HOROVOD_AVAILABLE:
+            raise RuntimeError("Please intall Horovod to use this plugin.")
+        if not ray.is_initialized():
+            ray.init()
+        super().__init__()
+        self.nickname = "horovod_ray"
+        self.num_hosts = num_hosts
+        self.num_slots = num_slots
+        self.use_gpu = use_gpu
+        self.executor = None
+
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        del d["executor"]
+        return d
+
+    def __setstate__(self, d):
+        d["executor"] = None
+        self.__dict__.update(d)
+
+    @property
+    def global_rank(self) -> int:
+        if not hvd.is_initialized():
+            return 0
+        return hvd.rank()
+
+    @property
+    def local_rank(self) -> int:
+        if not hvd.is_initialized():
+            return 0
+        return hvd.local_rank()
+
+    @property
+    def world_size(self) -> int:
+        if not hvd.is_initialized():
+            return self.num_hosts * self.num_slots
+        return hvd.size()
+
+    def setup(self, model: LightningModule):
+        """Creates the RayExecutor object."""
+        self._model = model
+        settings = RayExecutor.create_settings(timeout_s=30)
+        self.executor = RayExecutor(
+            settings,
+            num_hosts=self.num_hosts,
+            num_slots=self.num_slots,
+            use_gpu=self.use_gpu)
+        self.executor.start(executable_cls=get_executable_cls())
+
+    def pre_dispatch(self):
+        """All pre-dispatch logic should be done in train_remote instead."""
+        pass
+
+    def start_training(self, trainer):
+        """Main training loop.
+
+        Trigger remote training via ``train_remote`` on each
+        worker. If using with Ray Tune, create a communication queue to
+        retrieve intermediate results, and process those results. Finally
+        retrieve the training results from the rank 0 worker and return."""
+        model = self._model
+        model_ref = ray.put(model)
+        # Don't pickle the model when training remotely.
+        self._model = None
+
+        queue = None
+        if TUNE_INSTALLED and is_session_enabled():
+            # Create communication queue and send to all the workers.
+            queue = Queue(actor_options={"num_cpus": 0})
+
+        result_futures = self.executor.run_remote(
+            self.train_remote, args=[model_ref, queue])
+
+        results = process_results(result_futures, queue)
+
+        results, state_dict, best_path = results[0]
+        self._results = results
+        self._model = model
+        self._model.load_state_dict(state_dict)
+        self._model.trainer.accelerator.training_type_plugin = self
+        if self.lightning_module.trainer.checkpoint_callback:
+            self.lightning_module.trainer.checkpoint_callback \
+                .best_model_path = best_path
+
+        if queue:
+            # Shutdown the queue.
+            queue.shutdown()
+
+        return results
+
+    def train_remote(self, model: ObjectRef, queue: Queue = None, **kwargs):
+        """Training function to be executed on each remote worker."""
+        self._model = ray.get(model)
+        self.lightning_module.trainer.accelerator_connector\
+            ._training_type_plugin = self
+        self.lightning_module.trainer.accelerator.training_type_plugin = self
+
+        hvd.init()
+        rank_zero_only.rank = self.global_rank
+
+        if queue is not None:
+            # Initialize session.
+            init_session(rank=self.global_rank, queue=queue)
+
+        # Move the model to the appropriate device.
+        super(HorovodRayPlugin, self).model_to_device()
+
+        # TODO: Make changes in PTL to clean this up.
+        super(HorovodRayPlugin, self).pre_dispatch()
+        results = super(HorovodRayPlugin,
+                        self).start_training(self.lightning_module.trainer)
+        if self.global_rank != 0:
+            # Only want results from the first worker.
+            return None
+
+        best_model_path = None
+        if self.lightning_module.trainer.checkpoint_callback is not None:
+            best_model_path = \
+                self.lightning_module.trainer.checkpoint_callback\
+                    .best_model_path
+
+        return results, self.lightning_module.state_dict(), best_model_path
+
+    def post_dispatch(self):
+        """Shuts down the RayExecutor."""
+        self.executor.shutdown()
+
+    @property
+    def is_distributed(self):
+        return True
+
+    @property
+    def root_device(self):
+        if self.use_gpu and torch.cuda.is_available():
+            return torch.device("cuda", hvd.local_rank())
+        else:
+            return torch.device("cpu")
---
+++ packages/ray-lightning/ray_lightning-0.1.1.tar.gz/ray_lightning-0.1.1/ray_lightning/tune.py
@@ -0,0 +1,224 @@
+from typing import Dict, List, Union
+
+import fsspec
+import os
+
+from pytorch_lightning import Trainer, LightningModule
+
+from ray_lightning.session import put_queue, get_actor_rank
+from ray_lightning.util import to_state_stream, Unavailable
+
+try:
+    from ray import tune
+    from ray.tune.integration.pytorch_lightning import TuneCallback
+    from ray.tune import is_session_enabled
+    TUNE_INSTALLED = True
+except ImportError:
+    tune = None
+    TuneCallback = Unavailable
+
+    def is_session_enabled():
+        return False
+
+    get_tune_ddp_resources = Unavailable
+
+    TUNE_INSTALLED = False
+
+if TUNE_INSTALLED:
+
+    def get_tune_ddp_resources(num_workers: int = 1,
+                               cpus_per_worker: int = 1,
+                               use_gpu: bool = False) -> Dict[str, int]:
+        """Returns the PlacementGroupFactory to use for Ray Tune."""
+        from ray.tune import PlacementGroupFactory
+
+        head_bundle = {"CPU": 1}
+        child_bundle = {"CPU": cpus_per_worker, "GPU": int(use_gpu)}
+        child_bundles = [child_bundle.copy() for _ in range(num_workers)]
+        bundles = [head_bundle] + child_bundles
+        placement_group_factory = PlacementGroupFactory(
+            bundles, strategy="PACK")
+        return placement_group_factory
+
+    class TuneReportCallback(TuneCallback):
+        """Distributed PyTorch Lightning to Ray Tune reporting callback
+
+            Reports metrics to Ray Tune, specifically when training is done
+            remotely with Ray via the accelerators in this library.
+
+            Args:
+                metrics (str|list|dict): Metrics to report to Tune.
+                    If this is a list, each item describes the metric key
+                    reported to PyTorch Lightning, and it will reported
+                    under the same name to Tune. If this is a dict, each key
+                    will be the name reported to Tune and the respective
+                    value will be the metric key reported to PyTorch Lightning.
+                on (str|list): When to trigger checkpoint creations.
+                    Must be one of the PyTorch Lightning event hooks (less
+                    the ``on_``), e.g. "batch_start", or "train_end".
+                    Defaults to "validation_end".
+
+            Example:
+
+            .. code-block:: python
+
+                import pytorch_lightning as pl
+                from ray_lightning import RayPlugin
+                from ray_lightning.tune import TuneReportCallback
+
+                # Create plugin.
+                ray_plugin = RayPlugin(num_workers=4, use_gpu=True)
+
+                # Report loss and accuracy to Tune after each validation epoch:
+                trainer = pl.Trainer(plugins=[ray_plugin], callbacks=[
+                    TuneReportCallback(["val_loss", "val_acc"],
+                        on="validation_end")])
+
+                # Same as above, but report as `loss` and `mean_accuracy`:
+                trainer = pl.Trainer(plugins=[ray_plugin], callbacks=[
+                    TuneReportCallback(
+                        {"loss": "val_loss", "mean_accuracy": "val_acc"},
+                        on="validation_end")])
+
+            """
+
+        def __init__(
+                self,
+                metrics: Union[None, str, List[str], Dict[str, str]] = None,
+                on: Union[str, List[str]] = "validation_end"):
+            super(TuneReportCallback, self).__init__(on)
+            if isinstance(metrics, str):
+                metrics = [metrics]
+            self._metrics = metrics
+
+        def _get_report_dict(self, trainer: Trainer,
+                             pl_module: LightningModule):
+            # Don't report if just doing initial validation sanity checks.
+            if trainer.running_sanity_check:
+                return
+            if not self._metrics:
+                report_dict = {
+                    k: v.item()
+                    for k, v in trainer.callback_metrics.items()
+                }
+            else:
+                report_dict = {}
+                for key in self._metrics:
+                    if isinstance(self._metrics, dict):
+                        metric = self._metrics[key]
+                    else:
+                        metric = key
+                    report_dict[key] = trainer.callback_metrics[metric].item()
+            return report_dict
+
+        def _handle(self, trainer: Trainer, pl_module: LightningModule):
+            if get_actor_rank() == 0:
+                report_dict = self._get_report_dict(trainer, pl_module)
+                if report_dict is not None:
+                    put_queue(lambda: tune.report(**report_dict))
+
+    class _TuneCheckpointCallback(TuneCallback):
+        """Distributed PyTorch Lightning to Ray Tune checkpoint callback
+
+            Saves checkpoints after each validation step. To be used
+            specifically with the plugins in this library.
+
+            Checkpoint are currently not registered if no ``tune.report()``
+            call is made afterwards. Consider using
+            ``TuneReportCheckpointCallback`` instead.
+
+            Args:
+                filename (str): Filename of the checkpoint within the
+                    checkpoint directory. Defaults to "checkpoint".
+                on (str|list): When to trigger checkpoint creations.
+                    Must be one of the PyTorch Lightning event hooks (less
+                    the ``on_``), e.g. "batch_start", or "train_end".
+                    Defaults to "validation_end".
+        """
+
+        def __init__(self,
+                     filename: str = "checkpoint",
+                     on: Union[str, List[str]] = "validation_end"):
+            super(_TuneCheckpointCallback, self).__init__(on)
+            self._filename = filename
+
+        @staticmethod
+        def _create_checkpoint(checkpoint_stream, global_step: int,
+                               filename: str):
+            with tune.checkpoint_dir(step=global_step) as checkpoint_dir:
+                file_path = os.path.join(checkpoint_dir, filename)
+                with fsspec.open(file_path, "wb") as f:
+                    f.write(checkpoint_stream)
+
+        def _handle(self, trainer: Trainer, pl_module: LightningModule):
+            if trainer.running_sanity_check:
+                return
+            checkpoint_dict = trainer.checkpoint_connector.dump_checkpoint()
+            # Convert to a state stream first.
+            checkpoint_stream = to_state_stream(checkpoint_dict)
+            global_step = trainer.global_step
+            if get_actor_rank() == 0:
+                put_queue(lambda: self._create_checkpoint(
+                    checkpoint_stream, global_step, self._filename))
+
+    class TuneReportCheckpointCallback(TuneCallback):
+        """PyTorch Lightning to Tune reporting and checkpointing callback.
+
+            Saves checkpoints after each validation step. Also reports metrics
+            to Tune, which is needed for checkpoint registration. To be used
+            specifically with the plugins in this library.
+
+            Args:
+                metrics (str|list|dict): Metrics to report to Tune.
+                    If this is a list, each item describes the metric key
+                    reported to PyTorch Lightning, and it will reported
+                    under the same name to Tune. If this is a dict, each key
+                    will be the name reported to Tune and the respective
+                    value will be the metric key reported to PyTorch Lightning.
+                filename (str): Filename of the checkpoint within the
+                    checkpoint directory. Defaults to "checkpoint".
+                on (str|list): When to trigger checkpoint creations. Must be
+                    one of the PyTorch Lightning event hooks (less the
+                    ``on_``), e.g. "batch_start", or "train_end". Defaults
+                    to "validation_end".
+
+
+            Example:
+
+            .. code-block:: python
+
+                import pytorch_lightning as pl
+                from ray_lightning import RayPlugin
+                from ray_lightning.tune import TuneReportCheckpointCallback.
+
+                # Create the Ray plugin.
+                ray_plugin = RayPlugin()
+
+                # Save checkpoint after each training batch and after each
+                # validation epoch.
+                trainer = pl.Trainer(plugins=[ray_plugin], callbacks=[
+                    TuneReportCheckpointCallback(
+                        metrics={"loss": "val_loss",
+                                "mean_accuracy": "val_acc"},
+                        filename="trainer.ckpt", on="validation_end")])
+
+
+            """
+
+        def __init__(
+                self,
+                metrics: Union[None, str, List[str], Dict[str, str]] = None,
+                filename: str = "checkpoint",
+                on: Union[str, List[str]] = "validation_end"):
+            super(TuneReportCheckpointCallback, self).__init__(on)
+            self._checkpoint = _TuneCheckpointCallback(filename, on)
+            self._report = TuneReportCallback(metrics, on)
+
+        def _handle(self, trainer: Trainer, pl_module: LightningModule):
+            self._checkpoint._handle(trainer, pl_module)
+            self._report._handle(trainer, pl_module)
+
+else:
+    # If Tune is not installed.
+    TuneReportCallback = Unavailable
+    TuneReportCheckpointCallback = Unavailable
