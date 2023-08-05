9b12bb413b801fdf976d3e929770413bdab2fee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package cay/0.1.1/cay-0.1.1.tar.gz

---
+++ packages/cay/cay-0.1.1.tar.gz/cay-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['cay']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['cay = cay.cli:main']}
+
+setup_kwargs = {
+    'name': 'cay',
+    'version': '0.1.1',
+    'description': 'Simple calculator implemented in Python',
+    'long_description': None,
+    'author': 'Naoya Yamashita',
+    'author_email': 'conao3@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
