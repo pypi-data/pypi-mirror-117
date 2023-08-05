48c3181469efd04acb258ea7da38f9619484d6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:49 +0000

    Add package whetstone/0.4.0/whetstone-0.4.0.tar.gz

---
+++ packages/whetstone/whetstone-0.4.0.tar.gz/whetstone-0.4.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['whetstone']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=21.7-beta.0,<22.0',
+ 'oauthlib>=3.1.0,<4.0.0',
+ 'requests>=2.24.0,<3.0.0',
+ 'requests_oauthlib>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'whetstone',
+    'version': '0.4.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Charlie Bini',
+    'author_email': 'cbini@kippnj.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
