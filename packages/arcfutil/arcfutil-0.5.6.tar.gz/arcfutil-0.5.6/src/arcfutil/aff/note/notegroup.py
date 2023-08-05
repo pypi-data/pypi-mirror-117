a7a08d956cdc4e209049479dd4ddf5e0e3f3c650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:01 +0000

    Add package arcfutil/0.5.6/arcfutil-0.5.6.tar.gz

---
+++ packages/arcfutil/arcfutil-0.5.6.tar.gz/arcfutil-0.5.6/src/arcfutil/aff/note/notegroup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# (c)2021 .direwolf <kururinmiracle@outlook.com>
+# Licensed under the MIT License.
+
+from .common_note import Note
+from .common_note import NoteGroup
+from .timing import Timing
+from ..sorter import sort
+
+
+class AffList(NoteGroup):
+    def __init__(self, *notes, offset: int = 0, desinty: int = 1):
+        super(AffList, self).__init__(*notes)
+        self.offset: int = offset
+        self.desnity: int = desinty
+
+    def __str__(self):
+        return ''.join([
+            'AudioOffset:{:d}\n'.format(int(self.offset)),
+            'TimingPointDensityFactor:{:d}\n'.format(int(self.desnity)) if self.desnity != 1 else '',
+            '-\n'
+        ]) + super().__str__()
+
+    def offsetto(self, value: int):
+        basebpm = 0
+        for each in self:
+            if isinstance(each, Timing) and each.time == 0:
+                basebpm = each.bpm
+        for each in self:
+            if each is not None:
+                each.offsetto(value)
+        self.insert(0, Timing(0, basebpm, 4))
+        return sort(self)
+
+
+class TimingGroup(NoteGroup):
+    def __init__(self, *notes, opt=None):
+        super(TimingGroup, self).__init__(*notes)
+        self.option = opt
+
+    def __str__(self):
+        group = 'timinggroup({0}){{'.format(str(self.option) if self.option is not None else '')
+        for each in self:
+            group += '\n{0}'.format(each)
+        group += '\n};'
+        return group
+
+    def append(self, __object) -> None:
+        if isinstance(__object, Note):  # 禁止TimingGroup内嵌套其它NoteGroup
+            super(NoteGroup, self).append(__object)
+        else:
+            raise TypeError('can only append Note (or its instance, not "{}") to {}'.format(
+                type(__object).__name__,
+                type(self).__name__
+            ))
+
+    def offsetto(self, value: int):
+        basebpm = 0
+        for each in self:
+            if isinstance(each, Timing) and each.time == 0:
+                basebpm = each.bpm
+        for each in self:
+            if each is not None:
+                each.offsetto(value)
+        self.insert(0, Timing(0, basebpm, 4))
+        return sort(self)
+
