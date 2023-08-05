3c521d6bdebe1aa408c8ed0a7033aae8bcef33e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package ctpbee-api/0.21/ctpbee_api-0.21.tar.gz

---
+++ packages/ctpbee-api/ctpbee_api-0.21.tar.gz/ctpbee_api-0.21/ctpbee_api/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.20"
+
+
+def get_interface(interface_name):
+    if interface_name == "ctp":
+        from ctpbee_api.ctp import MdApi, TdApi
+        return MdApi, TdApi
+
+    elif interface_name == "ctp_mini":
+        from ctpbee_api.ctp_mini import MiniMdApi, MiniTdApi
+        return MiniMdApi, MiniTdApi
+
+    else:
+        raise ValueError("INTERFACE NOT SUPPORTED")
---
+++ packages/ctpbee-api/ctpbee_api-0.21.tar.gz/ctpbee_api-0.21/ctpbee_api/ctp_mini/__init__.py
@@ -0,0 +1,3 @@
+from .CMdApi import MdApi as MiniMdApi
+from .CTdApi import TdApi as MiniTdApi
+from .mini_constant import *
