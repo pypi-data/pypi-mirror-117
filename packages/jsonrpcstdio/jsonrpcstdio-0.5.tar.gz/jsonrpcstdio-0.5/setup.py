eb531c08b4dc254530da415a2d973fb735ff08c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:41 +0000

    Add package jsonrpcstdio/0.5/jsonrpcstdio-0.5.tar.gz

---
+++ packages/jsonrpcstdio/jsonrpcstdio-0.5.tar.gz/jsonrpcstdio-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='jsonrpcstdio',
+      description='Asynchroncous Python implementation of JSON RPC for arbitrary'
+                  ' Streams and a particular implementation for JSON RPC of '
+                  'standard IO',
+      author='amas0',
+      version='0.5',
+      packages=find_packages(),
+      install_requires=[
+          'aioconsole',
+          'pydantic'
+      ])
