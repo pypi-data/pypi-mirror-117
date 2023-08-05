9688d98bc770dd0e76d1b7466eecd66890266d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:59 +0000

    Add package targetran/0.6.0/targetran-0.6.0.tar.gz

---
+++ packages/targetran/targetran-0.6.0.tar.gz/targetran-0.6.0/README.md
@@ -0,0 +1,5 @@
+# Targetran
+
+Targetran is a data augmentation library useful for object detection model 
+training. It provides a simple API in Python to transform both the images and 
+the target rectangular bounding-boxes.
---
+++ packages/targetran/targetran-0.6.0.tar.gz/targetran-0.6.0/targetran/tf/__init__.py
@@ -0,0 +1,19 @@
+from ._tf import (
+    to_tf,
+    seqs_to_tf_dataset,
+    tf_flip_left_right,
+    tf_flip_up_down,
+    tf_rotate,
+    tf_shear,
+    tf_translate,
+    tf_crop,
+    TFCombineAffine,
+    TFRandomTransform,
+    TFRandomFlipLeftRight,
+    TFRandomFlipUpDown,
+    TFRandomRotate,
+    TFRandomShear,
+    TFRandomTranslate,
+    TFRandomCrop,
+    TFResize
+)
