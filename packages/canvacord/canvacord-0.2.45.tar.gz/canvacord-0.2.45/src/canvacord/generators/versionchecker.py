4a5c08c443d619ac3bc2c80f9adc61652bba4a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:57 +0000

    Add package canvacord/0.2.45/canvacord-0.2.45.tar.gz

---
+++ packages/canvacord/canvacord-0.2.45.tar.gz/canvacord-0.2.45/src/canvacord/generators/versionchecker.py
@@ -0,0 +1,30 @@
+import aiohttp
+import requests
+import asyncio
+import json
+try:
+    from packaging.version import parse
+except ImportError:
+    print("\n Started Installing required Canvacord Package")
+    from pip._vendor.packaging.version import parse
+    print("\n Finished Installing required Canvacord Package")
+
+thisversion = "0.2.35"
+
+async def checkversion():
+        url_pattern = 'https://pypi.python.org/pypi/canvacord/json'
+        req = requests.get(url_pattern)
+        version = parse('0')
+        try:
+            j = json.loads(req.text.encode(req.encoding))
+            releases = j.get('releases', [])
+            for release in releases:
+                ver = parse(release)
+                if not ver.is_prerelease:
+                    version = max(version, ver)
+            if newresponse == thisversion:
+                pass
+            else:
+                print(f"Canvacord is on Version {newresponse} but you are only on Version {thisversion} Please Update for all the newest bug fixes and features!") 
+        except:
+            pass
