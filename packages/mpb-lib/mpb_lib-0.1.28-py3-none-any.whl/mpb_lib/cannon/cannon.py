8a0de76a867020ab298cb4236907e38f71e1442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:56 +0000

    Add package mpb-lib/0.1.28/mpb_lib-0.1.28-py3-none-any.whl

---
+++ packages/mpb-lib/mpb_lib-0.1.28-py3-none-any.whl/mpb_lib/cannon/cannon.py
@@ -0,0 +1,32 @@
+from string import Template
+
+from IPython.display import display, HTML
+try:
+    from google.colab.output import eval_js
+except:
+    def eval_js(js):
+        body = HTML(f"""<script>
+        {js}
+        </script>""")
+        display(body)
+
+
+from .cannon_html import HTML_TEMPLATE, SCRIPT, LOAD_SCRIPT
+
+
+def load_script():
+    html_body = HTML(LOAD_SCRIPT)
+    display(html_body)
+
+
+def cannon_game(powder):
+    html_source = Template(HTML_TEMPLATE)
+    html_body = HTML(html_source.substitute(**{"SCRIPT":SCRIPT, "stage":1, "powder":powder}))
+    display(html_body)
+
+
+def cannon_game2(powder):
+    html_source = Template(HTML_TEMPLATE)
+    html_body = HTML(html_source.substitute(**{"SCRIPT":SCRIPT, "stage":2, "powder":powder}))
+    display(html_body)
+
