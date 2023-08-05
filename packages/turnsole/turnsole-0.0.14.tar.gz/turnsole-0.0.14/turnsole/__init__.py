b469573ddda12d5b0d817baa1bb76a3a74787cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:49 +0000

    Add package turnsole/0.0.14/turnsole-0.0.14.tar.gz

---
+++ packages/turnsole/turnsole-0.0.14.tar.gz/turnsole-0.0.14/turnsole/__init__.py
@@ -0,0 +1,6 @@
+from .convenience import resize
+from .convenience import resize_with_pad
+from .encodings import base64_encode_file
+from .encodings import base64_to_image
+from .encodings import base64_encode_image
+from .encodings import base64_decode_image
