7ec26df52fd48371742a1fa50b9825896cb23a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:07 +0000

    Add package discord-ui/2.1.1/discord-ui-2.1.1.tar.gz

---
+++ packages/discord-ui/discord-ui-2.1.1.tar.gz/discord-ui-2.1.1/README.md
@@ -0,0 +1,8 @@
+# discord-message-components is now discord-ui
+## This package has been renamed. Instead use 
+
+```
+pip install discord-ui
+```
+
+New package: https://pypi.org/project/discord-ui/
---
+++ packages/discord-ui/discord-ui-2.1.1.tar.gz/discord-ui-2.1.1/setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+
+def get_version():
+    """returns the version of the package"""
+    with open("./discord-message-components/__init__.py", "r", encoding="utf-8") as f:
+        return [f for f in f.readlines() if f.startswith("__version__")][0].split('"')[1].split('"')[0]
+def get_readme():
+    """returns the readme content for the package"""
+    with open("./README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="discord-ui",
+    version=get_version(),
+    author="404kuso, RedstoneZockt",
+    author_email="bellou9022@gmail.com, redstoneprofihd@gmail.com",
+    description="A discord user-interface extension for discord.py",
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KusoRedsto/discord-ui",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9' if 'READTHEDOCS' not in os.environ else None,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
