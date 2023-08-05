3dcbeb9d0b09dd3a5e5975198f3affce06019a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:32 +0000

    Add package django-cfdi/1.65/django_cfdi-1.65-py3-none-any.whl

---
+++ packages/django-cfdi/django_cfdi-1.65-py3-none-any.whl/cfdi/__init__.py
@@ -0,0 +1,7 @@
+"""django-cfdi is a tool for friendly work with CFDI's ."""
+
+
+__version__ = open("cfdi/version", "r").read()
+
+#from .classes import *
+from .classes import CFDI, XmlNewObject, Object
