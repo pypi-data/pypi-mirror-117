c2345e8232e0c926d547b30640fe6da26d31cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:35 +0000

    Add package slowo/0.3/slowo-0.3.tar.gz

---
+++ packages/slowo/slowo-0.3.tar.gz/slowo-0.3/src/slowo/wepwace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import re
+
+WORD_HASH = {
+    "small": "smol",
+    "cute": "kawaii~",
+    "fluff": "floof",
+    "love": "luv",
+    "stupid": "baka",
+    "idiot": "baka",
+    "what": "nani",
+    "meow": "nya~",
+    "roar": "rawrr~",
+}
+
+uwugex = re.compile(r"(?<![w])[lr](?![w])") # Updated UwUgex
+
+def word_replace(input_string: str) -> str:
+    for word in WORD_HASH: # Python sucks because Verboof doesn't let me do what I want :c
+        input_string = input_string.replace(word, WORD_HASH[word])
+
+    return input_string
+
+def char_replace(input_string: str) -> str:
+    return uwugex.sub("w", input_string)
+
