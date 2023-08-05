a850fcdc4a34685553c83a7f1aafeb2723ccb601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:54 +0000

    Add package osiris-sdk/0.6.3/osiris-sdk-0.6.3.tar.gz

---
+++ packages/osiris-sdk/osiris-sdk-0.6.3.tar.gz/osiris-sdk-0.6.3/src/osiris/apis/dependencies.py
@@ -0,0 +1,78 @@
+"""
+Contains common functionalities for Egress and Ingress API's
+"""
+import json
+import logging
+from http import HTTPStatus
+from json import JSONDecodeError
+from typing import Any
+
+from requests import Response
+
+
+logger = logging.getLogger(__name__)
+
+
+def handle_download_response(response: Response) -> Any:
+    """
+     Checks status codes and converts JSON string to Python objects. Returns empty list if no result was found.
+    """
+
+    check_status_code(response)
+
+    if response.status_code == HTTPStatus.NO_CONTENT:
+        return []
+
+    try:
+        return json.loads(response.content)
+    except JSONDecodeError:
+        message = 'File is not correctly JSON formatted.'
+        logger.error(message)
+        raise ValueError(message) from JSONDecodeError
+
+
+def handle_parquet_response(response: Response) -> Any:
+    """
+     Checks status codes and converts JSON string to Python objects. Returns empty list if no result was found.
+    """
+
+    check_status_code(response)
+
+    if response.status_code == HTTPStatus.NO_CONTENT:
+        return None
+
+    return response.content
+
+
+def check_status_code(response: Response):
+    """
+    Converts HTTP errors to Python Exceptions
+    """
+    if response.status_code == HTTPStatus.BAD_GATEWAY:
+        # The response in response.text gives only 502 Bad Gateway in HTML
+        # This is often caused by processing too much data in Egress-API
+        detail = '(502 Bad Gateway) Often caused by too large dataset request - try to limit date range requested'
+        logger.error('(Bad gateway) %s', response.text)
+        raise Exception(detail)
+
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        detail = json.loads(response.text)['detail']
+        logger.error('(FileNotFoundError) %s', detail)
+        raise FileNotFoundError(detail)
+
+    if response.status_code == HTTPStatus.BAD_REQUEST:
+        detail = json.loads(response.text)['detail']
+        logger.error('(ValueError) %s', detail)
+        raise ValueError(detail)
+
+    if response.status_code == HTTPStatus.FORBIDDEN or \
+       response.status_code == HTTPStatus.UNAUTHORIZED:
+        detail = json.loads(response.text)['detail']
+        logger.error('(PermissionError) %s', detail)
+        raise PermissionError(detail)
+
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR or \
+       response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        detail = json.loads(response.text)['detail']
+        logger.error('(Exception) %s', detail)
+        raise Exception(detail)
