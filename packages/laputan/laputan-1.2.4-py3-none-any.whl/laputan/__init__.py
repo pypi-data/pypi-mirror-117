aa2356806e559a816fbbff7b3fbc72b134b154d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package laputan/1.2.4/laputan-1.2.4-py3-none-any.whl

---
+++ packages/laputan/laputan-1.2.4-py3-none-any.whl/laputan/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+print("\n               \\  \\|/  /")
+# print("             \\  \\_ | _/  /")
+print("            \\ _\\_ ||| _/_ /")
+print("                  |||")
+print("              /\\/-/|\\-\\/\\")
+print("          _ | _M_ _|_ _M_ | _")
+print("        / .~.    _ ^ _    .~. \\")
+print("       |_|_0_|  |_&_&_|  |_0_|_|")
+print("      //    .~.    o    .~.    \\\\")
+print("     |_M_ _|_?_|__[_]__|_?_|_ _M_|")
+print("            |             |")
+print("             \\  laputan  /")
+print("               \\       /         _")
+print("    * ¨^  .~       =           ~^' _-")
+print("      ~ °\n")
+# print("\n            Author: D. HU")
+print("        Version 1.2.4 (20210820)")
+print("\n")
+
+
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+def iTest():
+	'''
+	Initial test
+	'''
+	print("\nHouston, Tranquility Base here. The Eagle has landed.\n")
+
+if __name__ == '__main__':
+    
+    iTest()
