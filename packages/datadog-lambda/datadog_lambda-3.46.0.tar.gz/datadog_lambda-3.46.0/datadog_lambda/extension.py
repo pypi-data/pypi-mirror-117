33cec48bbfaf0edc696f474b3bed867f9252b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:11 +0000

    Add package datadog-lambda/3.46.0/datadog_lambda-3.46.0.tar.gz

---
+++ packages/datadog-lambda/datadog_lambda-3.46.0.tar.gz/datadog_lambda-3.46.0/datadog_lambda/extension.py
@@ -0,0 +1,33 @@
+import logging
+import requests
+from os import path
+
+AGENT_URL = "http://127.0.0.1:8124"
+HELLO_PATH = "/lambda/hello"
+FLUSH_PATH = "/lambda/flush"
+EXTENSION_PATH = "/opt/extensions/datadog-agent"
+
+logger = logging.getLogger(__name__)
+
+
+def is_extension_running():
+    if not path.exists(EXTENSION_PATH):
+        return False
+    try:
+        requests.get(AGENT_URL + HELLO_PATH)
+    except Exception as e:
+        logger.debug("Extension is not running, returned with error %s", e)
+        return False
+    return True
+
+
+def flush_extension():
+    try:
+        requests.post(AGENT_URL + FLUSH_PATH, data={})
+    except Exception as e:
+        logger.debug("Failed to flush extension, returned with error %s", e)
+        return False
+    return True
+
+
+should_use_extension = is_extension_running()
