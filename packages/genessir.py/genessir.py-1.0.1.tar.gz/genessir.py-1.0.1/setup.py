ab3f69ca6f65d48fb7dffe9010b212b8c14b4b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package genessir.py/1.0.1/genessir.py-1.0.1.tar.gz

---
+++ packages/genessir.py/genessir.py-1.0.1.tar.gz/genessir.py-1.0.1/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup
+import re
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+version = ''
+with open('genessir/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+
+extras_require = {
+    'voice': ['PyNaCl>=1.3.0,<1.5'],
+    'docs': [
+        'sphinx==3.0.3',
+        'sphinxcontrib_trio==1.1.2',
+        'sphinxcontrib-websupport',
+    ]
+}
+
+setup(name='genessir.py',
+      author='Genessir',
+      url='https://github.com/dolfies/discord.py-self',
+      project_urls={
+        "Documentation": "https://dolf.ml/discord.py-self",
+        "Issue tracker": "https://github.com/dolfies/discord.py-self/issues",
+      },
+      version=version,
+      packages=['genessir', 'genessir.ext.commands', 'genessir.ext.tasks', 'genessir.accounts'],
+      license='MIT',
+      description='A Python wrapper for the Discord API',
+      include_package_data=True,
+      install_requires=requirements,
+      extras_require=extras_require,
+      python_requires='>=3.8',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
