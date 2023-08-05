e21bf339c9da65b3ef3b8750f5ef3f7c5b88f8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:48 +0000

    Add package gdbd-distributions/1.2/gdbd_distributions-1.2.tar.gz

---
+++ packages/gdbd-distributions/gdbd_distributions-1.2.tar.gz/gdbd_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gdbd_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['gdbd_distributions'],
+      author = 'Sourabh Rustagi',
+      author_email = 'sourabhrustagi1@gmail.com',
+      zip_safe=False)
