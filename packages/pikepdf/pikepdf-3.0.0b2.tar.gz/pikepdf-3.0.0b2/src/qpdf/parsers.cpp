671bfccd25b7d294a0c288c8c519e031573aea1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:42 +0000

    Add package pikepdf/3.0.0b2/pikepdf-3.0.0b2.tar.gz

---
+++ packages/pikepdf/pikepdf-3.0.0b2.tar.gz/pikepdf-3.0.0b2/src/qpdf/parsers.cpp
@@ -0,0 +1,186 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (C) 2021, James R. Barlow (https://github.com/jbarlow83/)
+ */
+
+#include <sstream>
+#include <locale>
+
+#include "pikepdf.h"
+#include "parsers.h"
+
+void PyParserCallbacks::handleObject(QPDFObjectHandle obj, size_t offset, size_t length)
+{
+    PYBIND11_OVERRIDE_NAME(void,
+        QPDFObjectHandle::ParserCallbacks,
+        "handle_object", /* Python name */
+        handleObject,    /* C++ name */
+        obj,
+        offset,
+        length);
+}
+
+void PyParserCallbacks::handleEOF()
+{
+    PYBIND11_OVERRIDE_PURE_NAME(void,
+        QPDFObjectHandle::ParserCallbacks,
+        "handle_eof", /* Python name */
+        handleEOF,    /* C++ name; trailing comma needed for macro */
+    );
+}
+
+OperandGrouper::OperandGrouper(const std::string &operators)
+    : parsing_inline_image(false), count(0)
+{
+    std::istringstream f(operators);
+    f.imbue(std::locale::classic());
+    std::string s;
+    while (std::getline(f, s, ' ')) {
+        this->whitelist.insert(s);
+    }
+}
+
+void OperandGrouper::handleObject(QPDFObjectHandle obj)
+{
+    this->count++;
+    if (obj.getTypeCode() == QPDFObject::object_type_e::ot_operator) {
+        std::string op = obj.getOperatorValue();
+
+        // If we have a whitelist and this operator is not on the whitelist,
+        // discard it and all the tokens we collected
+        if (!this->whitelist.empty()) {
+            if (op[0] == 'q' || op[0] == 'Q') {
+                // We have token with multiple stack push/pops
+                if (this->whitelist.count("q") == 0 &&
+                    this->whitelist.count("Q") == 0) {
+                    this->tokens.clear();
+                    return;
+                }
+            } else if (this->whitelist.count(op) == 0) {
+                this->tokens.clear();
+                return;
+            }
+        }
+        if (op == "BI") {
+            this->parsing_inline_image = true;
+        } else if (this->parsing_inline_image) {
+            if (op == "ID") {
+                this->inline_metadata = this->tokens;
+            } else if (op == "EI") {
+                auto PdfInlineImage =
+                    py::module_::import("pikepdf").attr("PdfInlineImage");
+                auto kwargs            = py::dict();
+                kwargs["image_data"]   = this->tokens[0];
+                kwargs["image_object"] = this->inline_metadata;
+                auto iimage            = PdfInlineImage(**kwargs);
+
+                // Package as list with single element for consistency
+                auto iimage_list = py::list();
+                iimage_list.append(iimage);
+
+                auto instruction = py::make_tuple(
+                    iimage_list, QPDFObjectHandle::newOperator("INLINE IMAGE"));
+                this->instructions.append(instruction);
+
+                this->parsing_inline_image = false;
+                this->inline_metadata.clear();
+            }
+        } else {
+            py::tuple instruction;
+            if (this->tokens.empty())
+                instruction = py::make_tuple(py::tuple(), obj);
+            else
+                instruction = py::make_tuple(this->tokens, obj);
+            this->instructions.append(instruction);
+        }
+        this->tokens.clear();
+    } else {
+        this->tokens.push_back(obj);
+    }
+}
+
+void OperandGrouper::handleEOF()
+{
+    if (!this->tokens.empty())
+        this->warning = "Unexpected end of stream";
+}
+
+py::list OperandGrouper::getInstructions() const { return this->instructions; }
+std::string OperandGrouper::getWarning() const { return this->warning; }
+
+py::bytes unparse_content_stream(py::iterable contentstream)
+{
+    uint n = 0;
+    std::ostringstream ss, errmsg;
+    const char *delim = "";
+    ss.imbue(std::locale::classic());
+
+    for (const auto &item : contentstream) {
+        auto operands_op = py::reinterpret_borrow<py::sequence>(item);
+
+        // First iteration: print nothing
+        // All others: print "\n" to delimit previous
+        // Result is no leading or trailing delimiter
+        ss << delim;
+        delim = "\n";
+
+        if (operands_op.size() != 2) {
+            errmsg << "Wrong number of operands at content stream instruction " << n
+                   << "; expected 2";
+            throw py::value_error(errmsg.str());
+        }
+
+        auto operator_ = operands_op[1];
+
+        QPDFObjectHandle op;
+        if (py::isinstance<py::str>(operator_)) {
+            py::str s = py::reinterpret_borrow<py::str>(operator_);
+            op        = QPDFObjectHandle::newOperator(std::string(s).c_str());
+        } else if (py::isinstance<py::bytes>(operator_)) {
+            py::bytes s = py::reinterpret_borrow<py::bytes>(operator_);
+            op          = QPDFObjectHandle::newOperator(std::string(s).c_str());
+        } else {
+            op = operator_.cast<QPDFObjectHandle>();
+            if (!op.isOperator()) {
+                errmsg
+                    << "At content stream instruction " << n
+                    << ", the operator is not of type pikepdf.Operator, bytes or str";
+                throw py::type_error(errmsg.str());
+            }
+        }
+
+        if (op.getOperatorValue() == std::string("INLINE IMAGE")) {
+            auto operands     = py::reinterpret_borrow<py::sequence>(operands_op[0]);
+            py::object iimage = operands[0];
+            py::handle PdfInlineImage =
+                py::module::import("pikepdf").attr("PdfInlineImage");
+            if (!py::isinstance(iimage, PdfInlineImage)) {
+                errmsg << "Expected PdfInlineImage as operand for instruction " << n;
+                throw py::value_error(errmsg.str());
+            }
+            py::object iimage_unparsed_bytes = iimage.attr("unparse")();
+            ss << std::string(py::bytes(iimage_unparsed_bytes));
+        } else {
+            try {
+                // First try direct conversion...
+                auto objectlist = operands_op[0].cast<std::vector<QPDFObjectHandle>>();
+                for (QPDFObjectHandle &obj : objectlist) {
+                    ss << obj.unparseBinary() << " ";
+                }
+            } catch (const py::cast_error &) {
+                auto operands = py::reinterpret_borrow<py::sequence>(operands_op[0]);
+                for (const auto &operand : operands) {
+                    QPDFObjectHandle obj = objecthandle_encode(operand);
+                    ss << obj.unparseBinary() << " ";
+                }
+            }
+            ss << op.unparseBinary();
+        }
+
+        n++;
+    }
+    return py::bytes(ss.str());
+}
---
+++ packages/pikepdf/pikepdf-3.0.0b2.tar.gz/pikepdf-3.0.0b2/tests/test_parsers.py
@@ -0,0 +1,241 @@
+import shutil
+import sys
+from subprocess import PIPE, run
+
+import pytest
+
+import pikepdf
+from pikepdf import (
+    Dictionary,
+    Name,
+    Object,
+    Operator,
+    Pdf,
+    PdfError,
+    PdfInlineImage,
+    PdfMatrix,
+    Stream,
+    _qpdf,
+    parse_content_stream,
+    unparse_content_stream,
+)
+from pikepdf._qpdf import StreamParser
+from pikepdf.models import PdfParsingError
+
+# pylint: disable=useless-super-delegation
+
+
+class PrintParser(StreamParser):
+    def __init__(self):
+        super().__init__()
+
+    def handle_object(self, obj, *_args):
+        print(repr(obj))
+
+    def handle_eof(self):
+        print("--EOF--")
+
+
+class ExceptionParser(StreamParser):
+    def __init__(self):
+        super().__init__()
+
+    def handle_object(self, obj, *_args):  # pylint: disable=unused-argument
+        raise ValueError("I take exception to this")
+
+    def handle_eof(self):
+        print("--EOF--")
+
+
+def slow_unparse_content_stream(instructions):
+    def encode(obj):
+        return _qpdf.unparse(obj)
+
+    def encode_iimage(iimage: PdfInlineImage):
+        return iimage.unparse()
+
+    def encode_operator(obj):
+        if isinstance(obj, Operator):
+            return obj.unparse()
+        return encode(Operator(obj))
+
+    def for_each_instruction():
+        for n, (operands, operator) in enumerate(instructions):
+            try:
+                if operator == Operator(b'INLINE IMAGE'):
+                    iimage = operands[0]
+                    if not isinstance(iimage, PdfInlineImage):
+                        raise ValueError(
+                            "Operator was INLINE IMAGE but operands were not "
+                            "a PdfInlineImage"
+                        )
+                    line = encode_iimage(iimage)
+                else:
+                    if operands:
+                        line = b' '.join(encode(operand) for operand in operands)
+                        line += b' ' + encode_operator(operator)
+                    else:
+                        line = encode_operator(operator)
+            except (PdfError, ValueError) as e:
+                raise PdfParsingError(line=n + 1) from e
+            yield line
+
+    return b'\n'.join(for_each_instruction())
+
+
+def test_open_pdf(resources):
+    pdf = Pdf.open(resources / 'graph.pdf')
+    page = pdf.pages[0]
+    Object._parse_stream(page.obj, PrintParser())
+
+
+def test_parser_exception(resources):
+    pdf = Pdf.open(resources / 'graph.pdf')
+    stream = pdf.pages[0]['/Contents']
+    with pytest.raises(ValueError):
+        Object._parse_stream(stream, ExceptionParser())
+
+
+@pytest.mark.skipif(shutil.which('pdftotext') is None, reason="poppler not installed")
+def test_text_filter(resources, outdir):
+    input_pdf = resources / 'veraPDF test suite 6-2-10-t02-pass-a.pdf'
+
+    # Ensure the test PDF has detect we can find
+    proc = run(
+        ['pdftotext', str(input_pdf), '-'], check=True, stdout=PIPE, encoding='utf-8'
+    )
+    assert proc.stdout.strip() != '', "Need input test file that contains text"
+
+    pdf = Pdf.open(input_pdf)
+    page = pdf.pages[0]
+
+    keep = []
+    for operands, command in parse_content_stream(
+        page, """TJ Tj ' " BT ET Td TD Tm T* Tc Tw Tz TL Tf Tr Ts"""
+    ):
+        if command == Operator('Tj'):
+            print("skipping Tj")
+            continue
+        keep.append((operands, command))
+
+    new_stream = Stream(pdf, pikepdf.unparse_content_stream(keep))
+    print(new_stream.read_bytes())  # pylint: disable=no-member
+    page['/Contents'] = new_stream
+    page['/Rotate'] = 90
+
+    pdf.save(outdir / 'notext.pdf', static_id=True)
+    pdf.close()
+
+    proc = run(
+        ['pdftotext', str(outdir / 'notext.pdf'), '-'],
+        check=True,
+        stdout=PIPE,
+        encoding='utf-8',
+    )
+
+    assert proc.stdout.strip() == '', "Expected text to be removed"
+
+
+def test_invalid_stream_object():
+    with pytest.raises(TypeError, match="must be a pikepdf.Object"):
+        parse_content_stream(42)
+
+    with pytest.raises(TypeError, match="called on page or stream"):
+        parse_content_stream(Dictionary({"/Hi": 3}))
+
+    with pytest.raises(
+        TypeError, match="parse_content_stream called on non-stream Object"
+    ):
+        false_page = Dictionary(Type=Name.Page, Contents=42)
+        parse_content_stream(false_page)
+
+
+# @pytest.mark.parametrize(
+#     "test_file,expected",
+#     [
+#         ("fourpages.pdf", True),
+#         ("graph.pdf", False),
+#         ("veraPDF test suite 6-2-10-t02-pass-a.pdf", True),
+#         ("veraPDF test suite 6-2-3-3-t01-fail-c.pdf", False),
+#         ('sandwich.pdf', True),
+#     ],
+# )
+# def test_has_text(resources, test_file, expected):
+#     pdf = Pdf.open(resources / test_file)
+#     for p in pdf.pages:
+#         page = p
+#         assert page.has_text() == expected
+
+
+def test_unparse_cs():
+    instructions = [
+        ([], Operator('q')),
+        ([*PdfMatrix.identity().shorthand], Operator('cm')),
+        ([], Operator('Q')),
+    ]
+    assert unparse_content_stream(instructions).strip() == b'q\n1 0 0 1 0 0 cm\nQ'
+
+
+def test_unparse_failure():
+    instructions = [([float('nan')], Operator('cm'))]
+    with pytest.raises(PdfParsingError):
+        unparse_content_stream(instructions)
+
+
+def test_parse_xobject(resources):
+    pdf = Pdf.open(resources / 'formxobject.pdf')
+    form1 = pdf.pages[0].Resources.XObject.Form1
+    instructions = parse_content_stream(form1)
+    assert instructions[0][1] == Operator('cm')
+
+
+def test_unparse_interpret_operator():
+    commands = []
+    matrix = [2, 0, 0, 2, 0, 0]
+    commands.insert(0, (matrix, 'cm'))
+    commands.insert(0, (matrix, b'cm'))
+    commands.insert(0, (matrix, Operator('cm')))
+    unparsed = unparse_content_stream(commands)
+    assert (
+        unparsed
+        == b'2 0 0 2 0 0 cm\n2 0 0 2 0 0 cm\n2 0 0 2 0 0 cm'
+        == slow_unparse_content_stream(commands)
+    )
+
+
+def test_unparse_inline(resources):
+    with Pdf.open(resources / 'image-mono-inline.pdf') as pdf:
+        p0 = pdf.pages[0]
+        cmds = parse_content_stream(p0)
+        unparsed = unparse_content_stream(cmds)
+        assert b'BI' in unparsed
+        assert unparsed == slow_unparse_content_stream(cmds)
+
+
+def test_unparse_invalid_inline_image():
+    instructions = [((42,), Operator(b'INLINE IMAGE'))]
+
+    with pytest.raises(PdfParsingError):
+        unparse_content_stream(instructions)
+
+
+class TestMalformedContentStreamInstructions:
+    def test_rejects_not_list_of_pairs(self):
+        with pytest.raises(PdfParsingError):
+            unparse_content_stream([(1, 2, 3)])
+
+    def test_rejects_not_operator(self):
+        with pytest.raises(PdfParsingError):
+            unparse_content_stream([(['one', 'two'], 42)])  # 42 is not an operator
+
+    def test_rejects_inline_image_missing(self):
+        with pytest.raises(PdfParsingError):
+            unparse_content_stream(
+                [('should be a PdfInlineImage but is not', b'INLINE IMAGE')]
+            )
+
+    def test_accepts_all_lists(self):
+        unparse_content_stream([[[], b'Q']])
+
+    def test_accepts_all_tuples(self):
+        unparse_content_stream((((Name.Foo,), b'/Do'),))
