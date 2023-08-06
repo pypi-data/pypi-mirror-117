69490357433e1682767843dc227641f2abfb1979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:56 +0000

    Add package TypeTools/1.2.0/TypeTools-1.2.0.tar.gz

---
+++ packages/TypeTools/TypeTools-1.2.0.tar.gz/TypeTools-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TypeTools",
+    version="1.2.0",
+    author="Alan Bagel",
+    author_email="jiuprogrammer@gmail.com",
+    description="Help with enforcing types.",
+    long_description="Help with enforcing types.",
+    packages=["typetools"],
+)
