44fa4c6a4d930798aa578e04b02171d1534197fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:13 +0000

    Add package jsondb.py/0.1.1/jsondb.py-0.1.1.tar.gz

---
+++ packages/jsondb.py/jsondb.py-0.1.1.tar.gz/jsondb.py-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="jsondb.py",
+    version="0.1.1",
+    description="JSON as local database",
+    author="Sunney-X",
+    author_email="sanishubitidze@live.com",
+    packages=find_packages(exclude="tests"),
+)
