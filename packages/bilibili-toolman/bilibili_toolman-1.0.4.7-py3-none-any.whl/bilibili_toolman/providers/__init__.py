d33cd1a01c4d8e0a0cd7703df118a916a4782cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:09 +0000

    Add package bilibili-toolman/1.0.4.7/bilibili_toolman-1.0.4.7-py3-none-any.whl

---
+++ packages/bilibili-toolman/bilibili_toolman-1.0.4.7-py3-none-any.whl/bilibili_toolman/providers/__init__.py
@@ -0,0 +1,31 @@
+'''Content provider modules'''
+from typing import List
+
+
+class DownloadResult:
+    video_path : str = ''
+    cover_path : str = ''
+
+    title : str = ''
+    description : str = ''    
+    soruce : str = ''
+
+    original : bool = False
+    results = None
+
+    extra = dict() # BAD MOVE! though this SHOULD be overwritten and not accessed
+
+    @property
+    def results(self):
+        '''A list of sub download results'''
+        self._results : List[DownloadResult] # hack to allow typing of ourself
+        return self._results    
+    
+    def __init__(self) -> None:
+        self._results = []
+
+    def __enter__(self):        
+        return DownloadResult()
+    def __exit__(self,*args):pass
+from . import youtube as provider_youtube
+from . import localfile as provider_localfile
