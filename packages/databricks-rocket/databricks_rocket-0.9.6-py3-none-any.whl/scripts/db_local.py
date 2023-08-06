2d2d5bfb425243650afd06d387810850cb7bbd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:28 +0000

    Add package databricks-rocket/0.9.6/databricks_rocket-0.9.6-py3-none-any.whl

---
+++ packages/databricks-rocket/databricks_rocket-0.9.6-py3-none-any.whl/scripts/db_local.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""Python script which compiles the library and deploys it via Databricks CLI to the
+Databricks file system of our live Databricks installation."""
+
+import fire
+import sys
+breakpoint()
+#import databricks_local
+#fire.Fire(databricks_local.Rocket)
