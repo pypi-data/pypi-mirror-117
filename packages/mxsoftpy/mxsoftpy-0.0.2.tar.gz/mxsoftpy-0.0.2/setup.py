f880976a2cc04ee0d077ed9b40fd34d032b76be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package mxsoftpy/0.0.2/mxsoftpy-0.0.2.tar.gz

---
+++ packages/mxsoftpy/mxsoftpy-0.0.2.tar.gz/mxsoftpy-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='mxsoftpy',
+    version='0.0.2',
+    author='great',
+    author_email='great@mxsoft.com',
+    url='',
+    description=u'美信python框架',
+    packages=['mxsoftpy'],
+    install_requires=[],
+    entry_points={}
+)
