1384d06ad083cd115c021b09e70803f8597d0219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:22 +0000

    Add package django-audit-fields/0.3.4/django_audit_fields-0.3.4-py3-none-any.whl

---
+++ packages/django-audit-fields/django_audit_fields-0.3.4-py3-none-any.whl/django_audit_fields/constants.py
@@ -0,0 +1,10 @@
+AUDIT_MODEL_UPDATE_FIELDS = ["modified", "user_modified", "hostname_modified"]
+
+AUDIT_MODEL_FIELDS = [
+    "created",
+    "modified",
+    "user_created",
+    "user_modified",
+    "hostname_created",
+    "hostname_modified",
+]
