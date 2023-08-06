0263685fff64cb493f352e4a07a0d27f3debc82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:03 +0000

    Add package fastapi-users/6.1.3/fastapi-users-6.1.3.tar.gz

---
+++ packages/fastapi-users/fastapi-users-6.1.3.tar.gz/fastapi-users-6.1.3/fastapi_users/__init__.py
@@ -0,0 +1,7 @@
+"""Ready-to-use and customizable users management for FastAPI."""
+
+__version__ = "6.1.3"
+
+from fastapi_users import models  # noqa: F401
+from fastapi_users.fastapi_users import FastAPIUsers  # noqa: F401
+from fastapi_users.user import InvalidPasswordException  # noqa: F401
