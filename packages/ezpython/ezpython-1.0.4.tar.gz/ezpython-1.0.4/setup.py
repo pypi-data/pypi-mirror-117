3159dd7ee69b38a2376b31af141f18a472f54477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:34 +0000

    Add package ezpython/1.0.4/ezpython-1.0.4.tar.gz

---
+++ packages/ezpython/ezpython-1.0.4.tar.gz/ezpython-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+ 
+setup(name = 'ezpython',     # 包名
+      version = '1.0.4',  # 版本号
+      description = '',
+      long_description = '添加了函数readcode', 
+      author = 'phy',
+      author_email = '',
+      url = '',
+      license = '',
+      install_requires = [],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+      ],
+      keywords = '',
+      packages = find_packages('src'),  # 必填，就是包的代码主目录
+      package_dir = {'':'src'},         # 必填
+      include_package_data = True,
+)
+#!/usr/bin/env python
+ 
