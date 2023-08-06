c5ff616baa9fa30fd811e7a9606ef13d8ecb5e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:08 +0000

    Add package DenemePack/0.0.7/DenemePack-0.0.7.tar.gz

---
+++ packages/DenemePack/DenemePack-0.0.7.tar.gz/DenemePack-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+from setuptools import setup, find_packages
+setuptools.setup(
+    name="DenemePack",
+    version="0.0.7",
+    author="berre",
+    packages=["Phi"],
+    package_data={'': ['*.dll'],'':['src']},  # This is the most important line.
+    zip_safe=False,
+    include_package_data=True,
+    author_email="berreergunn@gmail.com",
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=2',
+    install_requires=[]
+)
