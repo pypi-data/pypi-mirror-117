9ccacd74619f6ce8e98f5d7aff6abbd91d08ce0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:45 +0000

    Add package lattice-stocks-data/1.0.1/lattice-stocks-data-1.0.1.tar.gz

---
+++ packages/lattice-stocks-data/lattice-stocks-data-1.0.1.tar.gz/lattice-stocks-data-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lattice-stocks-data",
+    version="1.0.1",
+
+    description="An API for gathering RapidAPI stock info",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://lattice.dev/",
+    project_urls={
+        "Source": "https://github.com/LatticeData/lattice-stocks-data",
+    },
+    py_modules = ["algorithms","buzz","economy","exchanges","financials","indices","market","screeners","search","similarity",
+                    "stock","valuation","yahoo_finance"],
+    
+    author="ashkon@lattice.dev",
+    package_dir={
+        "": "./stocksdata",
+    },
+    
+    tests_require=['pytest'],
+
+    python_requires=">=3.7",
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
