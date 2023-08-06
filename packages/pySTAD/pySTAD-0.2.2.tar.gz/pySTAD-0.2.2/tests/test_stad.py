0b41905fe91ccb667d5ee278629b16130e7f5be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:46 +0000

    Add package pySTAD/0.2.2/pySTAD-0.2.2.tar.gz

---
+++ packages/pySTAD/pySTAD-0.2.2.tar.gz/pySTAD-0.2.2/tests/test_stad.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import isspmatrix
+from scipy.spatial.distance import pdist
+
+import stad as sd
+
+data = pd.read_csv('../examples/data/horse.csv')
+idx = np.random.choice(data.shape[0], 200, replace=False)
+data = data.iloc[idx, :]
+dist = pdist(data, 'euclidean')
+dist /= dist.max()
+
+
+class TestStad:
+  def test_stad(self):
+    network, sweep = sd.stad(dist)
+    assert isspmatrix(network)
+    assert sweep.n_steps == 10 # just to check sweep is a _SweepBase
+    assert network.shape == (200, 200)
