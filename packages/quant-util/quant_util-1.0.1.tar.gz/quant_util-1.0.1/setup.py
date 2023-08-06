aa0f09fc9911d2db5f43264f07cadb32951c3262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:07 +0000

    Add package quant-util/1.0.1/quant_util-1.0.1.tar.gz

---
+++ packages/quant-util/quant_util-1.0.1.tar.gz/quant_util-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='quant_util',
+      version='1.0.1',
+      description='local quant_util',
+      author='linzeming',
+      author_email='linzeming1995@outlook.com',
+      url='https://www.python.org/',
+      license='MIT',
+      packages=['quant_util'],
+      python_requires='>=3'
+     )
