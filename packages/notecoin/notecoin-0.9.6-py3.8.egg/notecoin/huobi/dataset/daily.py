2100aca3c825666aa717f93d1b3da9aabe3bea55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:25 +0000

    Add package notecoin/0.9.6/notecoin-0.9.6-py3.8.egg

---
+++ packages/notecoin/notecoin-0.9.6-py3.8.egg/notecoin/huobi/dataset/daily.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+from notecoin.huobi.history.core import load_daily_all, load_symbol_all
+
+load_daily_all(period='1min',
+                      date=datetime(2021, 5, 18),
+                      download_dir='/root/workspace/tmp/coin/merge/1min')
