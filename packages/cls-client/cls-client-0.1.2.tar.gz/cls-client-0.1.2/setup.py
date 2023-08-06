f62529b3dace3768d3dd0a02165ac96ffa4ce0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:44 +0000

    Add package cls-client/0.1.2/cls-client-0.1.2.tar.gz

---
+++ packages/cls-client/cls-client-0.1.2.tar.gz/cls-client-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cls_client', 'cls_client.shared']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['appdirs>=1.4.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cls-client',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Dave Gaeddert',
+    'author_email': 'dave.gaeddert@dropseed.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
