6ee50f7b8e9c22d28261f7ada3dacc8edfd158eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:45 +0000

    Add package wacky-rl/0.0.4/wacky-rl-0.0.4.tar.gz

---
+++ packages/wacky-rl/wacky-rl-0.0.4.tar.gz/wacky-rl-0.0.4/wacky_rl/layers/__init__.py
@@ -0,0 +1,5 @@
+#from wacky_rl.layers.actor_layer import DiscreteActionLayer
+#from wacky_rl.layers.actor_layer import ContinActionLayer
+
+from wacky_rl.layers.contin_actor_layer import ContinActionLayer
+from wacky_rl.layers.discrete_actor_layer import DiscreteActionLayer
---
+++ packages/wacky-rl/wacky-rl-0.0.4.tar.gz/wacky-rl-0.0.4/wacky_rl/losses/__init__.py
@@ -0,0 +1,10 @@
+from wacky_rl.losses._loss_core import WackyLoss
+
+from wacky_rl.losses.actor_losses import ActorLoss
+from wacky_rl.losses.actor_losses import SoftActorLoss
+from wacky_rl.losses.actor_losses import PPOActorLoss
+
+from wacky_rl.losses.q_losses import SoftQLoss
+
+from wacky_rl.losses.val_losses import SoftValueLoss
+from wacky_rl.losses.val_losses import MeanSquaredErrorLoss
