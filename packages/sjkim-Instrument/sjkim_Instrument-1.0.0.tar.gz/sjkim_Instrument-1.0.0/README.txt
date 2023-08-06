c38e8da1b17801fdeb852d4e3c2e5a1e66eff039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:40 +0000

    Add package sjkim-Instrument/1.0.0/sjkim_Instrument-1.0.0.tar.gz

---
+++ packages/sjkim-Instrument/sjkim_Instrument-1.0.0.tar.gz/sjkim_Instrument-1.0.0/README.txt
@@ -0,0 +1,1 @@
+"GPIB instrument SCPI Control library"
---
+++ packages/sjkim-Instrument/sjkim_Instrument-1.0.0.tar.gz/sjkim_Instrument-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sjkim_Instrument",
+    version="1.0.0",
+    description="GPIB instrument SCPI Control library",
+    author="sjkim",
+    author_email="ksj101110111@gmail.com",
+    url="https://ksj10111011.blog.me/",
+    py_modules=["Instrument"],
+)
