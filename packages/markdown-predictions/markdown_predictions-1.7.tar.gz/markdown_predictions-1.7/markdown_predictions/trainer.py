a062c807d38ae3b6236f2b523fe14262131165a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:34 +0000

    Add package markdown-predictions/1.7/markdown_predictions-1.7.tar.gz

---
+++ packages/markdown-predictions/markdown_predictions-1.7.tar.gz/markdown_predictions-1.7/markdown_predictions/trainer.py
@@ -0,0 +1,18 @@
+from markdown_predictions.clean_data import PreProcessor
+from markdown_predictions.parse_data import LoadSalesData
+
+import pandas as pd
+
+
+def get_data(data_path: str) -> pd.DataFrame:
+    """ Load in and clean the data """
+    load_data = LoadSalesData.load_in_files(data_path)
+    pre_processor = PreProcessor(df=load_data.sales_data)
+    pre_processor.clean_up_data()
+    
+    return pre_processor.df
+
+
+if __name__ == "__main__":
+    print("HELLO 🥸🥝")
+    print(get_data("raw_data"))
