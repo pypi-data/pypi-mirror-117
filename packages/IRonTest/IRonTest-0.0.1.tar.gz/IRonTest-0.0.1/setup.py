43cf631d5601ed250e5755cd1c404daceae654e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package IRonTest/0.0.1/IRonTest-0.0.1.tar.gz

---
+++ packages/IRonTest/IRonTest-0.0.1.tar.gz/IRonTest-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="IRonTest",
+    version="0.0.1",
+    author="berre",
+    author_email="berreergunn@gmail.com",
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
+)
