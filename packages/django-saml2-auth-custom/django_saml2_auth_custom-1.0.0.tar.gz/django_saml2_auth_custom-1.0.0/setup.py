5eb76841879bf87b1263abdde60e8e41b9dc59d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:17 +0000

    Add package django-saml2-auth-custom/1.0.0/django_saml2_auth_custom-1.0.0.tar.gz

---
+++ packages/django-saml2-auth-custom/django_saml2_auth_custom-1.0.0.tar.gz/django_saml2_auth_custom-1.0.0/setup.py
@@ -0,0 +1,58 @@
+from codecs import open
+from setuptools import (setup, find_packages)
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django_saml2_auth_custom',
+
+    version='1.0.0',
+
+    description='Customizing Django SAML2 Authentication',
+    long_description=long_description,
+
+    url='https://github.com/ClaGz/',
+
+    author='Cláudio Givisiez',
+    author_email='audio.givisiez@gmail.com',
+
+    license='Apache 2.0',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='Customizing Django SAML2 Authentication',
+
+    packages=find_packages(),
+
+    install_requires=['pysaml2==4.0.5'],
+    include_package_data=True,
+)
