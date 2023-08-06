6257c2975e0d3066ad63006a8b6bfddf93e30441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:43 +0000

    Add package pacc/0.0.254/pacc-0.0.254-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.254-py3-none-any.whl/pacc/tools/__init__.py
@@ -0,0 +1,21 @@
+from .regular import findAllWithRe, findAllNumsWithRe
+from .sleep import sleep
+from .email import EMail
+from .dir import createDir
+from .xml import prettyXML, getXML
+from .math import average
+from .url import getURLsFromString
+from .ocr import getTextsFromPic
+
+__all__ = [
+    "findAllWithRe",
+    'findAllNumsWithRe',
+    "sleep",
+    'EMail',
+    'createDir',
+    'prettyXML',
+    'getXML',
+    'average',
+    'getURLsFromString',
+    'getTextsFromPic'
+]
