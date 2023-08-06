6a04c1314cc6b1dce4123167607637f90c9eb38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:57 +0000

    Add package petchoulauncher/0.1.7/PetchouLauncher-0.1.7.tar.gz

---
+++ packages/petchoulauncher/PetchouLauncher-0.1.7.tar.gz/PetchouLauncher-0.1.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['petchoulauncher']
+
+package_data = \
+{'': ['*'], 'petchoulauncher': ['assets/*']}
+
+install_requires = \
+['Kivy>=2.0.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'petchoulauncher',
+    'version': '0.1.7',
+    'description': 'A nice way to launch my other projects',
+    'long_description': None,
+    'author': 'PetchouHelper',
+    'author_email': 'petchou91d@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
