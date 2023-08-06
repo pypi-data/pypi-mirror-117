34c12d43fb51d9bf2e98b044b2bb073fcce10222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:03 +0000

    Add package pyLDT-cosmo/0.4.7/pyLDT-cosmo-0.4.7.tar.gz

---
+++ packages/pyLDT-cosmo/pyLDT-cosmo-0.4.7.tar.gz/pyLDT-cosmo-0.4.7/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+import os
+import re
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'pyLDT_cosmo', '__init__.py')) as fp:
+    for line in fp:
+        m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
+        if m:
+            version = m.group(2)
+            break
+    else:
+        raise RuntimeError('Unable to find own __version__ string')
+
+setuptools.setup(
+    name="pyLDT-cosmo",
+    version=version,
+    author="Matteo Cataneo",
+    author_email="mcataneo85@gmail.com",
+    description="Package for PDF calculations in Large Deviation Theory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcataneo/pyLDT-cosmo",
+    packages=setuptools.find_packages(),
+    package_data={'pyLDT_cosmo': ['benchmarks/*.dat']},
+    install_requires=[
+        'numpy==1.19.5',
+        'scipy==1.6.2',
+        'camb==1.3.0',
+        'mcfit==0.0.16',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
