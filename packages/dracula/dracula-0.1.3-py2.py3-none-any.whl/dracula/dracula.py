99bf35572b9cde1ab00216396cac7d8644f1bc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:25 +0000

    Add package dracula/0.1.3/dracula-0.1.3-py2.py3-none-any.whl

---
+++ packages/dracula/dracula-0.1.3-py2.py3-none-any.whl/dracula/dracula.py
@@ -0,0 +1,39 @@
+"""main"""
+class DraculaPalette:
+    bg = b = '#282a36'
+    fg = f = '#f8f8f2'
+    selection = s = '#44475a'
+    comment = m = '#6272a4'
+    cyan = c = '#8be9fd'
+    green = g = '#50fa7b'
+    orange = o = '#ffb86c'
+    pink = k = '#ff79c6'
+    purple = l = '#bd93f9'
+    red = r = '#ff5555'
+    yellow = y = '#f1fa8c'
+    colors_dict = {
+        "BG": "#282A36",
+        "FG": "#F8F8F2",
+        "SELECTION": "#44475A",
+        "COMMENT": "#6272A4",
+        "CYAN": "#8BE9FD",
+        "GREEN": "#50FA7B",
+        "ORANGE": "#FFB86C",
+        "PINK": "#FF79C6",
+        "PURPLE": "#BD93F9",
+        "RED": "#FF5555",
+        "YELLOW": "#F1FA8C"
+    }
+    aliases = {
+        "BG": "bg",
+        "FG": "fg",
+        "SELECTION": "s",
+        "COMMENT": "m",
+        "CYAN": "c",
+        "GREEN": "g",
+        "ORANGE": "o",
+        "PINK": "k",
+        "PURPLE": "l",
+        "RED": "r",
+        "YELLOW": "y"
+    }
