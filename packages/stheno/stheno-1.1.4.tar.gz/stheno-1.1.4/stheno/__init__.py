92ed41e2e3fe236f3af9de491dc0c25b9d659a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:05 +0000

    Add package stheno/1.1.4/stheno-1.1.4.tar.gz

---
+++ packages/stheno/stheno-1.1.4.tar.gz/stheno-1.1.4/stheno/__init__.py
@@ -0,0 +1,26 @@
+# noinspection PyUnresolvedReferences
+import matrix
+
+# noinspection PyUnresolvedReferences
+from mlkernels import *
+
+# noinspection PyUnresolvedReferences
+import lab as B
+
+from plum import PromisedType, Dispatcher
+
+PromisedFDD = PromisedType()
+PromisedGP = PromisedType()
+PromisedMeasure = PromisedType()
+
+_dispatch = Dispatcher()
+
+
+class BreakingChangeWarning(UserWarning):
+    """A breaking change."""
+
+
+from .lazy import *
+from .model import *
+from .mo import *
+from .random import *
