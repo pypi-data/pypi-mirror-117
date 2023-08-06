84415413e27ed8098699bc396705c3a6b146be21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:49 +0000

    Add package archivr/0.1.1/archivr-0.1.1.tar.gz

---
+++ packages/archivr/archivr-0.1.1.tar.gz/archivr-0.1.1/archivr/__init__.py
@@ -0,0 +1,44 @@
+__version__ = "0.1.0"
+
+from contextlib import contextmanager
+import os
+from pathlib import Path
+from typing import Generator, List
+import shutil
+import click
+from tqdm import tqdm
+
+@contextmanager
+def working_dir(dir: Path) -> Generator[Path, None, None]:
+    try:
+        cwd = os.curdir
+        os.chdir(dir)
+        yield dir
+    finally:
+        os.chdir(cwd)
+
+def get_folders(src: Path) -> List[Path]:
+    """Gets all subfolders of the given path."""
+
+    return [x for x in src.iterdir() if x.is_dir()]
+
+def clean(path: Path) -> None:
+    """Removes all contents of the given path."""
+    for f in path.iterdir():
+        if f.is_dir():
+            clean(f)
+            f.rmdir()
+            continue
+        
+        f.unlink()
+
+def archive(dir: str) -> None:
+    """Zips all folders in `src` and saves them to an `archive` folder."""
+    src: Path = Path(dir)
+    with working_dir(src) as d:
+        (d / 'arch').mkdir()
+        folders = get_folders(d)
+        folders.remove((d / 'arch'))
+        for folder in tqdm(folders):
+            archive_name = Path(f"arch/{folder.name}").resolve()
+            shutil.make_archive(str(archive_name), 'zip', folder)
