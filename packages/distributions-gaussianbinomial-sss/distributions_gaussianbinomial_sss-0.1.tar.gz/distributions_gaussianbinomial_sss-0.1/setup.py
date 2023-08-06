614b9eafbd347ea970155807489293040f759e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:52 +0000

    Add package distributions-gaussianbinomial-sss/0.1/distributions_gaussianbinomial_sss-0.1.tar.gz

---
+++ packages/distributions-gaussianbinomial-sss/distributions_gaussianbinomial_sss-0.1.tar.gz/distributions_gaussianbinomial_sss-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gaussianbinomial_sss',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_gaussianbinomial_sss'],
+      Author='Sandheep Sebastian',
+      zip_safe=False)
