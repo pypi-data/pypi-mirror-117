ad9d0bf5b3df2e343aa4fc8f4ef174130b0f5321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:05 +0000

    Add package date-assistant/0.10.2/date_assistant-0.10.2-py3-none-any.whl

---
+++ packages/date-assistant/date_assistant-0.10.2-py3-none-any.whl/date_assistant/formats.py
@@ -0,0 +1,13 @@
+# Let's use christmas date as an example
+# Who doesn't like christmas? :)
+
+# 2021-12-25
+YYYY_MM_DD = '%Y-%m-%d'
+# 25-12-2021
+DD_MM_YYYY = '%d-%m-%Y'
+# 12-25-2021
+MM_DD_YYYY = '%m-%d-%Y'
+# 12-2021
+MM_YYYY = '%m-%Y'
+# 2021-12
+YYYY_MM = '%Y-%m'
