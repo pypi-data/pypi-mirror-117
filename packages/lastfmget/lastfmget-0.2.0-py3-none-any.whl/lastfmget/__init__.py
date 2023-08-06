5a8b7f605b239dc36ac5e4945e003136a551bb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:19 +0000

    Add package lastfmget/0.2.0/lastfmget-0.2.0-py3-none-any.whl

---
+++ packages/lastfmget/lastfmget-0.2.0-py3-none-any.whl/lastfmget/__init__.py
@@ -0,0 +1,13 @@
+from .core import init
+from .methods import *
+from .raw_methods import *
+
+from .errors import (
+    LastFmGetError,
+    NotConfiguredError,
+    LastFmError,
+    ParamError,
+    ApiKeyError,
+    OfflineError,
+    RateLimitError
+)
