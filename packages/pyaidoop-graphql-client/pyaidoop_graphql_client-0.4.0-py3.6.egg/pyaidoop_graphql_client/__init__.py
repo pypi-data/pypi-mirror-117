61c1ce60f93680ae1554888798dc063f8f37602d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:08 +0000

    Add package pyaidoop-graphql-client/0.4.0/pyaidoop_graphql_client-0.4.0-py3.6.egg

---
+++ packages/pyaidoop-graphql-client/pyaidoop_graphql_client-0.4.0-py3.6.egg/pyaidoop_graphql_client/__init__.py
@@ -0,0 +1,4 @@
+import pyaidoop_graphql_client.graphql
+import pyaidoop_graphql_client.api
+
+__version__ = "0.4.0"
---
+++ packages/pyaidoop-graphql-client/pyaidoop_graphql_client-0.4.0-py3.6.egg/pyaidoop_graphql_client/graphql/__init__.py
@@ -0,0 +1,6 @@
+from pyaidoop_graphql_client.graphql.decorator import *
+from pyaidoop_graphql_client.graphql.queries_tracking_workspace import *
+from pyaidoop_graphql_client.graphql.queries_robot_arm import *
+from pyaidoop_graphql_client.graphql.queries_tracking_camera import *
+from pyaidoop_graphql_client.graphql.queries_trackable_object import *
+from pyaidoop_graphql_client.graphql.queries_robot_api import *
