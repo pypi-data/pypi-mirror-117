d0c48de53ccab279f21f2140bd91598dcf84cede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:04 +0000

    Add package sendqqmail/0.1/sendqqmail-0.1.tar.gz

---
+++ packages/sendqqmail/sendqqmail-0.1.tar.gz/sendqqmail-0.1/sendqqmail/sendqqmail.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+def send():
+    sender =input('your qq mail:')
+    receiver =input('you will send mail:')
+    psw =input('your mail passworl:')
+    content =input('mail body text:')
+    msg = MIMEText(content)
+    msg['From'] =input('your name:')
+    msg['Subject'] =input('mail title:')
+    msg['To'] = receiver
+    try:
+        server = smtplib.SMTP_SSL('smtp.qq.com',465)
+        server.login(sender,psw)
+        server.sendmail(sender,receiver,msg.as_string())
+        server.quit()
+        print("send already")
+    except:
+        print("fail")
+    return
+if __name__=='__main__':
+    print(__name__)
+    send()
