ddf3119982e13558bad82ba43d1b78fffb0f8bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:50 +0000

    Add package propertypro-scrapper/1.1/propertypro%20scrapper-1.1.tar.gz

---
+++ packages/propertypro-scrapper/propertypro%20scrapper-1.1.tar.gz/propertypro scrapper-1.1/README.txt
@@ -0,0 +1,19 @@
+Propertypro Scraper
+-------------------
+A data collection tool that helps scrape a housing website propertypro
+Installation
+----------------
+
+to use the package in your notebook:
+
+``!pip install propertypro-scrapper==1.1``
+
+``from propertypro.propertypro import Propertypro``
+
+
+Usage
+--------
+
+- scraper = Propertypro()
+
+- scraper.scrape_data(100, ["enugu"])
---
+++ packages/propertypro-scrapper/propertypro%20scrapper-1.1.tar.gz/propertypro scrapper-1.1/setup.py
@@ -0,0 +1,23 @@
+# from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
+
+ 
+classifiers = [
+  'Intended Audience :: Education',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='propertypro scrapper',
+  version='1.1',
+  description='A scraper that helps scrape a housing website propertypro',
+  long_description=open('README.txt').read(),
+  url='https://github.com/Ifyokoh/',  
+  author='Ifeoma Okoh',
+  author_email='odibest1893@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='scraper', 
+  packages=find_packages()
+)
