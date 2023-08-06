4d22fd29226048fb9b8db40726f91b9aa3f8c7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:43 +0000

    Add package tic-tac-toe-api-bot/0.2.2/tic_tac_toe_api_bot-0.2.2.tar.gz

---
+++ packages/tic-tac-toe-api-bot/tic_tac_toe_api_bot-0.2.2.tar.gz/tic_tac_toe_api_bot-0.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+f = open('./1.txt')
+l = f.read()
+f.close()
+print(l)
+
+setup(name='tic_tac_toe_api_bot',
+      version='0.2.2',
+      description='simple bot for tic tac toe',
+      long_description=l,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+      ],
+      keywords='tic_tac_toe bot api game_api ',
+      url='https://github.com/ruslan-ilesik/tic-tac-toe-bot-pip-package',
+      author='lesikr',
+      license='MIT',
+      packages=['tic_tac_toe'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False,
+      )
