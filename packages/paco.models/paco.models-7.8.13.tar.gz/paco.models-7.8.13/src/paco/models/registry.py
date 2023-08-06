42272cb43971048ee46037fed1b23204083d18d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:31 +0000

    Add package paco.models/7.8.13/paco.models-7.8.13.tar.gz

---
+++ packages/paco.models/paco.models-7.8.13.tar.gz/paco.models-7.8.13/src/paco/models/registry.py
@@ -0,0 +1,6 @@
+# Hooks that can be registered by paco.extend
+EXTEND_BASE_MODEL_HOOKS = []
+CW_ALARM_ACTIONS_HOOK = None
+CW_ALARM_HOOKS = []
+CW_ALARM_DESCRIPTION_HOOK = None
+CODESTAR_NOTIFICATION_RULE_HOOK = None
