d60b61963f9e5bfe1908b5c31d8f0045bd1bffeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:45 +0000

    Add package lithopscloud/1.0.0b42/lithopscloud-1.0.0b42.tar.gz

---
+++ packages/lithopscloud/lithopscloud-1.0.0b42.tar.gz/lithopscloud-1.0.0b42/src/lithopscloud/modules/lithops/__init__.py
@@ -0,0 +1,8 @@
+from lithopscloud.modules.lithops.ssh_key import LithopsSshKeyConfig
+from lithopscloud.modules.lithops.endpoint import LithopsEndpointConfig
+from lithopscloud.modules.lithops.vpc import LithopsVPCConfig
+from lithopscloud.modules.lithops.image import LithopsImageConfig
+from lithopscloud.modules.lithops.cos import CosConfig
+
+
+MODULES = [LithopsEndpointConfig, LithopsVPCConfig, LithopsSshKeyConfig, LithopsImageConfig, CosConfig]
