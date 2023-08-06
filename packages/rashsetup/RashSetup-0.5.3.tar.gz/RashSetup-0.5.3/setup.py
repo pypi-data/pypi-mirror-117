ced65ab45418230fcf786c36ab3e5604a144b52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:26 +0000

    Add package rashsetup/0.5.3/RashSetup-0.5.3.tar.gz

---
+++ packages/rashsetup/RashSetup-0.5.3.tar.gz/RashSetup-0.5.3/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rashsetup',
+ 'rashsetup.RashScrappers',
+ 'rashsetup.RashScrappers.RashScrappers',
+ 'rashsetup.RashScrappers.RashScrappers.spiders',
+ 'rashsetup.__RashModules__',
+ 'rashsetup.__RashModules__.Rash',
+ 'rashsetup.__RashModules__.Rash.Bars',
+ 'rashsetup.__RashModules__.Rash.Bars.Docks',
+ 'rashsetup.__RashModules__.Rash.Bars.Menu',
+ 'rashsetup.__RashModules__.Rash.Bars.Status',
+ 'rashsetup.__RashModules__.Rash.Bars.TabBar',
+ 'rashsetup.__RashModules__.Rash.Bars.Tools',
+ 'rashsetup.__RashModules__.Rash.Home',
+ 'rashsetup.__RashModules__.Rash.MainWindow',
+ 'rashsetup.__RashModules__.Rash.Processes',
+ 'rashsetup.__RashModules__.Rash.Source',
+ 'rashsetup.__RashModules__.Rash.Source.Manager',
+ 'rashsetup.__RashModules__.__']
+
+package_data = \
+{'': ['*'],
+ 'rashsetup.RashScrappers': ['RashScrappers/spiders/temp/*'],
+ 'rashsetup.__RashModules__': ['Rash/Misc/CSS/*',
+                               'Rash/Misc/Gifs/*',
+                               'Rash/Misc/HTML/*',
+                               'Rash/Misc/HTML/TextBrowser/*',
+                               'Rash/Misc/HTML/WebEngineView/*',
+                               'Rash/Misc/Icons/*',
+                               'Rash/Misc/UI/*',
+                               '__/Misc/CSS/*',
+                               '__/Misc/Gifs/*',
+                               '__/Misc/HTML/*',
+                               '__/Misc/HTML/TextBrowser/*',
+                               '__/Misc/HTML/WebEngineView/*',
+                               '__/Misc/Icons/*',
+                               '__/Misc/UI/*']}
+
+install_requires = \
+['PySide2>=5.15.2,<6.0.0']
+
+setup_kwargs = {
+    'name': 'rashsetup',
+    'version': '0.5.3',
+    'description': 'Setup Module that can be used for both testing Rash and also Setting up Rash',
+    'long_description': None,
+    'author': 'Rahul',
+    'author_email': 'saihanumarahul66@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<3.10',
+}
+
+
+setup(**setup_kwargs)
