26dcf3ca7485bd1c699598ad765a63406c26690a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:26 +0000

    Add package distributions-jp/0.23/distributions_jp-0.23.tar.gz

---
+++ packages/distributions-jp/distributions_jp-0.23.tar.gz/distributions_jp-0.23/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jp',
+      version='0.23',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
