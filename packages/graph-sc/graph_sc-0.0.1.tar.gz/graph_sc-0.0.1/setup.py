c2b269840d0b1f134071f4042d559ce6098eecc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:41 +0000

    Add package graph-sc/0.0.1/graph_sc-0.0.1.tar.gz

---
+++ packages/graph-sc/graph_sc-0.0.1.tar.gz/graph_sc-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read()
+
+setup(
+    name="graph_sc",
+    author_email="ciortanmadalina@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[requirements],
+    python_requires=">=3.6",
+    description="Graph-sc",
+    include_package_data=True,
+    package_data={"": ["**/*.h5"]},
+)
