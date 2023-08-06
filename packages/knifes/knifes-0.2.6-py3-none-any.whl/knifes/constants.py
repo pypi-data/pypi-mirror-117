c1c9e09c0e784072519365b818018edad1900c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:56 +0000

    Add package knifes/0.2.6/knifes-0.2.6-py3-none-any.whl

---
+++ packages/knifes/knifes-0.2.6-py3-none-any.whl/knifes/constants.py
@@ -0,0 +1,9 @@
+IMAGE = 'image'
+VIDEO = 'video'
+AUDIO = 'audio'
+
+
+APPLE_VERIFY_RECEIPT_PROD = 'https://buy.itunes.apple.com/verifyReceipt'
+APPLE_VERIFY_RECEIPT_SANDBOX = 'https://sandbox.itunes.apple.com/verifyReceipt'
+
+ALIPAY_SERVER_URL = 'https://openapi.alipay.com/gateway.do'
