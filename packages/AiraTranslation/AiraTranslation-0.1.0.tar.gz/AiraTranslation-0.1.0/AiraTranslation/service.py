8605f389e687ea2127d1a3a23c95f1ebf4f7f7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:13 +0000

    Add package AiraTranslation/0.1.0/AiraTranslation-0.1.0.tar.gz

---
+++ packages/AiraTranslation/AiraTranslation-0.1.0.tar.gz/AiraTranslation-0.1.0/AiraTranslation/service.py
@@ -0,0 +1,19 @@
+# import time
+
+
+
+
+
+
+class TranslationService:
+
+    target_url = None
+    parameters = None
+
+    @classmethod
+    def translate(cls, text):
+        translated_text = '***' + text + '###'
+        return translated_text
+
+
+
---
+++ packages/AiraTranslation/AiraTranslation-0.1.0.tar.gz/AiraTranslation-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='AiraTranslation',
+    version='0.1.0',
+    description='A example Python package',
+    author='Badal Singh',
+    author_email='Badal.Singh@evalueserve.com',
+    packages=['AiraTranslation'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
