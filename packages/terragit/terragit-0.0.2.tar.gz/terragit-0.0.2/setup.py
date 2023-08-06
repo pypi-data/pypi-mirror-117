66a2402f1600a19ab4fdc931fbd8e4c32aa32aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:19 +0000

    Add package terragit/0.0.2/terragit-0.0.2.tar.gz

---
+++ packages/terragit/terragit-0.0.2.tar.gz/terragit-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='terragit',
+  version='0.0.2',
+  description='terragit package',
+  long_description=open('README.txt').read(),
+  url='',  
+  author='walid',
+  author_email='walid.mansia@allence-tunisie.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='terragit',
+  packages=find_packages(),
+  install_requires=['']
+)
