6d69159d158304186b516e4794ca52c4b9a4e0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:32 +0000

    Add package iotkaran/0.0.2/iotkaran-0.0.2.tar.gz

---
+++ packages/iotkaran/iotkaran-0.0.2.tar.gz/iotkaran-0.0.2/README.md
@@ -0,0 +1,15 @@
+# Project description
+https://packaging.python.org/tutorials/packaging-projects/
+
+
+> tokenmain
+
+py -m build
+
+py -m twine upload --repository testpypi dist/*
+
+py -m twine upload --repository tpypi dist/*
+__token__
+
+pypi-AgEIcHlwaS5vcmcCJDgwZjdmZTQ5LWE3YTUtNDMwMi1iYWQ0LWY1NDNmZTU5MjcyYwACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgjY7geJqo_d8fgfnltaoWQEGa3P6uQbiVBwWx5f3hvaU
+
---
+++ packages/iotkaran/iotkaran-0.0.2.tar.gz/iotkaran-0.0.2/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iotkaran",
+    version="0.0.2",
+    author="reza behjani",
+    author_email="iotkaran2020@gmail.com",
+    description="library for connect to server iotkaran ",
+    long_description='''# Project description
+this library for connect to server iotkaran and send data to dashboard 
+
+Addres Dashboard :https://dashboard.iotkaran.ir 
+> Automatic Installation
+```
+pip3 install iotkaran
+```
+www.iotkaran.ir
+
+[GitHub](http://github.com/iotkaran/python)
+
+> Example
+
+> Automatic Installation
+```
+>>> from iotkaran import Client
+>>> iotk = Client("server.iotkaran.ir", 2323)
+```''' ,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/iotkaran",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/iotkaran/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
