b0eb3e6a7045a2f7b5747eb02ef946126bc487d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:54 +0000

    Add package djtesting/0.0.1/djtesting-0.0.1.tar.gz

---
+++ packages/djtesting/djtesting-0.0.1.tar.gz/djtesting-0.0.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/25 17:34
+# @Author  : chinablue
+# @Email   : dongjun@reconova.cn
+# @File    : setup.py
+
+import os
+import setuptools
+
+setuptools.setup(
+    name="djtesting",
+    version="0.0.1",
+    author="chinablue",
+    author_email="15099947428@163.com",
+    license="MIT",
+    description="automated interface testing for business",
+    packages=setuptools.find_packages(),
+    platforms=["win10"],
+)
