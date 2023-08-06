7d87f1256fba100a37115417faedf3d6f7319bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:11 +0000

    Add package lor-deckcodes/4.0.0/lor_deckcodes-4.0.0-py3-none-any.whl

---
+++ packages/lor-deckcodes/lor_deckcodes-4.0.0-py3-none-any.whl/lor_deckcodes/constants.py
@@ -0,0 +1,24 @@
+CURRENT_FORMAT_VERSION = 20
+SUPPORTED_VERSIONS = (17, 18, 19, 20)
+faction_mapping = {
+    0: 'DE',
+    1: 'FR',
+    2: 'IO',
+    3: 'NX',
+    4: 'PZ',
+    5: 'SI',
+    6: 'BW',
+    7: 'SH',
+    9: 'MT',
+    10: 'BC',
+    'DE': 0,
+    'FR': 1,
+    'IO': 2,
+    'NX': 3,
+    'PZ': 4,
+    'SI': 5,
+    'BW': 6,
+    'SH': 7,
+    'MT': 9,
+    'BC': 10
+}
