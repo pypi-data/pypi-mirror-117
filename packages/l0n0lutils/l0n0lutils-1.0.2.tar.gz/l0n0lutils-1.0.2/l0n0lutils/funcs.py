c33b67dc2fa05542a267a0a565a78b921a99a122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:28 +0000

    Add package l0n0lutils/1.0.2/l0n0lutils-1.0.2.tar.gz

---
+++ packages/l0n0lutils/l0n0lutils-1.0.2.tar.gz/l0n0lutils-1.0.2/l0n0lutils/funcs.py
@@ -0,0 +1,39 @@
+import asyncio
+import hashlib
+import random
+import string
+
+
+def md5(data):
+    if isinstance(data, str):
+        data = data.encode()
+    return hashlib.md5(data).hexdigest()
+
+
+def random_string(slen=10):
+    letters = string.ascii_letters \
+        + string.digits  \
+        + string.whitespace  \
+        + string.punctuation
+    return ''.join(random.sample(letters, slen))
+
+
+def ok(data: dict):
+    return "OK" == data["errMsg"] \
+        or "Ok" == data["errMsg"] \
+        or "oK" == data["errMsg"] \
+        or "ok" == data["errMsg"]
+
+
+def asyncio_run_forever(loop: asyncio.BaseEventLoop = None):
+    loop = loop or asyncio.get_event_loop()
+    try:
+        print("Press Ctrl+C to Close.")
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for task in asyncio.all_tasks(loop):
+            task.cancel()
+        loop.stop()
+        loop.close()
