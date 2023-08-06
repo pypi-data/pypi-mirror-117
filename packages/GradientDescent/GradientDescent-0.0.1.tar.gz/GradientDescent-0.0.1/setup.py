a3139a0c4843addad54cf5f3b7a64ccfe18d63a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:23 +0000

    Add package GradientDescent/0.0.1/GradientDescent-0.0.1.tar.gz

---
+++ packages/GradientDescent/GradientDescent-0.0.1.tar.gz/GradientDescent-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md','r') as f:
+    long_description = f.read()
+VERSION = '0.0.1'
+DESCRIPTION = 'Makes learning gradient descent easy'
+LONG_DESCRIPTION = 'Makes learning gradient descent easy and makes converting statistical concepts to code easier with transparent usage and flexible control'
+
+# Setting up
+setup(
+    name="GradientDescent",
+    version=VERSION,
+    author="Aayushmaan Jain",
+    author_email="<aayushmaan1306@gmail.com.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy','pandas','sklearn','scipy'],
+    keywords=['python', 'machine', 'learning', 'Machine Learning'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
