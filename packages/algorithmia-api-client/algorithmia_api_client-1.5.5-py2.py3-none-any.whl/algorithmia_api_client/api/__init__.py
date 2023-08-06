65593fa0a24840bcaffa67abbe2d872d1159ba99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:44 +0000

    Add package algorithmia-api-client/1.5.5/algorithmia_api_client-1.5.5-py2.py3-none-any.whl

---
+++ packages/algorithmia-api-client/algorithmia_api_client-1.5.5-py2.py3-none-any.whl/algorithmia_api_client/api/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from algorithmia_api_client.api.admin_api import AdminApi
+from algorithmia_api_client.api.algorithms_api import AlgorithmsApi
+from algorithmia_api_client.api.auth_api import AuthApi
+from algorithmia_api_client.api.builds_api import BuildsApi
+from algorithmia_api_client.api.connections_api import ConnectionsApi
+from algorithmia_api_client.api.connectors_api import ConnectorsApi
+from algorithmia_api_client.api.data_api import DataApi
+from algorithmia_api_client.api.eventlisteners_api import EventlistenersApi
+from algorithmia_api_client.api.frontend_api import FrontendApi
+from algorithmia_api_client.api.invites_api import InvitesApi
+from algorithmia_api_client.api.notifications_api import NotificationsApi
+from algorithmia_api_client.api.organizations_api import OrganizationsApi
+from algorithmia_api_client.api.registries_api import RegistriesApi
+from algorithmia_api_client.api.reservations_api import ReservationsApi
+from algorithmia_api_client.api.scm_api import ScmApi
+from algorithmia_api_client.api.secrets_api import SecretsApi
+from algorithmia_api_client.api.style_api import StyleApi
+from algorithmia_api_client.api.usage_api import UsageApi
+from algorithmia_api_client.api.users_api import UsersApi
