22d0fa35eda281daab2ec49c06e2817f9ee9ba45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:17 +0000

    Add package planpy/0.0.3/planpy-0.0.3.tar.gz

---
+++ packages/planpy/planpy-0.0.3.tar.gz/planpy-0.0.3/planpy/__init__.py
@@ -0,0 +1,66 @@
+
+import weakref
+
+
+
+
+class tools:
+
+
+    """
+
+    Main function class
+
+    """
+
+    def add_start_date(self, start_date):
+        self.start_date = start_date
+        return start_date
+
+
+    def add_end_date(self, end_date):
+        self.end_date = end_date
+        return self.end_date
+
+    def add_budget(self, budget):
+        self.subject = subject
+        return self.subject
+
+    def assigned_to(self, assigned):
+        self.assigned = assigned
+        return self.assigned
+
+    def business_owner(self, business_owner):
+        self.business_owner = business_owner
+        return self.business_owner
+
+
+
+class task(tools):
+    """Main task class of library.
+
+            Inititlise a new project:
+                Methods can then be added off task
+
+    """
+    def __init__(self, name, project, progress):
+        self.name = name
+        self.project = project
+        self.progress = progress
+
+
+
+class project(tools):
+
+    """
+    Main project class of library.
+
+        Initialise a new project:
+            Methods can then be added off the project and visualised.
+
+    """
+    projects = []
+
+    def __init__(self,project_name):
+        self.project_name = project_name
+        self.__class__.projects.append(weakref.proxy(self))
