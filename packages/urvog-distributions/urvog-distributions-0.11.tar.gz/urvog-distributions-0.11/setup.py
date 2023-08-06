81dc26ee834f178e27119823009a5953fb51eef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:48 +0000

    Add package urvog-distributions/0.11/urvog-distributions-0.11.tar.gz

---
+++ packages/urvog-distributions/urvog-distributions-0.11.tar.gz/urvog-distributions-0.11/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='urvog-distributions',
+      version='0.11',
+      description='Gaussian and Binomial distributions',
+      packages=['urvog-distributions'],
+      zip_safe=False)
