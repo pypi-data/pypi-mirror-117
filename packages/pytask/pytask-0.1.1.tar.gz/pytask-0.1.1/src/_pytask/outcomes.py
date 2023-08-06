b06cee4116b6ae5904ac78829d85f6ee95f0d73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:57 +0000

    Add package pytask/0.1.1/pytask-0.1.1.tar.gz

---
+++ packages/pytask/pytask-0.1.1.tar.gz/pytask-0.1.1/src/_pytask/outcomes.py
@@ -0,0 +1,18 @@
+class PytaskOutcome(Exception):
+    """Base outcome of a task."""
+
+
+class Skipped(PytaskOutcome):
+    """Outcome if task is skipped."""
+
+
+class SkippedAncestorFailed(PytaskOutcome):
+    """Outcome if an ancestor failed."""
+
+
+class SkippedUnchanged(PytaskOutcome):
+    """Outcome if task has run before and is unchanged."""
+
+
+class Persisted(PytaskOutcome):
+    """Outcome if task should persist."""
