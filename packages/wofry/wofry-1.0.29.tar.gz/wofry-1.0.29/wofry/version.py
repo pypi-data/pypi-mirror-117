bc7107640b833a709679f38dcea1e350d46bcb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:20 +0000

    Add package wofry/1.0.29/wofry-1.0.29.tar.gz

---
+++ packages/wofry/wofry-1.0.29.tar.gz/wofry-1.0.29/wofry/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofry SETUP.PY
+short_version = '1.0.29'
+version = '1.0.29'
+full_version = '1.0.29'
+git_revision = '085d05516903107a25b73dce024e15f88e721f52'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
