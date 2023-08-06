20434116211d5d933d3063c8e30eff3d9d5a8ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:48 +0000

    Add package superqt/0.2.3/superqt-0.2.3.tar.gz

---
+++ packages/superqt/superqt-0.2.3.tar.gz/superqt-0.2.3/superqt/__init__.py
@@ -0,0 +1,32 @@
+"""superqt is a collection of QtWidgets for python."""
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+from ._eliding_label import QElidingLabel
+from .combobox import QEnumComboBox
+from .sliders import (
+    QDoubleRangeSlider,
+    QDoubleSlider,
+    QLabeledDoubleRangeSlider,
+    QLabeledDoubleSlider,
+    QLabeledRangeSlider,
+    QLabeledSlider,
+    QRangeSlider,
+)
+from .spinbox import QLargeIntSpinBox
+
+__all__ = [
+    "QDoubleRangeSlider",
+    "QDoubleSlider",
+    "QElidingLabel",
+    "QLabeledDoubleRangeSlider",
+    "QLabeledDoubleSlider",
+    "QLabeledRangeSlider",
+    "QLabeledSlider",
+    "QLargeIntSpinBox",
+    "QRangeSlider",
+    "QEnumComboBox",
+]
