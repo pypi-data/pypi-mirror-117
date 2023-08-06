695c51acf4055d435ede172bce67d31ae121e60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:44 +0000

    Add package bws_daemon/0.0.1/bws_daemon-0.0.1-py3-none-any.whl

---
+++ packages/bws_daemon/bws_daemon-0.0.1-py3-none-any.whl/bws_daemon/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Simple Flask Bws Daemons
+
+    :author: Jose Angel Delgado
+    :email: esojangel@gmail.com
+"""
+from __future__ import annotations
+
+# from .daemon import Daemon
+from .manager import *
+from .cli import *
+
+
+__version__ = "0.0.1"
