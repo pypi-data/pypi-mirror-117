57d5a8edb99529a483ecc37c7f9cdc729d15a0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package basic-distributions-udacity-exercise/0.1/basic_distributions_udacity_exercise-0.1.tar.gz

---
+++ packages/basic-distributions-udacity-exercise/basic_distributions_udacity_exercise-0.1.tar.gz/basic_distributions_udacity_exercise-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='basic_distributions_udacity_exercise',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['basic_distributions_udacity_exercise'],
+      author='Jhoann Sebastian Pabon Barona',
+      author_mail='ps.joan3@gmail.com',
+    zip_safe=False)
