5fe10ffd273e608912f8968f835d4792d4dd40a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:42 +0000

    Add package fservice/0.0.4/fservice-0.0.4.tar.gz

---
+++ packages/fservice/fservice-0.0.4.tar.gz/fservice-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='fservice',
+    version='0.0.4',    
+    description='Stateful service management system',
+    url='https://github.com/freefarmdata/fservice',
+    author='Jack Mead',
+    author_email='jackmead515@gmail.com',
+    license='BSD 2-clause',
+    packages=['fservice'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
