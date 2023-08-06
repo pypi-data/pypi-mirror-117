35dddcf76e7a62afb8c8b20ee710a454ef5dc71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:36 +0000

    Add package model-api-wrapper-minh-le/0.0.1/model_api_wrapper_minh_le-0.0.1.tar.gz

---
+++ packages/model-api-wrapper-minh-le/model_api_wrapper_minh_le-0.0.1.tar.gz/model_api_wrapper_minh_le-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="model_api_wrapper_minh_le",
+    version="0.0.1",
+    author="Minh Le",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "./"},
+    packages=setuptools.find_packages(where="./"),
+)
