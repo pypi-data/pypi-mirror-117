174eb35c2f6fd49338ccc650fe64f77230e991ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:29 +0000

    Add package gawain/0.0.5/gawain-0.0.5.tar.gz

---
+++ packages/gawain/gawain-0.0.5.tar.gz/gawain-0.0.5/setup.py
@@ -0,0 +1,38 @@
+import re
+
+from setuptools import setup
+
+
+version = ''
+with open('gawain/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='gawain',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    version='0.0.5',
+    long_description=readme,
+    url='https://github.com/sinkaroid/gawain',
+    packages=['gawain'],
+    license='MIT',
+    description='Asyncio booru wrapper with spell blocker',
+    include_package_data=True,
+    keywords = ['booru', 'wrapper'],
+    install_requires=requirements
+)
