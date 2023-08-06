e9ae1a2383eaec8af89763fac933cead1522f65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:58 +0000

    Add package gfamap/0.2.3/gfamap-0.2.3.tar.gz

---
+++ packages/gfamap/gfamap-0.2.3.tar.gz/gfamap-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.3'
+DESCRIPTION = 'Map edges of a genome graph to a reference genome database.'
+
+setup(
+    name = 'gfamap',
+    version = VERSION,
+    author = 'Joseph Lee',
+    author_email = 'joseph.lee@u.nus.edu',
+    url = 'https://github.com/jleechung/gfamap',
+    description = DESCRIPTION,
+    long_description = DESCRIPTION,
+    license = 'MIT',
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'gfamap = gfamap.gfamap:main'
+            ]
+        },
+    keywords = 'python genome graph genome assembly'
+)
+
