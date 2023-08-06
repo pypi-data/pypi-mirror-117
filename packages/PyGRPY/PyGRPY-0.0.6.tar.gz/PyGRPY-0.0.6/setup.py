645b920d519ee84aeead603ab9616c98773aff5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:28 +0000

    Add package PyGRPY/0.0.6/PyGRPY-0.0.6.tar.gz

---
+++ packages/PyGRPY/PyGRPY-0.0.6.tar.gz/PyGRPY-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='PyGRPY',
+      version='0.0.6',
+      description='Python port of Generalized Rotne Prager Yakamava tensors',
+      url='https://github.com/RadostW/PyGYPY/',
+      author='Radost Waszkiewicz',
+      author_email='radost.waszkiewicz@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!
+      project_urls = {
+          'Documentation': 'https://pygypy.readthedocs.io',
+          'Source': 'https://github.com/RadostW/PyGYPY/'
+      },
+      license='GNU GPLv3',
+      packages=['pygrpy'],
+      zip_safe=False)
