635cfe37f0788fcd503884b2cdb69bf992afe25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:58 +0000

    Add package wai.common/0.0.37/wai.common-0.0.37.tar.gz

---
+++ packages/wai.common/wai.common-0.0.37.tar.gz/wai.common-0.0.37/src/wai/common/serialisation/serialisers/__init__.py
@@ -0,0 +1,10 @@
+from ._BoolSerialiser import BoolSerialiser
+from ._DictSerialiser import DictSerialiser
+from ._FixedLengthStringSerialiser import FixedLengthStringSerialiser
+from ._FloatSerialiser import FloatSerialiser
+from ._IntSerialiser import IntSerialiser
+from ._LengthPrefixedBytesSerialiser import LengthPrefixedBytesSerialiser
+from ._LengthPrefixedStringSerialiser import LengthPrefixedStringSerialiser
+from ._NullTerminatedStringSerialiser import NullTerminatedStringSerialiser
+from ._SecondsSinceEpochSerialiser import SecondsSinceEpochSerialiser
+from ._StringSerialiser import StringSerialiser, NULL_BYTE
