62db7b3f8c3bf5f512cd931fba22f1ee0fb70245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:58 +0000

    Add package seqalign/0.1.5/seqalign-0.1.5-py3-none-any.whl

---
+++ packages/seqalign/seqalign-0.1.5-py3-none-any.whl/seqalign/doc.py
@@ -0,0 +1,22 @@
+#===============================================================================
+# doc.py
+#===============================================================================
+
+# Constants ====================================================================
+
+DOC = """
+There are two commands for aligning sequencing data: `seqalign-chip-seq`
+and `seqalign-atac-seq`. See their respective documentation for more
+information by running:
+
+seqalign-chip-seq --help
+seqalign-atac-seq --help
+"""
+
+
+
+
+# Functions ====================================================================
+
+def main():
+    print(DOC)
