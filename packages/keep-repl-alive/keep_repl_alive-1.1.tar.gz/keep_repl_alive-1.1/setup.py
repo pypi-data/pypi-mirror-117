38f2598ab265998e9b66b536905c21d028fb5866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:27 +0000

    Add package keep-repl-alive/1.1/keep_repl_alive-1.1.tar.gz

---
+++ packages/keep-repl-alive/keep_repl_alive-1.1.tar.gz/keep_repl_alive-1.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+    name='keep_repl_alive',
+    packages=['keep_repl_alive'],
+    version='1.1',
+    license='MIT',
+    description='A simple python module for keeping repls awake',
+    author='Rishit',
+    author_email='vermarishit039@gmail.com',
+    url='https://github.com/IamEinstein/keep_alive',
+    download_url='https://github.com/IamEinstein/keep_alive/archive/v_01.tar.gz',
+    keywords=['replit', 'keep alive', 'flask', 'threading'],
+    install_requires=[
+        'flask',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+
+
+
+
+    ],
+)
