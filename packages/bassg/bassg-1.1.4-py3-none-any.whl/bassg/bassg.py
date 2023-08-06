5c15593c14997f86ee9258e52f3ef3db3d85b662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:41 +0000

    Add package bassg/1.1.4/bassg-1.1.4-py3-none-any.whl

---
+++ packages/bassg/bassg-1.1.4-py3-none-any.whl/bassg/bassg.py
@@ -0,0 +1,28 @@
+import click
+from . import Generator
+
+@click.group()
+def cli():
+    pass
+
+@cli.command('create')
+@click.argument('project_name')
+def create(project_name):
+    '''
+    Create the project and all of its directories
+    '''
+    generator = Generator(project_name)
+    generator.create_directories()
+    
+@cli.command('generate')
+@click.argument('project_name')
+def generate(project_name):
+    '''
+    Generate the site from the files in the project directory
+    '''
+    generator = Generator(project_name)
+    # Load the config.json file here
+    generator.generate_site()
+
+if __name__ == '__main__':
+    cli()
