d5f3415195bad58c264dc7ed9f6c3c5a76ad4ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:31 +0000

    Add package tracking-model/0.1.1/tracking_model-0.1.1.tar.gz

---
+++ packages/tracking-model/tracking_model-0.1.1.tar.gz/tracking_model-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tracking_model']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cvxopt>=1.2.6,<2.0.0',
+ 'matplotlib>=3.4.3,<4.0.0',
+ 'numpy>=1.21.2,<2.0.0',
+ 'pandas>=1.3.2,<2.0.0',
+ 'scikit-learn>=0.24.2,<0.25.0',
+ 'seaborn>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'tracking-model',
+    'version': '0.1.1',
+    'description': 'Genetic Algorithm Optimization for the Index-Tracking problem.',
+    'long_description': None,
+    'author': 'Adriel Martins',
+    'author_email': 'adrielfalcao@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
