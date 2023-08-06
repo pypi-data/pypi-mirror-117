be760f610f22e3c783fe185cdac060752d7eb6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:32 +0000

    Add package lzw-package/1.0.0/lzw_package-1.0.0.tar.gz

---
+++ packages/lzw-package/lzw_package-1.0.0.tar.gz/lzw_package-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='lzw_package',
+    version='1.0.0',
+    description='lzw_test',
+    url='https://pypi.org/',
+    author='lzw',
+    license='MIT'
+)
