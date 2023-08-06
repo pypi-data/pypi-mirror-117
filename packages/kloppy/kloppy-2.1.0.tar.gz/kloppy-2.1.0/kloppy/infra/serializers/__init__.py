27f082a016582616cfa43f85b6be37ecd73105a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:28 +0000

    Add package kloppy/2.1.0/kloppy-2.1.0.tar.gz

---
+++ packages/kloppy/kloppy-2.1.0.tar.gz/kloppy-2.1.0/kloppy/infra/serializers/__init__.py
@@ -0,0 +1,15 @@
+from .event import (
+    MetricaEventsJsonSerializer,
+    OptaSerializer,
+    SportecEventSerializer,
+    StatsBombSerializer,
+    WyscoutSerializer,
+)
+from .tracking import (
+    EPTSSerializer,
+    MetricaTrackingSerializer,
+    TRACABSerializer,
+    TrackingDataSerializer,
+    SkillCornerTrackingSerializer,
+)
+from .code import XMLCodeSerializer
---
+++ packages/kloppy/kloppy-2.1.0.tar.gz/kloppy-2.1.0/kloppy/infra/serializers/tracking/__init__.py
@@ -0,0 +1,5 @@
+from .base import TrackingDataSerializer
+from .epts import EPTSSerializer
+from .metrica import MetricaTrackingSerializer
+from .tracab import TRACABSerializer
+from .skillcorner import SkillCornerTrackingSerializer
