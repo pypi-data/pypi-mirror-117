d5f81c9446644cda67ae844f72bd1dd723e5c2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:42 +0000

    Add package downloader-python/0.0.1rc1/downloader-python-0.0.1rc1.tar.gz

---
+++ packages/downloader-python/downloader-python-0.0.1rc1.tar.gz/downloader-python-0.0.1rc1/README.md
@@ -0,0 +1,6 @@
+#downloader
+this is a downloader that faster than P2P
+
+speed = 3MB/s
+
+
---
+++ packages/downloader-python/downloader-python-0.0.1rc1.tar.gz/downloader-python-0.0.1rc1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='downloader-python',
+    version='0.0.1rc1',
+    url='https://github.com',
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python']
+)
