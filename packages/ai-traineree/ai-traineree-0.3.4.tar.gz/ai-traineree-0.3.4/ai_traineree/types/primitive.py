7e313d24609703b4c758e38d3c723f92222b416f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:49 +0000

    Add package ai-traineree/0.3.4/ai-traineree-0.3.4.tar.gz

---
+++ packages/ai-traineree/ai-traineree-0.3.4.tar.gz/ai-traineree-0.3.4/ai_traineree/types/primitive.py
@@ -0,0 +1,12 @@
+from typing import Dict, List, Sequence, Union
+
+
+Numeric = Union[int, float]
+ObsType = ObservationType = Union[List[int], List[float]]
+StateType = Union[int, List[float]]
+ActionType = Union[int, List]
+DoneType = bool
+RewardType = Union[int, float]
+
+HyperparameterType = Dict[str, str]
+FeatureType = Sequence[int]
