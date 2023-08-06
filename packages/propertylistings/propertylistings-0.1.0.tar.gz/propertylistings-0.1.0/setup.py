34b7486b898070af8c1d77261acdd3aaaee4a59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:04 +0000

    Add package propertylistings/0.1.0/propertylistings-0.1.0.tar.gz

---
+++ packages/propertylistings/propertylistings-0.1.0.tar.gz/propertylistings-0.1.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='propertylistings',
+        version='0.1.0',
+        author='Christopher Christofi',
+        author_email='christopherlchristofi@outlook.com',
+        license='GPL-3.0',
+        description='Webscraping tool for archiving sales records on RightMove.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/christopher-christofi/propertylistings',
+        project_urls={
+            "Bug Tracker": "https://github.com/christopher-christofi/propertylistings/issues",
+        },
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        python_requires=">=3.0",
+        install_requires=[
+            'beautifulsoup4',
+            'requests',
+            'click',
+        ],
+        entry_points={
+            'console_scripts': [
+                'propertylistings = propertylistings.scripts.propertylistings:cli',
+            ],
+        },
+        classifiers=[
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+        ],
+    )
