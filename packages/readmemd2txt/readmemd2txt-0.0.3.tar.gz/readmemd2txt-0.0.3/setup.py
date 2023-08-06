e274fbef6c4ad47f939294f457fa182ad119b343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:26 +0000

    Add package readmemd2txt/0.0.3/readmemd2txt-0.0.3.tar.gz

---
+++ packages/readmemd2txt/readmemd2txt-0.0.3.tar.gz/readmemd2txt-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['readmemd2txt']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['readmemd2txt = readmemd2txt.readmemd2txt:main']}
+
+setup_kwargs = {
+    'name': 'readmemd2txt',
+    'version': '0.0.3',
+    'description': 'Quick and dirty converter: README.md > pure text',
+    'long_description': None,
+    'author': 'suizokukan',
+    'author_email': 'suizokukan@orange.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
