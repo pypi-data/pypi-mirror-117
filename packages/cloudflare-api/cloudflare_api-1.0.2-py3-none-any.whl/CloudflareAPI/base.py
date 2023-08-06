0bdafa6f48d730685f7edc86f455cb1cae265d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:57 +0000

    Add package cloudflare-api/1.0.2/cloudflare_api-1.0.2-py3-none-any.whl

---
+++ packages/cloudflare-api/cloudflare_api-1.0.2-py3-none-any.whl/CloudflareAPI/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+
+class CFBase:
+    def __init__(self) -> None:
+        self.base_url = "https://api.cloudflare.com/client/v4"
+
+    def build_url(self, path: Optional[str] = None) -> str:
+        if path is None:
+            return f"{self.base_url}{self.base_path}"
+        else:
+            if path.startswith("/"):
+                return f"{self.base_url}{self.base_path}{path}"
+            return f"{self.base_url}{self.base_path}/{path}"
