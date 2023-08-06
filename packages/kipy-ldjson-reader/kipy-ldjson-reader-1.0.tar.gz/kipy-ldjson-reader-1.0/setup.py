199149253e06aefbeae9cd4b7b209427e203c323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:02 +0000

    Add package kipy-ldjson-reader/1.0/kipy-ldjson-reader-1.0.tar.gz

---
+++ packages/kipy-ldjson-reader/kipy-ldjson-reader-1.0.tar.gz/kipy-ldjson-reader-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+        name='kipy-ldjson-reader',
+        version='1.0',
+        packages=['kipy_ldjson_reader'],
+        url='https://github.com/peracto/kipy-ldjson-reader',
+        keywords=['ldjson'],
+        license='MIT License',
+        author='Gary Ranson',
+        author_email='',
+        description='Reads ldjson data streams',
+        python_requires='>=3.7',
+        classifiers=[
+                'Programming Language :: Python :: 3.7'
+        ]
+)
