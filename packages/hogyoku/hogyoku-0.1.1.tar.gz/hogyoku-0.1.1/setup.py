bd6b7d199aa2e3a49174f83460bf8c51704e49e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:50 +0000

    Add package hogyoku/0.1.1/hogyoku-0.1.1.tar.gz

---
+++ packages/hogyoku/hogyoku-0.1.1.tar.gz/hogyoku-0.1.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+setup(
+    name='hogyoku',
+    version='v0.1.1',
+    description='hogyoku',
+    py_modules=['hogyoku'],
+    author='chenjianwei',
+    author_email='1572069841@qq.com',
+    url='',
+    license='MIT'
+)
