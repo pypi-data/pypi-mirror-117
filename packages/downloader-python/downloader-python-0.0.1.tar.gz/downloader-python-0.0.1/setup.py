ec4eba178fff0344d62f1df1d8c281e3b55fa5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package downloader-python/0.0.1/downloader-python-0.0.1.tar.gz

---
+++ packages/downloader-python/downloader-python-0.0.1.tar.gz/downloader-python-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='downloader-python',
+    version='0.0.1',
+    url='https://github.com',
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python'],
+    py_moudles=['PyQt5','requests']
+)
