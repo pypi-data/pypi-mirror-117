20fb43fb96cf43fd797904d2bcdbc06b84109698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:58 +0000

    Add package nimblephysics/0.5.4/nimblephysics-0.5.4-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/nimblephysics/nimblephysics-0.5.4-cp37-cp37m-macosx_10_14_x86_64.whl/nimblephysics/__init__.py
@@ -0,0 +1,9 @@
+from nimblephysics_libs._nimblephysics import *
+from .timestep import timestep
+from .native_trajectory_support import *
+from .gui_server import NimbleGUI
+from .mapping import map_to_pos, map_to_vel
+from .loader import loadWorld
+from .models import *
+
+__doc__ = "Python bindings from Nimble"
