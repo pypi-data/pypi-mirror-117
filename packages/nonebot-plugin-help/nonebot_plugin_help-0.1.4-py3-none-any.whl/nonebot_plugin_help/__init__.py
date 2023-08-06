15d882bedb93f73abc17ee02765bd93d5d6f6465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:31 +0000

    Add package nonebot-plugin-help/0.1.4/nonebot_plugin_help-0.1.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-help/nonebot_plugin_help-0.1.4-py3-none-any.whl/nonebot_plugin_help/__init__.py
@@ -0,0 +1,31 @@
+"""
+Nonebot 2 Help Plugin
+Author: XZhouQD
+Since: 16 May 2021
+"""
+from pathlib import Path
+import nonebot
+
+from .handler import helper
+
+
+default_start = list(nonebot.get_driver().config.command_start)[0]
+
+# store all subplugins
+_sub_plugins = set()
+# load sub plugins
+_sub_plugins |= nonebot.load_plugins(
+    str((Path(__file__).parent / "plugins").resolve()))
+
+__usage__ = f'''欢迎使用Nonebot 2 Help Plugin
+支持使用的前缀：{" ".join(list(nonebot.get_driver().config.command_start))}
+{default_start}help  # 获取本插件帮助
+{default_start}help list  # 展示已加载插件列表
+{default_start}help <plugin_name>  # 调取目标插件帮助信息
+'''
+
+__version__ = '0.1.4'
+
+__help_plugin_name__ = "XZhouQD's Help Menu"
+
+nonebot.export.help = helper
