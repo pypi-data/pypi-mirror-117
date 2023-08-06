274de9a97a3e155bec123793db552ad6e4b13b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:27 +0000

    Add package metest/0.0.1/metest-0.0.1-py3-none-any.whl

---
+++ packages/metest/metest-0.0.1-py3-none-any.whl/metest/datametric.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Master module for metest.datametric
+"""
+
+class datametric:
+
+    def __init__(self) -> None:
+        """Init function for datametric
+        """
+        return
