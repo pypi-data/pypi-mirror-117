06b135beb1030e046fead08b8013ba4d5dc4bfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:16 +0000

    Add package plan-visualiser/0.0.22/plan_visualiser-0.0.22.tar.gz

---
+++ packages/plan-visualiser/plan_visualiser-0.0.22.tar.gz/plan_visualiser-0.0.22/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.0.22'
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'python-pptx',
+      'dataclasses',
+      'colour',
+      'ddt==1.4.2',
+      'openpyxl',
+      ]
+
+setup(
+      version=VERSION,
+      install_requires=INSTALL_REQUIRES,
+      )
