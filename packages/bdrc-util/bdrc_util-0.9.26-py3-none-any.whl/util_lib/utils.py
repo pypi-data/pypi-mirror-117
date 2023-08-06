aaa035ee5c6697f190f84fd03a2f5177d640b010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:14 +0000

    Add package bdrc-util/0.9.26/bdrc_util-0.9.26-py3-none-any.whl

---
+++ packages/bdrc-util/bdrc_util-0.9.26-py3-none-any.whl/util_lib/utils.py
@@ -0,0 +1,17 @@
+"""
+utilities shared by bdrc_utils
+"""
+import argparse
+from pathlib import Path
+from typing import AnyStr
+
+
+def reallypath(what_path: AnyStr) -> Path:
+    """
+    Resolves everything about the path
+    :param what_path: Pathlike object
+    :return: fully resolved path
+    """
+    from os import path
+    return path.realpath(path.expandvars(path.expanduser(what_path)))
+
