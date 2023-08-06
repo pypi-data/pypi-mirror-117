664c15e32ba6adeed17a153e4fb9f7381b16cdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:47 +0000

    Add package jsonrpcclient/4.0.0a1/jsonrpcclient-4.0.0a1.tar.gz

---
+++ packages/jsonrpcclient/jsonrpcclient-4.0.0a1.tar.gz/jsonrpcclient-4.0.0a1/README.md
@@ -0,0 +1,30 @@
+> :warning: This master branch is for the upcoming version 4,
+> currently [in development](https://github.com/explodinglabs/jsonrpcclient/discussions/176) (read: [The future of this
+> library](https://github.com/explodinglabs/jsonrpcclient/discussions/171)).
+> For the latest stable release, see the [3.x
+> branch](https://github.com/explodinglabs/jsonrpcclient/tree/3.x). Also,
+> please pin your dependency to "jsonrpcclient<4" until you're ready to upgrade
+> to v4.
+
+# jsonrpcclient
+
+Generate JSON-RPC requests and parse responses.
+
+![PyPI](https://img.shields.io/pypi/v/jsonrpcclient.svg)
+![Downloads](https://pepy.tech/badge/jsonrpcclient/week)
+![Checks](https://github.com/explodinglabs/jsonrpcclient/actions/workflows/checks.yml/badge.svg)
+![Coverage Status](https://coveralls.io/repos/github/explodinglabs/jsonrpcclient/badge.svg?branch=master)
+
+```sh
+pip install --pre jsonrpcclient
+```
+
+```python
+>>> from jsonrpcclient import request
+>>> request("get")
+'{"jsonrpc": "2.0", "method": "get", "id": 1}'
+```
+
+Full documentation is at [jsonrpcclient.com](https://www.jsonrpcclient.com/en/latest/).
+
+See also: [jsonrpcserver](https://www.jsonrpcserver.com/)
---
+++ packages/jsonrpcclient/jsonrpcclient-4.0.0a1.tar.gz/jsonrpcclient-4.0.0a1/jsonrpcclient/requests.py
@@ -0,0 +1,63 @@
+import json
+from functools import partial
+from typing import Any, Dict, Iterator, List, Union
+
+from . import id_generators
+from .sentinels import NOID
+from .utils import compose
+
+
+def notification_dict_pure(
+    method: str, params: Union[Dict[str, Any], List[Any]]
+) -> Dict[str, Any]:
+    return {
+        "jsonrpc": "2.0",
+        "method": method,
+        **({"params": params} if params else {}),
+    }
+
+
+def notification_dict(
+    method: str, params: Union[Dict[str, Any], List[Any], None] = None
+) -> Dict[str, Any]:
+    return notification_dict_pure(method, params if params else [])
+
+
+notification = compose(json.dumps, notification_dict)
+
+
+def request_pure(
+    id_generator: Iterator[Any],
+    method: str,
+    params: Union[Dict[str, Any], List[Any]],
+    id: Any,
+) -> Dict[str, Any]:
+    return {
+        "jsonrpc": "2.0",
+        "method": method,
+        **({"params": params} if params else {}),
+        "id": id if id is not NOID else next(id_generator),
+    }
+
+
+def request_impure(
+    id_generator: Iterator[Any],
+    method: str,
+    params: Union[Dict[str, Any], List[Any], None] = None,
+    id: Any = NOID,
+) -> Dict[str, Any]:
+    return request_pure(
+        id_generator or id_generators.decimal(), method, params or [], id
+    )
+
+
+request_dict_natural = partial(request_impure, id_generators.decimal())
+request_dict_hexadecimal = partial(request_impure, id_generators.hexadecimal())
+request_dict_random = partial(request_impure, id_generators.random())
+request_dict_uuid = partial(request_impure, id_generators.uuid())
+
+request_dict = request_dict_natural
+request = compose(json.dumps, request_dict_natural)
+request_hex = compose(json.dumps, request_dict_hexadecimal)
+request_random = compose(json.dumps, request_dict_random)
+request_uuid = compose(json.dumps, request_dict_uuid)
