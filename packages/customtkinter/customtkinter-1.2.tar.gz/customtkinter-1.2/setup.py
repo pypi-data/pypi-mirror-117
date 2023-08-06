1c220f1978053d615e29e5010306ecc5afbe7cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:34 +0000

    Add package customtkinter/1.2/customtkinter-1.2.tar.gz

---
+++ packages/customtkinter/customtkinter-1.2.tar.gz/customtkinter-1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+
+# Update in pypi:
+#
+# 1. python3 -m pip install --upgrade build
+# 2.
+
+
+def read(filename):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), filename)).read()
+    except Exception as err:
+        return ""
+
+
+setup(name="customtkinter",
+      version="1.2",
+      author="Tom Schimansky",
+      license="Creative Commons Zero v1.0 Universal",
+      url="https://github.com/TomSchimansky/CustomTkinter",
+      description="Create modern looking gui with tkinter and python",
+      long_description_content_type="text/markdown",
+      long_description=read('Readme_pypi.md'),
+      packages=["customtkinter"],
+      classifiers=["Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3",
+                   "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"],
+      install_requires=["darkdetect"])
