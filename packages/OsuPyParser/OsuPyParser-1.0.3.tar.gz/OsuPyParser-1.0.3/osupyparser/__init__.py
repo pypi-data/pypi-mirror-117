09e0e8ca9f66c31d2bb66fcf25d25359790d9fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:57 +0000

    Add package OsuPyParser/1.0.3/OsuPyParser-1.0.3.tar.gz

---
+++ packages/OsuPyParser/OsuPyParser-1.0.3.tar.gz/OsuPyParser-1.0.3/osupyparser/__init__.py
@@ -0,0 +1,7 @@
+from .osu.osu_parser import OsuFile
+from .osr.osr_parser import ReplayFile
+from .osr.constants import Key, KeyMania, KeyTaiko, OsuReplayFrame, TaikoReplayFrame, ManiaReplayFrame, CatchReplayFrame
+from .osu.objects import Circle
+from .osu.objects import Spinner
+from .osu.objects import Slider
+from .osu.objects import HitObject
