e97b7f07f35b3d243e5d3835f1f9b5b0e28a096e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:40 +0000

    Add package nevergrad/0.4.3.post8/nevergrad-0.4.3.post8.tar.gz

---
+++ packages/nevergrad/nevergrad-0.4.3.post8.tar.gz/nevergrad-0.4.3.post8/nevergrad/optimization/families.py
@@ -0,0 +1,46 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""Parametrizable families of optimizers.
+
+Caution
+-------
+This module and its available classes are experimental and may change quickly in the near future.
+"""
+from .optimizerlib import ParametrizedOnePlusOne
+from .optimizerlib import EMNA
+from .optimizerlib import ParametrizedTBPSA
+from .optimizerlib import ParametrizedBO
+from .optimizerlib import ParametrizedCMA
+from .optimizerlib import Chaining
+from .optimizerlib import NoisySplit
+from .optimizerlib import ConfSplitOptimizer
+from .optimizerlib import ConfPortfolio
+from .optimizerlib import BayesOptim
+from .differentialevolution import DifferentialEvolution
+from .es import EvolutionStrategy
+from .recastlib import ScipyOptimizer
+from .recastlib import Pymoo
+from .oneshot import RandomSearchMaker
+from .oneshot import SamplingSearch
+
+
+__all__ = [
+    "ParametrizedOnePlusOne",
+    "ParametrizedCMA",
+    "ParametrizedBO",
+    "DifferentialEvolution",
+    "EvolutionStrategy",
+    "ScipyOptimizer",
+    "Pymoo",
+    "RandomSearchMaker",
+    "SamplingSearch",
+    "Chaining",
+    "EMNA",
+    "ParametrizedTBPSA",
+    "NoisySplit",
+    "ConfSplitOptimizer",
+    "ConfPortfolio",
+    "BayesOptim",
+]
