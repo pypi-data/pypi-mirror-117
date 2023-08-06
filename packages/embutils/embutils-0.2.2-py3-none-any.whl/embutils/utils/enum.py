0610b598fe1fc1f64d1e11bdfa13ec490cc8faac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package embutils/0.2.2/embutils-0.2.2-py3-none-any.whl

---
+++ packages/embutils/embutils-0.2.2-py3-none-any.whl/embutils/utils/enum.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: ascii -*-
+"""
+Enumeration utilities.
+
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
+"""
+
+from enum import IntEnum
+
+
+# -->> Definitions <<------------------
+
+
+# -->> API <<--------------------------
+class IntEnumMod(IntEnum):
+    """
+    Modified integer enumeration class. This class extends the functionalities
+    of the base enumeration definition.
+    """
+    def __repr__(self) -> str:
+        """
+        Representation string.
+
+        :returns: Representation string.
+        :rtype: str
+        """
+        return f'{self.__class__.__name__}({self.__str__()})'
+
+    def __str__(self) -> str:
+        """
+        Class object as string.
+
+        :returns: Object value string.
+        :rtype: str
+        """
+        return f'{self.name}(0x{self.value:X})'
+
+    @classmethod
+    def from_int(cls, value: int) -> 'IntEnumMod':
+        """
+        Converts, if possible, the input value to an enum object.
+
+        :param int value: Value to be converted.
+
+        :returns: Enumeration object.
+        :rtype: IntEnumMod
+
+        :raises ValueError: Value is not contained on the enum.
+        """
+        if cls.has_value(value=value):
+            return cls(value)
+        raise ValueError(f'Value 0x{value:02X} is not defined.')
+
+    @classmethod
+    def has_value(cls, value: int) -> bool:
+        """
+        Checks if the input value exist on the enum definition.
+
+        :param int value: Value to be checked.
+
+        :returns: True if exists, false otherwise.
+        :rtype: bool
+        """
+        return value in cls._value2member_map_
---
+++ packages/embutils/embutils-0.2.2-py3-none-any.whl/embutils/utils/serialized.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: ascii -*-
+"""
+Serialized object definition.
+
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
+"""
+
+from abc import abstractmethod
+from typing import Union
+
+
+# -->> Definitions <<------------------
+
+
+# -->> API <<--------------------------
+class Serialized:
+    """
+    Abstract implementation for a serialized item.
+    """
+    #:  Serialized object byte length. If the length is variable, this defines the minimum.
+    _LENGTH = 0
+
+    @property
+    def length(self) -> int:
+        """
+        Serialized item length in bytes. This property allows to handle the cases
+        in which the object has variable size and the attribute :attr:`LENGTH` only
+        defines the minimum length.
+        """
+        return self._LENGTH
+
+    @abstractmethod
+    def serialize(self) -> bytearray:
+        """
+        Serializes the object to a byte array.
+
+        :returns: Serialized object.
+        :rtype: bytearray
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def deserialize(data: bytearray) -> Union[None, 'Serialized']:
+        """
+        Deserializes the object from a byte array.
+
+        :param bytearray data: Bytes to be deserialized.
+
+        :returns: None if deserialization fail, deserialized object otherwise.
+        :rtype: Serialized
+        """
+        pass
