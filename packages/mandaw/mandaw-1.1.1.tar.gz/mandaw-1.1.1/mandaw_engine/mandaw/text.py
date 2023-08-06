409f0bd7f954389af3ce2f36c1cddb7133f31d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:31 +0000

    Add package mandaw/1.1.1/mandaw-1.1.1.tar.gz

---
+++ packages/mandaw/mandaw-1.1.1.tar.gz/mandaw-1.1.1/mandaw_engine/mandaw/text.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Text:
+    def __init__(self, window, text, font_size = 24, file = None, color = "white", x = 0, y = 0):
+        self.window = window
+        self.font_size = font_size
+        self.file = file
+        self.font = pygame.font.Font(self.file, self.font_size)
+        self.color = color
+        self.x = x
+        self.y = y
+
+        self.text = self.font.render(text, True, self.color)
+
+    def draw(self):
+        self.window.window.blit(self.text, (self.x, self.y))
+
+    def center(self):
+        self.x = self.window.width / 2 - self.font_size / 2
+        self.y = self.window.height / 2 - self.font_size / 2
+
+if __name__ == '__main__':
+    from mandaw import *
+
+    mandaw = Mandaw("Mandaw", bg_color="cyan")
+
+    text = Text(mandaw, "Mandaw", 50)
+    text.center()
+
+    while True:
+        text.draw()
+        mandaw.run()
