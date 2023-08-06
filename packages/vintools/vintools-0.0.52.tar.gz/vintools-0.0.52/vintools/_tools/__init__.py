070a43b186102844f69ff51d6ee8121746963c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:55 +0000

    Add package vintools/0.0.52/vintools-0.0.52.tar.gz

---
+++ packages/vintools/vintools-0.0.52.tar.gz/vintools-0.0.52/vintools/_tools/__init__.py
@@ -0,0 +1,12 @@
+
+# tools __init__.py
+
+__author__ = ', '.join([
+    'Michael E. Vinyard'
+])
+__email__ = ', '.join([
+    'vinyard@g.harvard.edu',
+])
+
+
+from ._cDNA_detector._cDNA_detector import _cDNA_detector as cDNA_detector
