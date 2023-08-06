24226b3fdd6aad3704eddce28811ebc088b6e28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:19 +0000

    Add package falcon-ratelimit/1.2/falcon-ratelimit-1.2.tar.gz

---
+++ packages/falcon-ratelimit/falcon-ratelimit-1.2.tar.gz/falcon-ratelimit-1.2/setup.py
@@ -0,0 +1,18 @@
+# coding=UTF-8
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fin:
+    long_description = '\n'.join(
+        x for x in fin.readlines() if not x.startswith('[!'))
+
+setup(name='falcon-ratelimit',
+      version='1.2',
+      packages=find_packages(include=['falconratelimit', 'falconratelimit.*']),
+      install_requires=["falcon", "six"],
+      tests_require=[
+          "nose", "mock", "coverage", "freezegun"
+      ],
+      test_suite='nose.collector',
+      long_description=long_description,
+      description='A rate limiter plugin for Falcon'
+      )
