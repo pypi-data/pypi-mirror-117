dade93244605c66e9d8dc0035cb8d8a8aff0eb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:18 +0000

    Add package sgraphic/0.0.2/sgraphic-0.0.2.tar.gz

---
+++ packages/sgraphic/sgraphic-0.0.2.tar.gz/sgraphic-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "sgraphic",
+    version = "0.0.2",
+    author = "Rene Czepluch Thomsen",
+    author_email = "sepluk1@gmail.com",
+    description = ("Fast and simple graphics. "),
+    url="https://github.com/ReneTC/Simple-graphics",
+    license = "BSD",
+    keywords = "graphics package 2d",
+    packages=['sgraphic'],
+)
