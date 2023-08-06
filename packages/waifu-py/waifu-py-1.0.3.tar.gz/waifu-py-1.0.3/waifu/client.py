301b4cd5fab43b1be95695df99ef4c8a9785bc66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:43 +0000

    Add package waifu-py/1.0.3/waifu-py-1.0.3.tar.gz

---
+++ packages/waifu-py/waifu-py-1.0.3.tar.gz/waifu-py-1.0.3/waifu/client.py
@@ -0,0 +1,139 @@
+"""
+MIT License
+
+Copyright (c) 2021 IchBinLeoon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+from typing import Optional, Union, Dict, List
+
+import requests
+
+from waifu.exceptions import APIException, InvalidCategory
+from waifu.utils import BASE_URL, ImageCategories, ImageTypes
+
+log = logging.getLogger(__name__)
+
+
+class WaifuClient:
+    """Wrapper client for the waifu.pics API.
+
+    This class is used to interact with the API.
+
+    Attributes:
+        session: A requests session.
+    """
+
+    def __init__(self, session: Optional[requests.Session] = None) -> None:
+        """Initializes the WaifuClient.
+
+        Args:
+            session: A requests session.
+        """
+        self.session = session
+
+    def _session(self) -> requests.Session:
+        """Gets a requests session by creating it if it does not already exist."""
+        if self.session is None:
+            self.session = requests.Session()
+        return self.session
+
+    def _request(self, url: str, method: str, *args, **kwargs) -> Dict[str, str]:
+        """Performs an HTTP request."""
+        session = self._session()
+        response = getattr(session, method)(url, *args, **kwargs)
+        log.debug(f'{method.upper()} {url} {response.status_code} {response.reason}')
+        if response.status_code != 200:
+            raise APIException(response.status_code, response.reason, (response.json()).get('message'))
+        data = response.json()
+        return data
+
+    def _get(self, url: str, *args, **kwargs) -> _request:
+        """Performs an HTTP GET request."""
+        return self._request(url, 'get', *args, **kwargs)
+
+    def _post(self, url: str, *args, **kwargs) -> _request:
+        """Performs an HTTP POST request."""
+        return self._request(url, 'post', *args, **kwargs)
+
+    def _fetch(
+        self,
+        type_: str,
+        category: str,
+        many: bool,
+        exclude: List[str]
+    ) -> Union[str, List[str]]:
+        """Returns a single or 30 unique images of the specific type and category."""
+        if category not in ImageCategories[type_]:
+            raise InvalidCategory(category)
+        if many is True:
+            data = self._post(f'{BASE_URL}/many/{type_}/{category}', json={'exclude': exclude})
+        else:
+            data = self._get(f'{BASE_URL}/{type_}/{category}')
+        if many is True:
+            return data.get('files')
+        return data.get('url')
+
+    def sfw(
+        self,
+        category: str,
+        many: Optional[bool] = False,
+        exclude: Optional[List[str]] = None
+    ) -> Union[str, List[str]]:
+        """Gets a single or 30 unique SFW (Safe For Work) images of the specific category.
+
+        Args:
+            category: The category of the image.
+            many: Get 30 unique images instead of one if true.
+            exclude: A list of URL's to not receive from the endpoint if many is true.
+
+        Returns:
+            A single or 30 unique image URL's.
+
+        Raises:
+            APIException: If the API response contains an error.
+            InvalidCategory: If the category is invalid.
+        """
+        data = self._fetch(ImageTypes.sfw, category, many, exclude)
+        return data
+
+    def nsfw(
+        self,
+        category: str,
+        many: Optional[bool] = False,
+        exclude: Optional[List[str]] = None
+    ) -> Union[str, List[str]]:
+        """Gets a single or 30 unique NSFW (Not Safe For Work) images of the specific category.
+
+        Args:
+            category: The category of the image.
+            many: Get 30 unique images instead of one if true.
+            exclude: A list of URL's to not receive from the endpoint if many is true.
+
+        Returns:
+            A single or 30 unique image URL's.
+
+        Raises:
+            APIException: If the API response contains an error.
+            InvalidCategory: If the category is invalid.
+        """
+        data = self._fetch(ImageTypes.nsfw, category, many, exclude)
+        return data
