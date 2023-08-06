baf04d44bfcbfc4f5ff1fbce864994d061f7766e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:44 +0000

    Add package roman-discovery/0.3.2/roman_discovery-0.3.2.tar.gz

---
+++ packages/roman-discovery/roman_discovery-0.3.2.tar.gz/roman_discovery-0.3.2/roman_discovery/__init__.py
@@ -0,0 +1,13 @@
+__author__ = """Roman Imankulov"""
+__email__ = "roman.imankulov@gmail.com"
+__version__ = "0.3.2"
+
+
+from roman_discovery.discovery import IRule, ModuleRule, ObjectRule, discover
+
+__all__ = (
+    "discover",
+    "IRule",
+    "ModuleRule",
+    "ObjectRule",
+)
