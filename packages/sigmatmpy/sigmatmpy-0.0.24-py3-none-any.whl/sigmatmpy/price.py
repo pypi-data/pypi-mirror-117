cba5376e82ea7f3482ad9e66f9555c004afd1f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:53 +0000

    Add package sigmatmpy/0.0.24/sigmatmpy-0.0.24-py3-none-any.whl

---
+++ packages/sigmatmpy/sigmatmpy-0.0.24-py3-none-any.whl/sigmatmpy/price.py
@@ -0,0 +1,27 @@
+name = "sigmatmpy"
+import requests
+import json
+
+
+
+class price(object):
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.refresh_token(username,password)
+
+    def refresh_token(self,username,password):
+
+        login_url = 'https://api-pk-data.sigmatm.com.au/api/v1/auth/login'
+        token = requests.post(login_url, json = {
+            "email": username,
+            "password": password
+        })
+        self.token = token.json()['access_token']
+
+    
+    def get_alert_data(self,alert):
+
+        link = f'https://api-pk-data.sigmatm.com.au/api/v1/alert/data?broker_id=2&alert={alert}'
+        response = requests.get(link , headers={'Authorization': f'Bearer {self.token}'})
+        return response.json()
