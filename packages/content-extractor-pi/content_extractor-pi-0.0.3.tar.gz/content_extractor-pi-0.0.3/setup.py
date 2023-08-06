13a696f07a9ab53bcb5498cd88b19badf772c8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:10 +0000

    Add package content-extractor-pi/0.0.3/content_extractor-pi-0.0.3.tar.gz

---
+++ packages/content-extractor-pi/content_extractor-pi-0.0.3.tar.gz/content_extractor-pi-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='content_extractor-pi',
+    version='0.0.3',
+    license="MIT",
+    author='Paolo Italiani',
+    author_email='paoita@hotmail.it',
+    description='Content extractor for files containing text',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.com/qarik/data-science/gcp_docunderstanding_spike/-/tree/content_extractor',
+    packages=['content_extractor'],
+    install_requires=REQUIREMENTS,
+)
