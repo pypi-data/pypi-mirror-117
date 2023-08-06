99feb5598b5cf685e3f741e546b3038a30d1b4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:46 +0000

    Add package offsets/1.37.9.8/offsets-1.37.9.8.tar.gz

---
+++ packages/offsets/offsets-1.37.9.8.tar.gz/offsets-1.37.9.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='offsets',
+      version='1.37.9.8',
+      description='do you want to get all csgo offsets? Ok, use class csgo, enjoy :)',
+      packages=['offsets'],
+      author_email='adamisaev9234@gmail.com',
+      zip_safe=False)
