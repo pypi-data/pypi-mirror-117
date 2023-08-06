f7db0c02a2feee7eb98417197cd41bb4f26ac6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:55 +0000

    Add package boringstonks/0.0.12/boringstonks-0.0.12.tar.gz

---
+++ packages/boringstonks/boringstonks-0.0.12.tar.gz/boringstonks-0.0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.12' 
+DESCRIPTION = 'API client for boring stonks'
+
+# Setting up
+setup(
+        name="boringstonks", 
+        version=VERSION,
+        author="Kevin Per",
+        author_email="kevin.per@protonmail.com",
+        description=DESCRIPTION,
+        packages=["boringstonks"],
+        install_requires=['pandas', 'requests', 'flatten_json'],
+        keywords=['python'],
+)
