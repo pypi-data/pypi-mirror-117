96a8940c8a71713e642d687609de3fdcdb980927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:57 +0000

    Add package minos-apigateway/0.0.4/minos_apigateway-0.0.4.tar.gz

---
+++ packages/minos-apigateway/minos_apigateway-0.0.4.tar.gz/minos_apigateway-0.0.4/HISTORY.md
@@ -0,0 +1,19 @@
+# History
+
+## 0.0.1 (2021-06-14)
+
+* First release on PyPI.
+
+## 0.0.2 (2021-06-18)
+
+* Bugfix.
+
+## 0.0.3 (2021-08-19)
+
+* Support auto discoverable endpoints.
+* Handle requests to unavailable microservices.
+* Improve request forwarding to microservices.
+* Fix bug related with unnecessary `json` casting.
+
+## 0.0.4 (2021-08-54)
+* Add support to `CORS`.
---
+++ packages/minos-apigateway/minos_apigateway-0.0.4.tar.gz/minos_apigateway-0.0.4/minos/api_gateway/rest/service.py
@@ -0,0 +1,41 @@
+"""minos.api_gateway.rest.service module."""
+
+import logging
+
+from aiohttp import (
+    web,
+)
+from aiohttp_middlewares import (
+    cors_middleware,
+)
+from aiomisc.service.aiohttp import (
+    AIOHTTPService,
+)
+
+from minos.api_gateway.common import (
+    MinosConfig,
+)
+from minos.api_gateway.rest import (
+    handler,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ApiGatewayRestService(AIOHTTPService):
+    def __init__(self, address: str, port: int, config: MinosConfig):
+        self.config = config
+        super().__init__(address, port)
+
+    async def create_application(self) -> web.Application:
+        middlewares = list()
+        if self.config.cors.enabled:
+            middlewares = [cors_middleware(allow_all=True)]
+
+        app = web.Application(middlewares=middlewares)
+
+        app["config"] = self.config
+
+        app.router.add_route("*", "/{endpoint:.*}", handler.orchestrate)
+
+        return app
