e263b41ee0954ea9933f8ffa43cecb2f832dc7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:17 +0000

    Add package pytest-runtime-xfail/1.0.1/pytest_runtime_xfail-1.0.1-py3-none-any.whl

---
+++ packages/pytest-runtime-xfail/pytest_runtime_xfail-1.0.1-py3-none-any.whl/pytest_runtime_xfail.py
@@ -0,0 +1,18 @@
+"""
+pytest plugin, providing a `runtime_xfail` fixture,
+which is callable as `runtime_xfail()`,
+to allow runtime decisions to mark a test as `xfail`.
+"""
+import pytest
+
+__version__ = "1.0.1"
+
+
+@pytest.fixture()
+def runtime_xfail(request):
+    """
+    Call runtime_xfail() to mark running test as xfail.
+    """
+    def _xfail(reason=''):
+        request.node.add_marker(pytest.mark.xfail(reason=reason))
+    return _xfail
