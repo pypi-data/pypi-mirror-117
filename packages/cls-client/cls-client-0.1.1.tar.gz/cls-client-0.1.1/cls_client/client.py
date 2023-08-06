d689509b99e9ae7d24b026e0b34c33ddfdbcec20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:14 +0000

    Add package cls-client/0.1.1/cls-client-0.1.1.tar.gz

---
+++ packages/cls-client/cls-client-0.1.1.tar.gz/cls-client-0.1.1/cls_client/client.py
@@ -0,0 +1,80 @@
+import os
+import functools
+
+from .shared import track_event, dispatch_events
+from .shared import logger
+
+
+def track_function(
+    name="",
+    type="",
+    include_args=[],
+    include_kwargs=[],
+    include_return_value=False,
+    include_env=[],
+    metadata={},
+    dispatch=False,
+):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            raised_exception = None
+
+            event_metadata = metadata.copy()
+
+            # Include specific args
+            if include_args:
+                event_metadata["args"] = []
+                for arg_index in include_args:
+                    try:
+                        event_metadata["args"][arg_index] = args[arg_index]
+                    except IndexError as e:
+                        logger.error(
+                            f"Could not include arg {arg_index} in event metadata: {e}"
+                        )
+
+            # Include specific kwargs
+            if include_kwargs:
+                event_metadata["kwargs"] = {}
+                for kwarg_name in include_kwargs:
+                    try:
+                        event_metadata["kwargs"][kwarg_name] = kwargs[kwarg_name]
+                    except KeyError as e:
+                        logger.error(
+                            f"Could not include kwarg {kwarg_name} in event metadata: {e}"
+                        )
+
+            # Inlcude specific environment variables
+            if include_env:
+                event_metadata["env"] = {}
+                for env_name in include_env:
+                    event_metadata["env"][env_name] = os.environ.get(env_name)
+
+            try:
+                return_value = func(*args, **kwargs)
+            except Exception as e:
+                raised_exception = e
+                return_value = "Exception"
+
+            if include_return_value:
+                event_metadata["return_value"] = return_value
+
+            slug = name or func.__name__
+
+            track_event(slug=slug, type=type, metadata=event_metadata)
+
+            if dispatch:
+                dispatch_events()
+
+            if raised_exception:
+                raise raised_exception
+
+            return return_value
+
+        return wrapper
+
+    return decorator
+
+
+def track_command(*args, **kwargs):
+    return track_function(type="command", dispatch=True, *args, **kwargs)
