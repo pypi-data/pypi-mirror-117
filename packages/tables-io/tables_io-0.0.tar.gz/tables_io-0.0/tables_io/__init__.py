c847d0af4c42014fae63fc8eeaa97a60a2f51ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:12 +0000

    Add package tables-io/0.0/tables_io-0.0.tar.gz

---
+++ packages/tables-io/tables_io-0.0.tar.gz/tables_io-0.0/tables_io/__init__.py
@@ -0,0 +1,31 @@
+"""tables_io is a library of functions for input, output and conversion of tabular data formats"""
+
+from ._version import version
+
+from .lazy_modules import *
+
+from . import types
+
+from . import arrayUtils
+
+from . import convUtils as conv
+
+from . import ioUtils as io
+
+from .tableDict import TableDict
+
+convertObj = conv.convertObj
+
+convert = conv.convert
+
+writeNative = io.writeNative
+
+write = io.write
+
+readNative = io.readNative
+
+read = io.read
+
+iterateNative = io.iterate
+
+iterate = io.iterate
