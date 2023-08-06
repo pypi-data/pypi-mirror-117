9c0f7441ba0cc1266111ee6e42ebe89c7076f37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:59 +0000

    Add package liesym/0.5.5/liesym-0.5.5-cp38-cp38-win_amd64.whl

---
+++ packages/liesym/liesym-0.5.5-cp38-cp38-win_amd64.whl/liesym/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+# C++ backend for sympy. Symbolic speed ups
+os.environ["USE_SYMENGINE"] = "1"
+
+# Compiled extension imports
+from .liesym import (
+    debug_mode as _debug_mode,
+    LieAlgebraBackend as _LieAlgebraBackend,
+)
+
+from .algebras import *
+from .groups import *
+
+__all__ = [
+    "A", "B", "C", "D", "F4", "G2", "E", "LieAlgebra", "NumericSymbol",
+    "SU", "generalized_gell_mann", "SO", "Sp", "Group", "LieGroup", "Z"
+]
