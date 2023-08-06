50345cd991b0489358f18edd1160019c05daa58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:43 +0000

    Add package sleepecg/0.3.0/sleepecg-0.3.0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/sleepecg/sleepecg-0.3.0-cp38-cp38-manylinux2014_x86_64.whl/sleepecg/__init__.py
@@ -0,0 +1,13 @@
+"""A package for sleep stage classification using ECG data."""
+
+from . import io
+from .heartbeats import compare_heartbeats, detect_heartbeats, rri_similarity
+
+__all__ = [
+    'io',
+    'detect_heartbeats',
+    'compare_heartbeats',
+    'rri_similarity',
+]
+
+__version__ = '0.3.0'
