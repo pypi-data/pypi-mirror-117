adad988f08b209daf0a469e1474d4ce0d0ce3888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:06 +0000

    Add package pyaedt/0.3.10/pyaedt-0.3.10.tar.gz

---
+++ packages/pyaedt/pyaedt-0.3.10.tar.gz/pyaedt-0.3.10/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import sys
+
+from _setup_common import name, version, author, maintainer, maintainer_email, description, long_description, packages, data_files, license, classifiers
+
+
+if sys.version_info == (3, 9):
+    install_requires = ["pywin32 >= 2.2.7;platform_system=='Windows'",
+                        "https://github.com/pyansys/PyAEDT/raw/release/0.3/pythonnet-2.5.2-cp39-cp39-win_amd64.whl"]
+elif sys.version_info >= (3, 0):
+    install_requires = ["pywin32 >= 2.2.7;platform_system=='Windows'",
+                        "pythonnet >= 2.4.0;platform_system=='Windows'"]
+else:
+    install_requires = []
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    install_requires=install_requires,
+    packages=packages,
+    data_files=data_files,
+    include_package_data=True,
+    license=license,
+    classifiers=classifiers,
+)
