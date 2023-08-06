ef98b1fb4383bdce8f5925d9bef6c938f517ff34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:36 +0000

    Add package xangobolt-pulumi/0.1.1/xangobolt-pulumi-0.1.1.tar.gz

---
+++ packages/xangobolt-pulumi/xangobolt-pulumi-0.1.1.tar.gz/xangobolt-pulumi-0.1.1/src/resourceComponents/bootstrap.py
@@ -0,0 +1,11 @@
+from pulumi import ComponentResource, ResourceOptions, StackReference
+from resources import resource_group
+import pulumi
+from pulumi import Output
+
+class Bootstrap(ComponentResource):
+    def __init__(self, name: str, props: None, opts:  ResourceOptions = None):
+        super().__init__('Bootstrap', name, {}, opts)
+
+        # Create bootstrap resource group
+        bootstrap_rg = resource_group.resource_group('bootstrap', props, ResourceOptions.provider)
