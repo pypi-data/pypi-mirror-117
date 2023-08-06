d3fff0d79955ee34d4df58d08084e5b7f601d3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:32 +0000

    Add package indelpost/0.1.2/indelpost-0.1.2.tar.gz

---
+++ packages/indelpost/indelpost-0.1.2.tar.gz/indelpost-0.1.2/indelpost/__init__.py
@@ -0,0 +1,7 @@
+from indelpost.variant import *
+from indelpost.varaln import *
+from indelpost.utilities import *
+from indelpost.consensus import *
+from indelpost.pileup import *
+from indelpost.contig import *
+from indelpost.version import __version__ 
