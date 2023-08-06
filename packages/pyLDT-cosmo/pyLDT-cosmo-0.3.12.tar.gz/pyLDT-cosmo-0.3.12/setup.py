9c9a4ed3f0bcfd0d7859903d5aebdaf69bd9f339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:01 +0000

    Add package pyLDT-cosmo/0.3.12/pyLDT-cosmo-0.3.12.tar.gz

---
+++ packages/pyLDT-cosmo/pyLDT-cosmo-0.3.12.tar.gz/pyLDT-cosmo-0.3.12/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyLDT-cosmo",
+    version="0.3.12",
+    author="Matteo Cataneo",
+    author_email="mcataneo85@gmail.com",
+    description="Package for PDF calculations in Large Deviation Theory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcataneo/pyLDT-cosmo",
+    packages=setuptools.find_packages(),
+    package_data={'pyLDT_cosmo': ['benchmarks/*.dat']},
+    install_requires=[
+        'numpy>=1.19',
+        'scipy>=1.5',
+        'camb==1.1.2',
+        'mcfit==0.0.16',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
