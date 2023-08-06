4cf52cbb4869446ffa7037f638544d6da5428537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:56 +0000

    Add package dapodik/0.16.6/dapodik-0.16.6-py3-none-any.whl

---
+++ packages/dapodik/dapodik-0.16.6-py3-none-any.whl/dapodik/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from .version import __version__, __dapodik_version__  # NOQA
+from .version import __semester__, __tahun_ajaran__  # NOQA
+
+from .response import DapodikResponse
+from .result import DapodikResult
+
+from .auth import BaseAuth
+from .rest import BaseRest
+from .customrest import BaseCustomrest
+from .validasi import BaseValidasi
+from .peserta_didik import BasePesertaDidik
+from .gtk import BaseGtk
+from .sarpras import BaseSarpras
+from .rombongan_belajar import BaseRombonganBelajar
+from .sekolah import BaseSekolah
+
+from .dapodik import Dapodik
+
+__all__ = [
+    "BaseAuth",
+    "BaseCustomrest",
+    "BaseGtk",
+    "BasePesertaDidik",
+    "BaseRest",
+    "BaseRombonganBelajar",
+    "BaseSarpras",
+    "BaseSekolah",
+    "BaseValidasi",
+    "Dapodik",
+    "DapodikResponse",
+    "DapodikResult",
+]
