94c91e030ed8652377e288f632a885d189c11151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:32 +0000

    Add package archivr/0.1.3/archivr-0.1.3-py3-none-any.whl

---
+++ packages/archivr/archivr-0.1.3-py3-none-any.whl/archivr/cli.py
@@ -0,0 +1,13 @@
+import click
+
+from archivr import archive
+
+
+@click.command()
+@click.argument("src", type=click.Path(exists=True))
+def cli(src: str) -> None:
+    archive(src)
+
+
+if __name__ == "__main__":
+    cli()
