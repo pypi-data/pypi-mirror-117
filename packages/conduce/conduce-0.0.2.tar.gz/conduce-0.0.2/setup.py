49b971e11ce89a8e1a492ed274dfe2e2df5d0758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:38 +0000

    Add package conduce/0.0.2/conduce-0.0.2.tar.gz

---
+++ packages/conduce/conduce-0.0.2.tar.gz/conduce-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conduce",
+    version="0.0.2",
+    author="Usman Ahmad",
+    author_email="uahmad3013@outlook.com",
+    description="A config util package to flatten-reduce long keys",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/selphaware/conduce",
+    project_urls={
+        "Bug Tracker": "https://github.com/selphaware/conduce/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["PyYAML==5.4.1"]
+)
