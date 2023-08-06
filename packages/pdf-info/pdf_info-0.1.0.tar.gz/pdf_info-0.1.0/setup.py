770d8e489d9b81fc79622367acc2a04a80ed3923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:07 +0000

    Add package pdf-info/0.1.0/pdf_info-0.1.0.tar.gz

---
+++ packages/pdf-info/pdf_info-0.1.0.tar.gz/pdf_info-0.1.0/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+setup(name="pdf_info",
+version = "0.1.0",
+description="Demo",
+long_description = "Demo",
+author="Satyam&Piyush",
+packages=['pdf_info'],
+install_requires=["PymuPdf","nltk","pdfminer","sklearn","numpy","PyPDF2"])
