a6da6bbad20e2d5157076717d9cfd32b35bd36d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:43 +0000

    Add package pyws1uem/0.0.9/pyws1uem-0.0.9.tar.gz

---
+++ packages/pyws1uem/pyws1uem-0.0.9.tar.gz/pyws1uem-0.0.9/pyws1uem/mem/mem.py
@@ -0,0 +1,12 @@
+class MEM(object):
+    """
+    Base MEM class
+
+    Workspace ONE UEM REST APIs enables you to manage emails on devices. 
+    Through these APIs, you can retreive email management device details 
+    and apply compliance policies to devices.
+    """
+    pass
+
+
+# TODO: Implement
