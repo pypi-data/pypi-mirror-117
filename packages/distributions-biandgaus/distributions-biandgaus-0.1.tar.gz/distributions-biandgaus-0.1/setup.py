2c5e509230659305cc356d5511bced523a6a79c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:44 +0000

    Add package distributions-biandgaus/0.1/distributions-biandgaus-0.1.tar.gz

---
+++ packages/distributions-biandgaus/distributions-biandgaus-0.1.tar.gz/distributions-biandgaus-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-biandgaus',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-biandgaus'],
+      zip_safe=False)
+
