9667b81d0c329f51ff81e840b57d4929a68fcf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:45 +0000

    Add package cytra/0.5.0/cytra-0.5.0.tar.gz

---
+++ packages/cytra/cytra-0.5.0.tar.gz/cytra-0.5.0/cytra/db/attrstate_mixin.py
@@ -0,0 +1,31 @@
+from sqlalchemy import inspect
+
+from cytra.db.transform_mixin import TransformMixin
+from cytra.db.access_control_mixin import InspectionMixin
+
+
+class AttrStateMixin(TransformMixin, InspectionMixin):
+    def get_attr_changes(self) -> dict:
+        state = inspect(self)
+        changes = {}
+
+        for attr in state.attrs:
+            hist = state.get_history(attr.key, True)
+
+            if not hist.has_changes():
+                continue
+
+            old_value = hist.deleted[0] if hist.deleted else None
+            new_value = hist.added[0] if hist.added else None
+            changes[attr.key] = (old_value, new_value)
+
+        return changes
+
+    def export_attr_changes(self) -> dict:
+        res = {}
+        inspections = self.get_inspections()["all_by_key"]
+        for k, v in self.get_attr_changes().items():
+            # (type_, key, info, col)
+            info = inspections[k][2]
+            res[self.export_column_name(k, info)] = v
+        return res
