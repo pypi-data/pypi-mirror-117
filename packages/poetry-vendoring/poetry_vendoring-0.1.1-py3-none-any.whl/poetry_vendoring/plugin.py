f3249596e99c45866bc25477cfb0b5ec1f986159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package poetry-vendoring/0.1.1/poetry_vendoring-0.1.1-py3-none-any.whl

---
+++ packages/poetry-vendoring/poetry_vendoring-0.1.1-py3-none-any.whl/poetry_vendoring/plugin.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from cleo.commands.command import Command
+from poetry_bundle_plugin.plugin import ApplicationPlugin
+
+class VendorCommand(Command):
+    
+    name = "vendor"
+
+    def handle(self) -> Optional[int]:
+        self.line("Calling vendor")
+        # return super().handle()
+        return 0
+
+def factory():
+    return VendorCommand()
+
+class VendorPlugin(ApplicationPlugin):
+    def activate(self, application):
+        application.command_loader.register_factory("vendor", factory)
