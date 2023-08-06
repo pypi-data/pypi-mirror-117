3a02ab203d8f4a1b0871a0c0e007db4fbfd7b762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:51 +0000

    Add package cay/0.1.4/cay-0.1.4-py3-none-any.whl

---
+++ packages/cay/cay-0.1.4-py3-none-any.whl/cay/cli.py
@@ -0,0 +1,17 @@
+"""Cay: Simple Calculator implementation."""
+
+from . import main
+
+
+def cli():
+    """Run CLI REPL."""
+    while True:
+        print("cay> ", end="")
+        try:
+            print(main.eval(main.read(input())))
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    cli()
