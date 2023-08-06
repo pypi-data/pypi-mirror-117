303bd21d8cf395fb224bc29cc70818887e9de0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:52 +0000

    Add package ferriswheel/0.0a1.post0/ferriswheel-0.0a1.post0-py3-none-any.whl

---
+++ packages/ferriswheel/ferriswheel-0.0a1.post0-py3-none-any.whl/ferris/errors.py
@@ -0,0 +1,86 @@
+from aiohttp import ClientResponse
+
+__all__ = (
+    'FerrisException',
+    'HTTPException',
+    'BadRequest',
+    'Unauthorized',
+    'Forbidden',
+    'NotFound',
+    'FerrisUnavailable',
+    'WebsocketException',
+    'Reconnect',
+)
+
+
+class FerrisException(Exception):
+    """Base class for all ferriswheel-originated exceptions.
+    Theoretically, you could use this exception to catch all ferris exceptions.
+    """
+
+    pass
+
+
+class HTTPException(FerrisException):
+    """
+    Base class for all ferris HTTP exceptions.
+
+    Attributes
+    ----------
+    status: int
+        The status code of the response.
+    resp: :class:`aiohttp.ClientResponse`
+        The aiohttp response object.
+    """
+
+    def __init__(self, resp: ClientResponse, content: str = None):
+        content = content or resp.reason
+        self.status = resp.status
+        self.resp = resp
+        super().__init__(content)
+
+
+class BadRequest(HTTPException):
+    """The request was invalid or cannot be otherwise served."""
+
+    pass
+
+
+class Unauthorized(HTTPException):
+    """The request requires user authentication."""
+
+    pass
+
+
+class Forbidden(HTTPException):
+    """The request was a valid request,
+    but the server is refusing to respond to it, as you don't have enough permissions to do that.
+    """
+
+    pass
+
+
+class NotFound(HTTPException):
+    """The server cannot find what you requesed for."""
+
+    pass
+
+
+class FerrisUnavailable(HTTPException):
+    """SSHHH! Ferris is sleeping, and can't answer the request right now.
+    Try again later.
+    """
+
+    pass
+
+
+class WebsocketException(FerrisException):
+    """Base class for all websocket exceptions."""
+
+    pass
+
+
+class Reconnect(WebsocketException):
+    """Signal to reconnect to the websocket."""
+
+    pass
---
+++ packages/ferriswheel/ferriswheel-0.0a1.post0-py3-none-any.whl/ferris/user.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, List, cast
+
+from .base import BaseObject
+from .guild import Guild
+
+if TYPE_CHECKING:
+    from .connection import Connection
+    from .types import Data
+
+__all__ = ('User',)
+
+
+class PartialUser(BaseObject):
+    """
+    Represents a Partial FerrisChat User.
+    """
+
+    __slots__ = ('_name',)
+
+    def __init__(self, data: Data, /) -> None:
+        self._process_data(data)
+
+    def _process_data(self, data: Data) -> None:
+        self._store_snowflake(cast(int, data.get('id')))
+
+        self._name = data.get('name')
+
+    @property
+    def name(self) -> str:
+        """str: The user's name."""
+        return self._name
+
+
+class User(BaseObject):
+    """
+    Represents a FerrisChat user.
+    """
+
+    __slots__ = ('_connection', '_name', '_guilds', '_id')
+
+    def __init__(self, connection: Connection, data: Data, /) -> None:
+        self._connection: Connection = connection
+        self._process_data(data)
+        super().__init__()
+
+    def _process_data(self, data: Data, /) -> None:
+        self._store_snowflake(cast(int, data.get('id')))
+
+        self._name: str = cast(str, data.get('name'))
+
+        self._guilds: Dict[int, Guild] = {}
+
+        for g in data.get('guilds', []):
+            guild = Guild(self._connection, g)
+            self._guilds[guild.id] = guild
+
+        # self._flags = data.get('flags')
+        # UserFlag after ferrischat implemented it
+
+    async def fetch_guilds(self) -> List[Guild]:
+        """|coro|
+
+        Fetches all the guilds this user is in.
+
+        Returns
+        -------
+        List[:class:`~.Guild`]
+            A list of the guilds this user is in.
+
+        .. warning::
+            This method will do nothing as FerrisChat has not implemented this feature yet.
+        """
+        ...
+
+    @property
+    def name(self, /) -> str:
+        """str: The username of this user."""
+        return self._name
+
+    @property
+    def guilds(self, /) -> List[Guild]:
+        """List[:class:`~.Guild`]: A list of the guilds this user is in."""
+        return list(self._guilds.values())
+
+    def __del__(self, /) -> None:
+        if not hasattr(self, '_connection'):
+            return
+
+        self._connection.deref_user(self.id)
+
+    def __expr__(self, /) -> str:
+        return f'<User id={self.id} name={self.name!r}>'
