49fc9d556cc3fafb2cca06dfdfba6bd5b5118ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:24 +0000

    Add package openlxp-xia/1.0.28/openlxp-xia-1.0.28.tar.gz

---
+++ packages/openlxp-xia/openlxp-xia-1.0.28.tar.gz/openlxp-xia-1.0.28/openlxp_xia/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from .models import MetadataFieldOverwrite, XIAConfiguration, XISConfiguration
+
+
+@admin.register(XIAConfiguration)
+class XIAConfigurationAdmin(admin.ModelAdmin):
+    list_display = (
+        'publisher',
+        'source_metadata_schema',
+        'source_target_mapping',
+        'target_metadata_schema',)
+    fields = ['publisher',
+              'source_metadata_schema',
+              ('source_target_mapping',
+               'target_metadata_schema')]
+
+
+@admin.register(XISConfiguration)
+class XISConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('xis_metadata_api_endpoint',
+                    'xis_supplemental_api_endpoint',)
+    fields = ['xis_metadata_api_endpoint',
+              'xis_supplemental_api_endpoint']
+
+
+@admin.register(MetadataFieldOverwrite)
+class MetadataFieldOverwriteAdmin(admin.ModelAdmin):
+    list_display = ('field_name',
+                    'field_type',
+                    'field_value',
+                    'overwrite',)
+    fields = ['field_name',
+              'field_type',
+              'field_value',
+              'overwrite']
