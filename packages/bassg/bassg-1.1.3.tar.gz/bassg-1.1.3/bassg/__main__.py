c084afedb24b3d3bcb1caa29df3c4070eb1a032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package bassg/1.1.3/bassg-1.1.3.tar.gz

---
+++ packages/bassg/bassg-1.1.3.tar.gz/bassg-1.1.3/bassg/__main__.py
@@ -0,0 +1,59 @@
+import click
+import os
+from .Generator import Generator
+
+@click.group()
+def cli():
+    pass
+
+@cli.command('create')
+@click.argument('project_name')
+def create(project_name):
+    '''
+    Create the project and all of its directories
+    '''
+    generator = Generator(project_name)
+    generator.create_directories()
+    
+@cli.command('generate')
+@click.argument('project_name')
+def generate(project_name):
+    '''
+    Generate the site from the files in the project directory
+    '''
+    if _check_project_exists(project_name):
+        generator = Generator(project_name)
+        # Load the config.json file here
+        generator.generate_site()
+    else:
+        print(f'Project \"{project_name}\" does not exist in this directory, '
+              f'create it with \"bassg create {project_name}\"')
+    
+    
+def _check_project_exists(project_name):
+    '''
+    Check that there's a project in the current directory
+    '''
+    folder_reqs = ['build', 'build\\assets', 'build\\config',
+                   'build\\markdown', 'build\\templates', 'site']
+    cwd = os.getcwd()
+    dirs = os.listdir(cwd)
+    
+    if project_name in dirs:
+        cwd = os.path.join(cwd, project_name)
+        os.chdir(cwd)
+        roots = []
+        for root, dirs, files in os.walk(cwd):
+            roots.append(root)
+        for folder_req in folder_reqs:
+            if os.path.join(cwd, folder_req) not in roots:
+                return False
+    else:
+        return False
+                
+    os.chdir('..')
+    return True
+    
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/bassg/bassg-1.1.3.tar.gz/bassg-1.1.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bassg',
+    version='1.1.3',
+    license='MIT',
+    author='Hunter Lawson',
+    author_email='hawson7@gmail.com',
+    description='bassg - Basic Agile Static Site Generator',
+    url='https://github.com/HunterLawson/bassg',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    py_modules=['bassg'],
+    install_requires=['beautifulsoup4', 'click', 'markdown', 'Jinja2']
+)
