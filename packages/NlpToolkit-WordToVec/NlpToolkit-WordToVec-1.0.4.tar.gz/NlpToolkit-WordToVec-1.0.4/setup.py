7806716c2e3112b4a71ec83b70c8da7bb340c2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:37 +0000

    Add package NlpToolkit-WordToVec/1.0.4/NlpToolkit-WordToVec-1.0.4.tar.gz

---
+++ packages/NlpToolkit-WordToVec/NlpToolkit-WordToVec-1.0.4.tar.gz/NlpToolkit-WordToVec-1.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-WordToVec',
+    version='1.0.4',
+    packages=['WordToVec'],
+    url='https://github.com/StarlangSoftware/WordToVec-Py',
+    license='',
+    author='olcay',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Word2Vec Library',
+    install_requires=['NlpToolkit-Corpus']
+)
