aff275155f9112b7e3ada55ab3073e761cd90482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:08 +0000

    Add package TypeTools/1.1/TypeTools-1.1-py3-none-any.whl

---
+++ packages/TypeTools/TypeTools-1.1-py3-none-any.whl/typetools/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["Descriptor", "Typed", "Unsigned", "MaxSizedIterable",
+           "Integer", "Float", "String", "Sequence"]
+
+from ._descriptor import Descriptor
+from ._typed import Typed
+from ._broadtypes import *
+from ._types import Integer, Float, String, Sequence
