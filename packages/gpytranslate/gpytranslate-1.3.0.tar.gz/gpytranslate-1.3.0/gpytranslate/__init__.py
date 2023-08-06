97cacf6067f8fc2ec7e60e27c12e5f513e13f359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:25 +0000

    Add package gpytranslate/1.3.0/gpytranslate-1.3.0.tar.gz

---
+++ packages/gpytranslate/gpytranslate-1.3.0.tar.gz/gpytranslate-1.3.0/gpytranslate/__init__.py
@@ -0,0 +1,6 @@
+from .gpytranslate import Translator
+from .sync import SyncTranslator
+from .types import TranslatedObject
+
+__version__ = "1.2.0"
+__all__ = ["Translator", "SyncTranslator", "TranslatedObject", "__version__"]
