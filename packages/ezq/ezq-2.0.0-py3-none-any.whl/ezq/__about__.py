fc92aab66f7f3756c7abfc3087f5a0b57312bdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:39 +0000

    Add package ezq/2.0.0/ezq-2.0.0-py3-none-any.whl

---
+++ packages/ezq/ezq-2.0.0-py3-none-any.whl/ezq/__about__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Simple wrapper for python multiprocessing."""
+
+__url__ = "https://github.com/metaist/ezq"
+__version__ = "2.0.0"
+__pubdate__ = "2021-08-22T19:56:48Z"  # date/time in UTC
+__author__ = "Metaist LLC"
+__email__ = "metaist@metaist.com"
+__copyright__ = "Copyright 2021 by Metaist LLC"
+__license__ = "MIT"
