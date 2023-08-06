a0394de21b2dcf86dca2589af53a2a2231aa4791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:57 +0000

    Add package terraforms-farrukh90/0.0.13/terraforms-farrukh90-0.0.13.tar.gz

---
+++ packages/terraforms-farrukh90/terraforms-farrukh90-0.0.13.tar.gz/terraforms-farrukh90-0.0.13/src/linux/ticket3.py
@@ -0,0 +1,65 @@
+import os 
+import time 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+def filestructure():
+    try:
+        FILETOWORK="/tmp/ordered.txt"
+        # Checks if "/tmp/ordered.txt" is created
+        if os.path.getsize(FILETOWORK) <= 0:
+            print(bcolors.FAIL + FILETOWORK + " " + "seems to be empty" + bcolors.ENDC  )
+        else:
+            print(bcolors.OKGREEN + FILETOWORK + " " + "is created" + bcolors.ENDC  )
+    except FileNotFoundError:
+        print(bcolors.FAIL +  FILETOWORK + " is not created"+ bcolors.ENDC)
+    time.sleep(0.5)
+   
+    try:
+        FILETOWORK="/tmp/head.txt"
+        # Checks if "/tmp/head.txt" is created
+        if os.path.getsize(FILETOWORK) <= 0:
+            print(bcolors.FAIL + FILETOWORK + " " + "seems to be empty" + bcolors.ENDC  )
+        else:
+            print(bcolors.OKGREEN + FILETOWORK + " " + "is created" + bcolors.ENDC  )
+                
+    except FileNotFoundError:
+        print(bcolors.FAIL +  FILETOWORK + " is not created"+ bcolors.ENDC)
+    time.sleep(0.5)
+    try:
+        FILETOWORK="/tmp/tail.txt"
+        num_lines = sum(1 for line in open(FILETOWORK))
+        # Checks if "/tmp/tail.txt" is created
+        if os.path.getsize(FILETOWORK) <= 0 and num_lines == 50:
+            print(bcolors.FAIL + FILETOWORK + " " + "seems to be empty" + bcolors.ENDC  )
+        else:
+            print(bcolors.OKGREEN + FILETOWORK + " " + "is created" + bcolors.ENDC  )
+    except FileNotFoundError:
+        print(bcolors.FAIL +  FILETOWORK + " is not created"+ bcolors.ENDC)
+    time.sleep(0.5)
+
+   
+    try:
+        FILETOWORK="/tmp/lines.txt"
+        num_lines = sum(1 for line in open(FILETOWORK))
+        # Checks if "/tmp/lines.txt" is created
+        if os.path.getsize(FILETOWORK) <= 0 and num_lines == 250:
+            print(bcolors.FAIL + FILETOWORK + " " + "seems to be empty" + bcolors.ENDC  )
+        else:
+            print(bcolors.OKGREEN + FILETOWORK + " " + "is created" + bcolors.ENDC  )
+    except FileNotFoundError:
+        print(bcolors.FAIL +  FILETOWORK + " is not created"+ bcolors.ENDC)
+    time.sleep(0.5)
+ # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
