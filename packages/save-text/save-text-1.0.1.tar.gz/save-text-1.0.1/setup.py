728062fce906f892a365c7bdabdf3dc6af6d68bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:08 +0000

    Add package save-text/1.0.1/save-text-1.0.1.tar.gz

---
+++ packages/save-text/save-text-1.0.1.tar.gz/save-text-1.0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+  name="save-text",
+  version="1.0.1",
+  description='You can save and retrieve text.',
+  author='YahiroRyo',  
+  author_email='YahiroRyo@users.noreply.github.com',
+  url="https://github.com/YahiroRyo",
+  license='MIT',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  entry_points={
+    "console_scripts":[
+      "save-text = save:main"
+    ]
+  }
+)
