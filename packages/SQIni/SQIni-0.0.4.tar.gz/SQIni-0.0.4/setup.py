49cd2c6c882e31d0aac5fe3c4aab415d7396ade8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:47 +0000

    Add package SQIni/0.0.4/SQIni-0.0.4.tar.gz

---
+++ packages/SQIni/SQIni-0.0.4.tar.gz/SQIni-0.0.4/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+VERSION = '0.0.4'
+DESCRIPTION = 'connect table settings of a sqlite database with a ini file for better editing '
+
+# Setting up
+setup(
+    name="SQIni",
+    version=VERSION,
+    author="Miku",
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/princessmiku/SQIni',
+    install_requires=[],
+    keywords=['python', 'sqlite3', 'sqlite', 'ini', 'sqini', 'orm', 'database'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    packages=["sqini"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
