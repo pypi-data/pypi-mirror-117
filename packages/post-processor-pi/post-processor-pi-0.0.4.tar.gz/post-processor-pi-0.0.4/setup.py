a59eba50b82ad0cb8712461cecac7f05a00de3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:12 +0000

    Add package post-processor-pi/0.0.4/post-processor-pi-0.0.4.tar.gz

---
+++ packages/post-processor-pi/post-processor-pi-0.0.4.tar.gz/post-processor-pi-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='post-processor-pi',
+    version='0.0.4',
+    license='MIT',
+    author='Paolo Italiani',
+    author_email='paoita@hotmail.it',
+    description='Post processor for Document AI',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'run_GUI = dai_post_processor.run_GUI:main'
+        ]
+    },
+    url='https://github.com/mike-huls/toolbox',
+    packages=['dai_post_processor'],
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "matplotlib"
+    ],
+)
