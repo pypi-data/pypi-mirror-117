12af76cfbec7dffed21a57177c84bb00bd02a9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:56 +0000

    Add package pyfunky/3.5.1/pyfunky-3.5.1-py2.py3-none-any.whl

---
+++ packages/pyfunky/pyfunky-3.5.1-py2.py3-none-any.whl/funky/utils/__init__.py
@@ -0,0 +1,29 @@
+"""Miscellaneous utility functions."""
+
+import sys
+import termios
+import tty
+
+
+def getch(prompt: str = None) -> str:
+    """Reads a single character from stdin.
+
+    Args:
+        prompt (optional): prompt that is presented to user.
+
+    Returns:
+        The single character that was read.
+    """
+    if prompt:
+        sys.stdout.write(prompt)
+
+    sys.stdout.flush()
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
