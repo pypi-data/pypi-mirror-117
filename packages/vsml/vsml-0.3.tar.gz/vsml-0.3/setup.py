e66aafe300e2447980017778c1b56805be1f8903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:10 +0000

    Add package vsml/0.3/vsml-0.3.tar.gz

---
+++ packages/vsml/vsml-0.3.tar.gz/vsml-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='vsml',
+    version='0.3',
+    description='a very simple markup language.',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url='https://github.com/GalaxiaTeam/vsml',
+    license='MIT',
+    packages=['vsml'],
+    author_name='Satyrn',
+    author_email='satyrn@tutanota.com',
+    keywords=['vsml', 'markup'],
+    download_url='https://pypi.org/project/vsml/',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
---
+++ packages/vsml/vsml-0.3.tar.gz/vsml-0.3/vsml/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+__author__ = "Satyrn"
+__version__ = "0.3"
+
+from .func import *
