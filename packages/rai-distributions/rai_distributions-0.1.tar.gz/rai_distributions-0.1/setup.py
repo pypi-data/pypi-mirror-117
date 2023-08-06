6323bad264b53f729628dad7803039b1d0f4aab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:32 +0000

    Add package rai-distributions/0.1/rai_distributions-0.1.tar.gz

---
+++ packages/rai-distributions/rai_distributions-0.1.tar.gz/rai_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rai_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rai_distributions'],
+      author = 'Santos Rai',
+      zip_safe=False)
