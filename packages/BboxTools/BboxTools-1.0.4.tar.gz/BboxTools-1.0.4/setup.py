07bce2efd486065f2ecfc04af41b3642327c4e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:59 +0000

    Add package BboxTools/1.0.4/BboxTools-1.0.4.tar.gz

---
+++ packages/BboxTools/BboxTools-1.0.4.tar.gz/BboxTools-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BboxTools',
+    version='1.0.4',
+    packages=[''],
+    url='https://github.com/Angtian/BboxTools',
+    license='BSD',
+    author='Angtian Wang',
+    author_email='angtianwang@gmail.com',
+    description='A python toolbox for bounding box operation'
+)
