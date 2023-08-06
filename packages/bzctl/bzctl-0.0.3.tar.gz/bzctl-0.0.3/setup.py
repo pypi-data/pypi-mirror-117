3eb3aef436e4df401f448e2a2a0237440f466bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:46 +0000

    Add package bzctl/0.0.3/bzctl-0.0.3.tar.gz

---
+++ packages/bzctl/bzctl-0.0.3.tar.gz/bzctl-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='bzctl',  
+     version='0.0.3',
+     scripts=['bzctl'] ,
+     author="Ahmet Yusuf Birinci",
+     author_email="ayb84870@gmail.com",
+     description="What that this script done is if you pass a run and the bunch of other arguments it collects the other arguments and putting the right arguments in right places on the yaml template.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://bag.org.tr/proje/bulutzincir/bulut-zincir/blob/master/Scripts/bulut-zincir",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
