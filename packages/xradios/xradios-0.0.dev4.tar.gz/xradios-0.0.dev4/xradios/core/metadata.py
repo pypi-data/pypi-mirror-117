f38c6e8ade96825e9711e839cc867cbd8f4a4038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:15 +0000

    Add package xradios/0.0.dev4/xradios-0.0.dev4.tar.gz

---
+++ packages/xradios/xradios-0.0.dev4.tar.gz/xradios-0.0.dev4/xradios/core/metadata.py
@@ -0,0 +1,75 @@
+import logging
+import os
+import re
+from functools import partial
+from pathlib import Path
+
+from pluginbase import PluginBase
+from streamscrobbler import streamscrobbler
+
+
+log = logging.getLogger(__name__)
+
+here = os.path.abspath(os.path.dirname(__file__))
+get_path = partial(os.path.join, here)
+plugin_base = PluginBase(package="xradios.plugins")
+
+plugin_source = plugin_base.make_plugin_source(
+    searchpath=[get_path("../plugins")]
+)
+
+
+def normalize_plugin_name(name):
+    name = re.sub(r"(\s|\-|\.|,|\"|\'|\`)+", "_", name)
+    return name.lower()
+
+
+def plugin_name(name):
+    return "plug_{}".format(normalize_plugin_name(name))
+
+
+class MetadataState:
+    def __init__(self):
+        self.id = ""
+        self.name = ""
+        self.play_now = ""
+        self.homepage = ""
+
+    def __repr__(self):
+        return "MetadataState([{}, {}, {}])".format(
+            self.name, self.play_now, self.homepage
+        )
+
+
+class MetadataManager:
+    def __init__(self):
+        self.station = None
+        self.s = MetadataState()
+
+    def get(self):
+        """
+        Try to find the id of the song that is playing.
+        """
+        song = None
+        name = plugin_name(self.station.name)
+
+        if name in plugin_source.list_plugins():
+            # Try to get the id of song with user plugin.
+            plugin = plugin_source.load_plugin(name)
+            song = plugin.run()
+        else:
+            # Try to get the id of song with streamscrobler plugin.
+            plugin = plugin_source.load_plugin("stream")
+            song = plugin.run(self.station.url)
+
+        if song:
+            self.s.play_now = song
+
+    def state(self):
+        return self.s
+
+    def __call__(self, station):
+        self.station = station
+        self.s.id = self.station.id
+        self.s.homepage = self.station.homepage
+        self.s.name = self.station.name
---
+++ packages/xradios/xradios-0.0.dev4.tar.gz/xradios-0.0.dev4/xradios/core/player.py
@@ -0,0 +1,38 @@
+import logging
+
+from mpv import MPV
+from pyradios import RadioBrowser
+
+
+rb = RadioBrowser()
+log = logging.getLogger(__name__)
+
+
+class Player:
+    player = MPV()
+    # player.loop_playlist = "inf"
+    player.set_loglevel = "no"
+
+    def play(self, station):
+        url = self._click_counter(station.stationuuid)
+        self.player.play(url)
+
+    def stop(self):
+        self.player.play("")
+
+    def pause(self):
+        if self.player.pause:
+            self.player.pause = False
+        else:
+            self.player.pause = True
+
+    def terminate(self):
+        self.player.terminate()
+
+    def _click_counter(self, stationuuid):
+        try:
+            station = rb.click_counter(stationuuid)
+        except Exception:
+            log.exception("click counter error:")
+        else:
+            return station["url"]
