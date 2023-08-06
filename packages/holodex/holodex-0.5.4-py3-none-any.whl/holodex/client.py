b7532dac3bfaf6c3432ce787a55aa3f477f73ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:01 +0000

    Add package holodex/0.5.4/holodex-0.5.4-py3-none-any.whl

---
+++ packages/holodex/holodex-0.5.4-py3-none-any.whl/holodex/client.py
@@ -0,0 +1,46 @@
+from holodex.model.live import Live
+from holodex.model.autocomplete import AutoComplete
+from typing import Any, Literal, Optional
+from aiohttp.client import ClientSession
+
+from holodex.http import HolodexHttpClient
+from holodex.model.channel_info import ChannelInfo
+
+
+class HolodexClient(HolodexHttpClient):
+    def __init__(self, session: Optional[ClientSession] = None) -> None:
+        super().__init__(session)
+
+    def __get_params(self, locals: dict[str, Any]):
+        locals.pop("self")
+        return {k: v for k, v in locals.items() if v is not None}
+
+    async def channel_info(self, channel_id: str) -> ChannelInfo:
+        return ChannelInfo(await self.channels(channel_id))
+
+    async def autocomplete(self, keyword: str) -> AutoComplete:
+        return AutoComplete(await self.search_autocomplete(keyword))
+
+    async def live_streams(
+        self,
+        *,
+        channel_id: Optional[str] = None,
+        id: Optional[str] = None,
+        include: Optional[list[str]] = None,
+        lang: Optional[Literal["all", "en", "ja"]] = None,
+        limit: Optional[int] = None,
+        max_upcoming_hours: Optional[int] = None,
+        mentioned_channel_id: Optional[str] = None,
+        offset: Optional[int] = None,
+        order: Optional[Literal["asc", "desc"]] = None,
+        org: Optional[
+            Literal["All Vtubers", "Hololive", "Nijisanji", "Independents"]
+        ] = None,
+        paginated: Optional[str] = None,
+        sort: Optional[str] = None,
+        status: Optional[str] = None,
+        topic: Optional[str] = None,
+        type: Optional[str] = None
+    ) -> Live:
+        params = self.__get_params(locals())
+        return Live(await self.live(params))
---
+++ packages/holodex/holodex-0.5.4-py3-none-any.whl/holodex/http.py
@@ -0,0 +1,39 @@
+from aiohttp import ClientSession
+from typing import Any, Literal, Optional
+
+
+class HolodexHttpClient:
+    BASE_URL = "https://holodex.net/api/v2/"
+
+    def __init__(self, session: Optional[ClientSession] = None) -> None:
+        self.session = session
+
+    async def close(self):
+        if self.session:
+            await self.session.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+
+    async def request(
+        self, method: Literal["GET", "POST"], endpoint: str, **kwargs: Any
+    ) -> Any:
+        if not self.session:
+            self.session = ClientSession()
+
+        async with self.session.request(
+            method, self.BASE_URL + endpoint, **kwargs
+        ) as r:
+            return await r.json()
+
+    async def channels(self, channel_id: str) -> Any:
+        return await self.request("GET", f"/channels/{channel_id}")
+
+    async def search_autocomplete(self, q: str) -> Any:
+        return await self.request("GET", f"/search/autocomplete", params={"q": q})
+
+    async def live(self, params: dict[str, Any]) -> Any:
+        return await self.request("GET", f"/live", params=params)
