7356fb257cc34efe723885dc236b5633fd6d3ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:02 +0000

    Add package alex-nn/0.1.9.dev0/alex-nn-0.1.9.dev0.tar.gz

---
+++ packages/alex-nn/alex-nn-0.1.9.dev0.tar.gz/alex-nn-0.1.9.dev0/alex/engine/example_data_pytorch.py
@@ -0,0 +1,31 @@
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor()])
+# ,
+#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
+                                          shuffle=True, num_workers=2)
+
+valset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+valloader = torch.utils.data.DataLoader(valset, batch_size=10000,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+inputs = images.to(device)
+print(device)
+
+val_inputs, val_labels = iter(valloader).next()
+
+loop(val_inputs, trainable_params)
