732cd825fa2a653683f645215b7f3fc67ce06bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:40 +0000

    Add package mmdok-cli/0.42/mmdok-cli-0.42.tar.gz

---
+++ packages/mmdok-cli/mmdok-cli-0.42.tar.gz/mmdok-cli-0.42/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='mmdok-cli',
+    version='0.42',    
+    description='Cli client for mmdok',
+    url='https://github.com/emagnca/mmcli',
+    author='Magnus Carlhammar',
+    author_email='mcarlhammar@gmail.com',
+    license='BSD 2-clause',
+    packages=['mmcli'],
+    install_requires=['cmd2==1.5.0',
+                      'requests==2.25.0',
+                      'urllib3==1.26.2'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',       
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+)
