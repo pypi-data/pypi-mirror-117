cfa661fa89e6e409a5fa7010b001b566b016a226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:53 +0000

    Add package quo/2021.4.5/quo-2021.4.5-py3-none-any.whl

---
+++ packages/quo/quo-2021.4.5-py3-none-any.whl/quo/outliers/__init__.py
@@ -0,0 +1,12 @@
+from .exceptions import (
+        Abort, 
+        QuoException, 
+        UsageError,
+        BadParameter,
+        MissingParameter,
+        NoSuchApp,
+        BadAppUsage, 
+        BadArgUsage, 
+        FileError,
+        Exit
+        )
