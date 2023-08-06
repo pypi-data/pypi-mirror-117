0c5ab55c66be32960ea91eaaa5096bf24ee5c2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:54 +0000

    Add package yyx-tools/0.0.16/yyx_tools-0.0.16.tar.gz

---
+++ packages/yyx-tools/yyx_tools-0.0.16.tar.gz/yyx_tools-0.0.16/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : /
+@Author  : Yanyuxiang
+@Email   : yanyuxiangtoday@163.com
+@FileName: /
+@Software: PyCharm
+'''
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yyx_tools",
+    version="0.0.16",
+    author="yanyuxiangtoday",
+    author_email="yanyuxiangtoday@163.com",
+    description="tools for algorithm engineer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanyuxiangToday/yyx_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
