5de761c22c02fd3729c2094200b1c2cfdd85a341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:58 +0000

    Add package agri-tech/0.1.3/agri_tech-0.1.3.tar.gz

---
+++ packages/agri-tech/agri_tech-0.1.3.tar.gz/agri_tech-0.1.3/tests/test_agri_tech.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `agri_tech` package."""
+
+import pytest
+
+# from agri_tech import agri_tech
+from agri_tech.agri_tech import Lidar
+
+from geopandas import GeoDataFrame, GeoSeries, read_file
+from geopandas.testing import assert_geodataframe_equal, assert_geoseries_equal
+
+
+
+
+class TestDataFrame:
+    def test_df_init(self):
+        lidar = Lidar()
+        assert type(lidar.geodf('spatial_subset.geojson')) is GeoDataFrame
+        
+    def test_geo_colname(self):
+        lidar = Lidar()
+        assert lidar.geodf('spatial_subset.geojson')._geometry_column_name == "geometry"
+        
