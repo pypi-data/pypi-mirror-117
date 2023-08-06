97215adfa1c268c3eaf7c097bffd4f15abb34371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:45 +0000

    Add package sqlmodel/0.0.1/sqlmodel-0.0.1-py3-none-any.whl

---
+++ packages/sqlmodel/sqlmodel-0.0.1-py3-none-any.whl/sqlmodel/sql/schema.py
@@ -0,0 +1,22 @@
+# from typing import List, Optional, Union
+# from sqlalchemy.sql.schema import MetaData as _MetaData
+# from sqlalchemy.sql.schema import Table
+# from sqlalchemy.future.engine import Engine, Connection
+
+# # TODO: remove this
+# # class MetaData(_MetaData):
+# #     def create_all(
+# #         self,
+# #         bind: Optional[Union[Engine, Connection]] = None,
+# #         tables: Optional[List[Table]] = None,
+# #         checkfirst: bool = True,
+# #     ) -> None:
+# #         super().create_all(bind=bind, tables=tables, checkfirst=checkfirst)  # type: ignore
+
+# #     def drop_all(
+# #         self,
+# #         bind: Optional[Union[Engine, Connection]] = None,
+# #         tables: Optional[List[Table]] = None,
+# #         checkfirst: bool = True,
+# #     ) -> None:
+# #         super().drop_all(bind=bind, tables=tables, checkfirst=checkfirst)  # type: ignore
