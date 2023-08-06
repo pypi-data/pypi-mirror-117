bd8c132511aec36806ea5f6e26a43334de8226eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:44 +0000

    Add package uploadgrampyapi/1.1.1/UploadgramPyAPI-1.1.1.tar.gz

---
+++ packages/uploadgrampyapi/UploadgramPyAPI-1.1.1.tar.gz/UploadgramPyAPI-1.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['uploadgrampyapi']
+setup_kwargs = {
+    'name': 'uploadgrampyapi',
+    'version': '1.1.1',
+    'description': 'This API can be upload, download, remove and rename any files from the service uploadgram.me.',
+    'long_description': None,
+    'author': 'tankalxat34',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
