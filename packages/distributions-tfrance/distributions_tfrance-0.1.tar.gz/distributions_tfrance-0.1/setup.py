ccc6b6e45cb78941c83fe9e13fff507ad1680da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:16 +0000

    Add package distributions-tfrance/0.1/distributions_tfrance-0.1.tar.gz

---
+++ packages/distributions-tfrance/distributions_tfrance-0.1.tar.gz/distributions_tfrance-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tfrance',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tfrance'],
+      zip_safe=False)
