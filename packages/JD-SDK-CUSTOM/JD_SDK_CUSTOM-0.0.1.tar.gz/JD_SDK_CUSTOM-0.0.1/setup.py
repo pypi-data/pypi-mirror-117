4e8ff3e552eeb335e33e151a7e83d2cdf48086b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:48 +0000

    Add package JD-SDK-CUSTOM/0.0.1/JD_SDK_CUSTOM-0.0.1.tar.gz

---
+++ packages/JD-SDK-CUSTOM/JD_SDK_CUSTOM-0.0.1.tar.gz/JD_SDK_CUSTOM-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JD_SDK_CUSTOM", # Replace with your own username
+    version="0.0.1",
+    author="Rponnt",
+    author_email="rponnt@gmail.com",
+    description="JD SDK version 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires= ['numpy','rsa','psutil','apscheduler','wincertstore'],
+    python_requires='>=3',
+)
