32c0855cf361a3f799bb409d01c77fd5f42e6b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:04 +0000

    Add package jinjaMarkdown/0.0.4/jinjaMarkdown-0.0.4.tar.gz

---
+++ packages/jinjaMarkdown/jinjaMarkdown-0.0.4.tar.gz/jinjaMarkdown-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jinjaMarkdown",
+    version="0.0.4",
+    author="Anthony Lazar",
+    author_email="alazar@uwaterloo.ca",
+    description="A package containing a markdown filter for jinja2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnthonyGithubCorner/jinjaMarkdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
