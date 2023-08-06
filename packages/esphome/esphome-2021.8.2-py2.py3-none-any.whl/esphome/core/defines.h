92d64892f0aa8af86ea846eb050c0eccf4fe403e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:17 +0000

    Add package esphome/2021.8.2/esphome-2021.8.2-py2.py3-none-any.whl

---
+++ packages/esphome/esphome-2021.8.2-py2.py3-none-any.whl/esphome/core/defines.h
@@ -0,0 +1,31 @@
+#pragma once
+// This file is auto-generated! Do not edit!
+
+#define USE_API
+#define USE_LOGGER
+#define USE_BINARY_SENSOR
+#define USE_SENSOR
+#define USE_SWITCH
+#define USE_WIFI
+#define USE_STATUS_LED
+#define USE_TEXT_SENSOR
+#define USE_FAN
+#define USE_COVER
+#define USE_LIGHT
+#define USE_CLIMATE
+#define USE_NUMBER
+#define USE_SELECT
+#define USE_MQTT
+#define USE_POWER_SUPPLY
+#define USE_HOMEASSISTANT_TIME
+#define USE_JSON
+#ifdef ARDUINO_ARCH_ESP32
+#define USE_ESP32_CAMERA
+#define USE_ESP32_BLE_SERVER
+#define USE_IMPROV
+#endif
+#define USE_TIME
+#define USE_DEEP_SLEEP
+#define USE_CAPTIVE_PORTAL
+#define ESPHOME_BOARD "dummy_board"
+#define USE_MDNS
