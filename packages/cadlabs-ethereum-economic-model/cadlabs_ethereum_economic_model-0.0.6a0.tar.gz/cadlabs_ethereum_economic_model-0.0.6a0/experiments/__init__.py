7dff846cfbd17ef817f178554fb9a00bc1ba85e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:07 +0000

    Add package cadlabs-ethereum-economic-model/0.0.6a0/cadlabs_ethereum_economic_model-0.0.6a0.tar.gz

---
+++ packages/cadlabs-ethereum-economic-model/cadlabs_ethereum_economic_model-0.0.6a0.tar.gz/cadlabs_ethereum_economic_model-0.0.6a0/experiments/__init__.py
@@ -0,0 +1,3 @@
+# import cadlabs_ethereum_economic_model.utils as _package_utils
+#
+# __all__ = _package_utils.import_submodules(__name__).keys()
---
+++ packages/cadlabs-ethereum-economic-model/cadlabs_ethereum_economic_model-0.0.6a0.tar.gz/cadlabs_ethereum_economic_model-0.0.6a0/setup.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'package'}
+
+packages = \
+['.ipynb_checkpoints',
+ 'cadlabs_ethereum_economic_model',
+ 'cadlabs_ethereum_economic_model.experiments',
+ 'cadlabs_ethereum_economic_model.model',
+ 'data',
+ 'data.api',
+ 'experiments',
+ 'experiments.notebooks',
+ 'experiments.notebooks.simulation_profiling',
+ 'experiments.notebooks.visualizations',
+ 'experiments.templates',
+ 'model',
+ 'model.parts',
+ 'model.parts.utils']
+
+package_data = \
+{'': ['*'],
+ 'experiments': ['outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_1b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_2.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_2.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_2.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_2.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_2.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3a.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3b.png',
+                 'outputs/hoban_borgers_economic_model/experiment_model_validation_scenario_3b.png',
+                 'outputs/validator_environment_yield_contour.png'],
+ 'experiments.notebooks.visualizations': ['assets/*']}
+
+install_requires = \
+['cadCAD-tools==0.0.1.4',
+ 'diskcache==5.2.1',
+ 'ipython-autotime==0.3.1',
+ 'ipywidgets==7.6.3',
+ 'jupyter-dash==0.4.0',
+ 'matplotlib==3.3.4',
+ 'notebook==6.3.0',
+ 'numpy==1.21.2',
+ 'plotly==4.14.3',
+ 'radcad==0.8.0',
+ 'requests>=2.26.0,<3.0.0',
+ 'scipy>=1.7.1,<2.0.0',
+ 'stochastic==0.6.0',
+ 'tqdm==4.61.0',
+ 'typing-extensions==3.7.4.3']
+
+setup_kwargs = {
+    'name': 'cadlabs-ethereum-economic-model',
+    'version': '0.0.6a0',
+    'description': '',
+    'long_description': None,
+    'author': 'BenSchZA',
+    'author_email': 'BenSchZA@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
