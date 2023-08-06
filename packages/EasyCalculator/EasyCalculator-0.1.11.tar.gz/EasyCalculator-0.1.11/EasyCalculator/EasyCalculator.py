49ac99f707a9fc410a76616baf287be2a15c724f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:55 +0000

    Add package EasyCalculator/0.1.11/EasyCalculator-0.1.11.tar.gz

---
+++ packages/EasyCalculator/EasyCalculator-0.1.11.tar.gz/EasyCalculator-0.1.11/EasyCalculator/EasyCalculator.py
@@ -0,0 +1,46 @@
+
+def add_two_numbers(x, y):     
+    """calculates the sum of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: sum of x and y
+    """      
+    #return _private__function_add(x, y)
+    return x+y
+
+
+def subtract_two_numbers(x,y):
+    """calculates the subtraction of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: Subtracted value of x and y
+    """  
+    #return _private__function_sub(x, y)
+    return (x-y)
+
+
+def multiply_two_numbers(x,y):
+    """calculates the multiplication of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: multiplied x and y
+    """  
+   # return _private__function_multiply(x, y)
+    return x*y
+
+def divide_two_numbers(x,y):
+    """calculates the division of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: Quotient of x and y
+    """  
+   # return _private__function_divivde(x, y)
+    return (x/y)
---
+++ packages/EasyCalculator/EasyCalculator-0.1.11.tar.gz/EasyCalculator-0.1.11/README.md
@@ -0,0 +1,12 @@
+Easy Calculator
+
+Add, Subtract, multiply and divide any two numbers.
+
+Usage:
+
+import EasyCalculator
+
+print(add_two_numbers(3, 2))
+print(subtract_two_numbers(3, 2))
+print(multiply_two_numbers(3, 2))
+print(divide_two_numbers(3, 2))
