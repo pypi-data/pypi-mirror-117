a75b7757294de29b56c84a05e26a4ae1db918a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package poetry-vendoring/0.1.1/poetry-vendoring-0.1.1.tar.gz

---
+++ packages/poetry-vendoring/poetry-vendoring-0.1.1.tar.gz/poetry-vendoring-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_vendoring']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry>=1.1.8,<2.0.0']
+
+entry_points = \
+{'poetry.application.plugin': ['vendor = poetry_vendoring.plugin:VendorPlugin']}
+
+setup_kwargs = {
+    'name': 'poetry-vendoring',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'mohamedarzikiwm',
+    'author_email': 'mohamed.a@wemaintain.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
