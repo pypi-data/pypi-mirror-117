d9f81fbf141e825c8f64141199077cd1aebf6bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:33 +0000

    Add package jspeechrecognizer/0.2-beta/jspeechrecognizer-0.2-beta.tar.gz

---
+++ packages/jspeechrecognizer/jspeechrecognizer-0.2-beta.tar.gz/jspeechrecognizer-0.2-beta/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+    name='jspeechrecognizer',
+    packages=['jspeechrecognizer'], 
+    version='0.2-beta', 
+    license='apache-2.0',
+    description='A Speech Recognition library that combines wakeword detection, speech recognition, and voice activity detection.',
+    author='Philippe Mathew',
+    author_email='philmattdev@gmail.com',
+    url='https://github.com/bossauh/JSpeechRecognizer',
+    download_url='https://github.com/bossauh/JSpeechRecognizer/archive/refs/tags/v_0.2-beta.tar.gz',
+    keywords=['speech', 'recognition', 'vad', 'wakeword'],
+    install_requires=[
+        'pvporcupine',
+        'vosk',
+        'speechrecognition',
+        'wave',
+        'sounddevice',
+        'tensorflow',
+        'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta', # 3=Alpha | 4=Beta | 5=Production
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
