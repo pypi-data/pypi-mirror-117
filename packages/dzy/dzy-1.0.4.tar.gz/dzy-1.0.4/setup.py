fc6e1fdf7d553ac09bc1c0713226e0d0aeff86b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:09 +0000

    Add package dzy/1.0.4/dzy-1.0.4.tar.gz

---
+++ packages/dzy/dzy-1.0.4.tar.gz/dzy-1.0.4/setup.py
@@ -0,0 +1,14 @@
+"""
+Setup dzy package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="dzy",
+    version="1.0.4",
+    description="dzy Python package",
+    author="dzy",
+    packages=find_packages(),
+    python_requires=">=3.6"
+    # install_requires=[""]
+)
