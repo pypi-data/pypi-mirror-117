0e91d21a6d730607daa545e5e1300a5b198613d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:38 +0000

    Add package tkinter-expansion/0.1/tkinter_expansion-0.1.tar.gz

---
+++ packages/tkinter-expansion/tkinter_expansion-0.1.tar.gz/tkinter_expansion-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as readme_file:
+  readme = readme_file.read()
+
+with open("dependencies.txt") as f:
+  requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+setup(
+  install_requires=requirements,
+  packages=find_packages(),
+  name='tkinter_expansion',
+  version='0.1',
+  license='MIT',
+  description='Python package that extends tkinter with custom Designer',
+  long_description_content_type="text/markdown",
+  long_description=readme,
+  url="https://fire-the-fox.github.io/tkinter_expansion_docs/",
+  download_url="https://github.com/Fire-The-Fox/tkinter_expansion",
+  author='Fire-The-Fox',
+  author_email='gajdos.jan77@gmail.com',
+  keywords=['Tkinter', 'expansion', 'designer', 'themes'],
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
