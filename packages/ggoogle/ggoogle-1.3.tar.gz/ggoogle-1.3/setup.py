429e8153755d9eca4197a36e64900c2bc33768b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:41 +0000

    Add package ggoogle/1.3/ggoogle-1.3.tar.gz

---
+++ packages/ggoogle/ggoogle-1.3.tar.gz/ggoogle-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+from pkg_resources import parse_requirements
+import utils
+
+
+setup(
+    name='ggoogle',
+    version=utils.__version__,
+    packages=find_packages(),
+    install_requires = [
+        'click==8.0.1',
+        'ggoogle==1.0',
+        'selenium==3.141.0',
+        'urllib3==1.26.6',
+    ],
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type = 'text/markdown',
+    entry_points={
+        'console_scripts':
+        ['g = utils.__main__:search_in_google']
+    },
+)
