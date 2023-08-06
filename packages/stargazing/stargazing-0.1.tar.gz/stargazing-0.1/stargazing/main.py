251a54d57c04d903106326d83335dcbbe4ab48dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:20 +0000

    Add package stargazing/0.1/stargazing-0.1.tar.gz

---
+++ packages/stargazing/stargazing-0.1.tar.gz/stargazing-0.1/stargazing/main.py
@@ -0,0 +1,14 @@
+from stargazing import Stargazing
+
+__all__ = ["main"]
+
+
+def main():
+    """Main entry point for script"""
+
+    stargazing = Stargazing()
+    stargazing.start()
+
+
+if __name__ == "__main__":
+    main()
