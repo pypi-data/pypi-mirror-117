4a2d6feb089224bd14d143de9129f8345f90e09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:46 +0000

    Add package kisaan/0.0.3/kisaan-0.0.3.tar.gz

---
+++ packages/kisaan/kisaan-0.0.3.tar.gz/kisaan-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='kisaan',
+  version='0.0.3',
+  description='A very basic discription',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Joshua Lowe',
+  author_email='josh@edublocks.org',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords="kisaan",
+  packages=find_packages(),
+  install_requires=["wheel","pymongo[srv]"]
+  
+)
