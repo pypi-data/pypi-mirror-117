052d6dcbaa2cea7db3d1dd7b2e5763bb4a07dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:02 +0000

    Add package packagenamemarcoshr/0.1/packagenamemarcoshr-0.1.tar.gz

---
+++ packages/packagenamemarcoshr/packagenamemarcoshr-0.1.tar.gz/packagenamemarcoshr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='packagenamemarcoshr',
+      version='0.1',
+      description='prueba prueba',
+      packages=['packagenamemarcoshr'],
+      zip_safe=False)
