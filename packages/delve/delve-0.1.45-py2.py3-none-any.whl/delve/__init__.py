f66fff67db71545d012385c573edfe87cf1a9ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:09 +0000

    Add package delve/0.1.45/delve-0.1.45-py2.py3-none-any.whl

---
+++ packages/delve/delve-0.1.45-py2.py3-none-any.whl/delve/__init__.py
@@ -0,0 +1,21 @@
+try:
+    import torch
+    from delve.torchcallback import CheckLayerSat
+    from delve.tools import reconstruct_csv_from_npy_data
+except ImportError:
+    pass
+name = "delve"
+
+try:
+    from delve.writers import PrintWriter as console
+    from delve.writers import NPYWriter as npy
+    from delve.writers import CSVWriter as csv
+    from delve.writers import CSVandPlottingWriter as plot
+    from delve.writers import CSVandPlottingWriter as plotcsv
+
+    from delve.writers import CSVandPlottingWriter as csvplot
+    from delve.writers import TensorBoardWriter as tensorboard
+except ImportError:
+    pass
+
+__version__ = "0.1.45"
