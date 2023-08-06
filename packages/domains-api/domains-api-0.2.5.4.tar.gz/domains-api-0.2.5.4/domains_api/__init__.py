194a80d91ae510a436be3a8d28bfb95892ef26c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:50 +0000

    Add package domains-api/0.2.5.4/domains-api-0.2.5.4.tar.gz

---
+++ packages/domains-api/domains-api-0.2.5.4.tar.gz/domains-api-0.2.5.4/domains_api/__init__.py
@@ -0,0 +1,9 @@
+# import sys
+#
+# from .file_handlers import FileHandlers
+# from .ip_checker import BaseUser, IPChecker
+# from .ip_changer import DomainsUser, IPChanger
+#
+#
+# if __name__ == '__main__':
+#     IPChanger(sys.argv[1:])
