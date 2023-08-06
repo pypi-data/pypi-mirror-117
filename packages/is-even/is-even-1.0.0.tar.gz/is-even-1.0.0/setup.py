6afeee5a05c178e79bbbd7e694f033160ff11993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package is-even/1.0.0/is-even-1.0.0.tar.gz

---
+++ packages/is-even/is-even-1.0.0.tar.gz/is-even-1.0.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='is-even',
+    version='1.0.0',
+    description='Return true if the given number is even.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Victor B",
+    author_email="victor.bonnelle@protonmail.com",
+    maintainer="Victor B",
+    maintainer_email="victor.bonnelle@protonmail.com",
+    url='https://github.com/victorbnl/is-even/',
+    packages=[
+        'is_even'
+    ]
+)
