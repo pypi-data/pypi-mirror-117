720c4d3a01a8906a82da787277d495cd7495c0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:50 +0000

    Add package primitives/1.4.0/primitives-1.4.0.tar.gz

---
+++ packages/primitives/primitives-1.4.0.tar.gz/primitives-1.4.0/src/primitives/__init__.py
@@ -0,0 +1,8 @@
+"""Fake objects designed with OOP in mind."""
+from _primitives.argument import Argument
+from _primitives.awaitable import Awaitable
+from _primitives.callable import Callable
+from _primitives.instance import Instance
+
+
+__all__ = ["Argument", "Awaitable", "Callable", "Instance"]
