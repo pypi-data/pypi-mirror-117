20f9f09e027f90c205fcc8e987e42128126845db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:46 +0000

    Add package crossedwires/0.0.5/crossedwires-0.0.5-py3-none-any.whl

---
+++ packages/crossedwires/crossedwires-0.0.5-py3-none-any.whl/crossedwires/cifar10/__init__.py
@@ -0,0 +1,5 @@
+from .vgg16 import VGG16Dataset
+from .resnet50 import ResNet50Dataset
+from .densenet121 import DenseNet121Dataset
+
+__all__ = ["VGG16Dataset", "ResNet50Dataset", "DenseNet121Dataset"]
