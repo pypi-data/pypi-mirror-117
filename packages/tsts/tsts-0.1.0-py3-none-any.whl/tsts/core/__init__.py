8f4207f470ec1539d0ac1837bbf3f4a528557e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:45 +0000

    Add package tsts/0.1.0/tsts-0.1.0-py3-none-any.whl

---
+++ packages/tsts/tsts-0.1.0-py3-none-any.whl/tsts/core/__init__.py
@@ -0,0 +1,25 @@
+from tsts.core.registry import (
+    COLLATORS,
+    DATALOADERS,
+    DATASETS,
+    LOGGERS,
+    LOSSES,
+    METRICS,
+    MODELS,
+    OPTIMIZERS,
+    SCALERS,
+    TRAINERS,
+)
+
+__all__ = [
+    "COLLATORS",
+    "DATALOADERS",
+    "DATASETS",
+    "LOGGERS",
+    "LOSSES",
+    "METRICS",
+    "MODELS",
+    "OPTIMIZERS",
+    "SCALERS",
+    "TRAINERS",
+]
