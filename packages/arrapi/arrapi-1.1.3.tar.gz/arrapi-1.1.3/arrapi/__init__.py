f78100fb0fcd7655f257d1c846874e8c7a7f9fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:32 +0000

    Add package arrapi/1.1.3/arrapi-1.1.3.tar.gz

---
+++ packages/arrapi/arrapi-1.1.3.tar.gz/arrapi-1.1.3/arrapi/__init__.py
@@ -0,0 +1,37 @@
+from .exceptions import ArrException, ConnectionFailure, Exists, Invalid, NotFound, Unauthorized
+from .objs import QualityProfile, LanguageProfile, MetadataProfile, RemotePathMapping, RootFolder, UnmappedFolder, SystemStatus, Tag, Movie, Series, Season
+from .sonarr import SonarrAPI
+from .radarr import RadarrAPI
+from .lidarr import LidarrAPI
+from .readarr import ReadarrAPI
+
+__version__ = "1.1.3"
+__author__ = "Nathan Taggart"
+__credits__ = "meisnate12"
+__package_name__ = "arrapi"
+__project_name__ = "ArrAPI"
+__description__ = "Python wrapper for Radarr and Sonarr APIs:"
+__url__ = "https://github.com/meisnate12/ArrAPI"
+__email__ = 'meisnate12@gmail.com'
+__license__ = 'MIT License'
+__all__ = [
+    "RadarrAPI",
+    "SonarrAPI",
+    "QualityProfile",
+    "LanguageProfile",
+    "MetadataProfile",
+    "RemotePathMapping",
+    "RootFolder",
+    "UnmappedFolder",
+    "SystemStatus",
+    "Tag",
+    "Movie",
+    "Series",
+    "Season",
+    "ArrException",
+    "ConnectionFailure",
+    "Exists",
+    "Invalid",
+    "NotFound",
+    "Unauthorized"
+]
