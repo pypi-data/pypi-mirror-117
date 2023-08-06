6f60eecacc3605e9dbd8cb35bb08308db3b79dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:49 +0000

    Add package merakitools/0.1.1/merakitools-0.1.1.tar.gz

---
+++ packages/merakitools/merakitools-0.1.1.tar.gz/merakitools-0.1.1/merakitools/main.py
@@ -0,0 +1,21 @@
+"""
+merakitools - main.py
+Billy Zoellers
+
+CLI tools for managing Meraki networks based on Typer
+"""
+
+# Python 3.9+ is required
+import sys
+MIN_PYTHON = (3, 9)
+if sys.version_info < MIN_PYTHON:
+  sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
+import typer
+
+from merakitools import orgs, networks, devices
+
+app = typer.Typer()
+app.add_typer(orgs.app, name="orgs", help="Meraki organizations")
+app.add_typer(networks.app, name="networks", help="Meraki networks")
+app.add_typer(devices.app, name="devices", help="Meraki devices")
