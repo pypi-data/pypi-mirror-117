2a6a8a6c415a42345a865ac2a96d3a3a14f5ac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:41 +0000

    Add package approvaltests/2.0.0/approvaltests-2.0.0-py3-none-any.whl

---
+++ packages/approvaltests/approvaltests-2.0.0-py3-none-any.whl/approvaltests/reporters/report_with_beyond_compare.py
@@ -0,0 +1,57 @@
+from approvaltests.reporters.first_working_reporter import FirstWorkingReporter
+from approvaltests.reporters.generic_diff_reporter import (
+    GenericDiffReporter,
+    GenericDiffReporterConfig,
+)
+
+
+class ReportWithBeyondCompareLinux(GenericDiffReporter):
+    def __init__(self):
+        super().__init__(
+            config=GenericDiffReporterConfig(
+                name=self.__class__.__name__, path="/usr/bin/bcompare"
+            )
+        )
+
+
+class ReportWithBeyondCompareMac(GenericDiffReporter):
+    def __init__(self):
+        super().__init__(
+            config=GenericDiffReporterConfig(
+                name=self.__class__.__name__, path="/usr/bin/bcompare"
+            )
+        )
+
+
+class ReportWithBeyondCompareWindows(GenericDiffReporter):
+    def __init__(self):
+        super().__init__(
+            config=GenericDiffReporterConfig(
+                name=self.__class__.__name__,
+                path="{ProgramFiles}/Beyond Compare 4/BCompare.exe",
+            )
+        )
+
+
+class ReportWithPycharm(GenericDiffReporter):
+    def __init__(self):
+        super().__init__(
+            config=GenericDiffReporterConfig(
+                name=self.__class__.__name__,
+                path="{ProgramFiles}/JetBrains/PyCharm 2021.1.1/bin/pycharm64.exe",
+                extra_args=["diff"],
+            )
+        )
+
+
+class ReportWithBeyondCompare(FirstWorkingReporter):
+    def __init__(self):
+        super().__init__(
+            ReportWithBeyondCompareMac(),
+            ReportWithBeyondCompareWindows(),
+            ReportWithBeyondCompareLinux(),
+        )
+
+
+def report_with_beyond_compare() -> ReportWithBeyondCompare:
+    return ReportWithBeyondCompare()
