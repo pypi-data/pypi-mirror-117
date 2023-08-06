b9c7767b11f5cd54b85b041b4e1fa7fa7dc4a42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:40 +0000

    Add package kisaan-shopping/0.1.1/kisaan-shopping-0.1.1.tar.gz

---
+++ packages/kisaan-shopping/kisaan-shopping-0.1.1.tar.gz/kisaan-shopping-0.1.1/setup.py
@@ -0,0 +1,27 @@
+# from os import name
+from setuptools import find_packages,setup
+setup(
+	name="kisaan-shopping",
+	packages=["kisaan-shopping"],
+	install_requires=["pymongo[srv]","json","datetime"],
+	version="0.1.1",
+	description="This is kisaan_shopping_app utils",
+	long_description="this too much long description",
+	author="Aditya",
+	author_email="mightybros98@outllok.com",
+	url="https://www.kisaan.com.au/",
+	download_url="https://www.w3school.com",
+	keywords=["kisaan_shopping_app_utils","https://www.kisaan.com.au/","stable",
+	"kisaan_shopping"],
+	license="MIT",
+	classifiers=[
+    	'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    	'Intended Audience :: Developers',      # Define that your audience are developers
+    	'Topic :: Software Development :: Build Tools',
+    	'License :: OSI Approved :: MIT License',   # Again, pick a license
+    	'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    	'Programming Language :: Python :: 3.4',
+    	'Programming Language :: Python :: 3.5',
+    	'Programming Language :: Python :: 3.6',
+  ],	
+)
