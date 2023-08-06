b7ebfdd6b9bf11e58689e571dda315a9ef5dde0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:28 +0000

    Add package alertnow-python/1.1.3/alertnow_python-1.1.3-py3-none-any.whl

---
+++ packages/alertnow-python/alertnow_python-1.1.3-py3-none-any.whl/logger.py
@@ -0,0 +1,37 @@
+import json
+import requests
+
+from common.configuration.httpClientConfig import HttClientConfig
+from common.dto import LogCreateDto
+from common.dto import LogRequestDto
+from common.enums.logLevel import LogLevel
+from common.enums.platform import Platform
+
+
+with requests.Session() as session:
+    session.headers.update({
+        'Content-Type': 'application/json',
+    })
+
+    def init(api_key):
+        response = session.post(f'{HttClientConfig.alertNowURL}/users/sign-in-with-api-key/{api_key}')
+        if response.status_code != 200:
+            return response
+
+        session.headers.update({
+            'Authorization': f'Bearer {response.text}'
+        })
+
+        return 'Connection was initialized successfully'
+
+    def info(message):
+        return create_log(LogCreateDto(LogLevel.INFO.value, message))
+
+    def error(occurred_error):
+        return create_log(LogCreateDto(LogLevel.ERROR.value, json.dumps(vars(occurred_error))))
+
+    def create_log(log_create_dto):
+        log_request_dto = LogRequestDto(Platform.PYTHON.value, json.dumps(vars(log_create_dto)))
+        response = session.post(f'{HttClientConfig.alertNowURL}/logs/create',
+                                data=json.dumps(vars(log_request_dto)))
+        return response
