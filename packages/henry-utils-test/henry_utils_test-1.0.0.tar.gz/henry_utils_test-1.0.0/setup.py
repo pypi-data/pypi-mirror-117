04ca1f325f37eb911a55ed79d854e35478cf46e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:26 +0000

    Add package henry-utils-test/1.0.0/henry_utils_test-1.0.0.tar.gz

---
+++ packages/henry-utils-test/henry_utils_test-1.0.0.tar.gz/henry_utils_test-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='henry_utils_test',
+    version='1.0.0',
+    author='Henry Liu',
+    author_email='2224546920@qq.com',
+    maintainer='Henry Liu',
+    description='to automatic create spark sql file ',
+    packages=['henry_utils', 'henry_utils.sqlserver'],
+    requires=['pymssql'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Utilities'
+    ]
+)
