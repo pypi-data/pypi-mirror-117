7cd8c8089cfa0925838024274fff363cf161271d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:46 +0000

    Add package graphgt/0.1.1/graphgt-0.1.1.tar.gz

---
+++ packages/graphgt/graphgt-0.1.1.tar.gz/graphgt-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+      name='graphgt',
+      version='0.1.1',
+      description='A Graph Generation and Transformation Dataset Collection',
+      url='https://graphgt.github.io/',
+      author='Yuanqi Du',
+      author_email='ydu6@gmu.edu',
+      packages=['graphgt'],
+      install_requires=['numpy'],
+      
+      classifiers=[
+                   'Intended Audience :: Science/Research',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3.5',
+                   ],
+      )
