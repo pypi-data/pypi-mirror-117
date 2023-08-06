a8923330ddc45662bfd291e6a23cc74122a22a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:04 +0000

    Add package console-debugger/0.1.7.3/console_debugger-0.1.7.3.tar.gz

---
+++ packages/console-debugger/console_debugger-0.1.7.3.tar.gz/console_debugger-0.1.7.3/console_debugger/logic/__init__.py
@@ -0,0 +1,10 @@
+from os.path import dirname
+from sys import path
+
+dirs = dirname(__file__).replace("\\", "/").split("/")[:-2]
+p = "/".join(dirs)
+if p not in path:
+	path.append(p)
+
+
+from console_debugger.logic.debugger import Debugger
