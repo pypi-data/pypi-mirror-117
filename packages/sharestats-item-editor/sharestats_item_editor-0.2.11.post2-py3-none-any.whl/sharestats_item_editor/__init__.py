390dde738a96d4e7c98957faeb05f1702b546d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:22 +0000

    Add package sharestats-item-editor/0.2.11.post2/sharestats_item_editor-0.2.11.post2-py3-none-any.whl

---
+++ packages/sharestats-item-editor/sharestats_item_editor-0.2.11.post2-py3-none-any.whl/sharestats_item_editor/__init__.py
@@ -0,0 +1,17 @@
+"""
+This module contains all modification of the item editor for the stats share
+projects.
+
+(c) O. Lindemann
+"""
+
+__author__ = "Oliver Lindemann"
+
+# set appname and version
+from . import rexam_item_editor
+rexam_item_editor.__version__ += "-2"
+__version__ = rexam_item_editor.__version__
+APPNAME = "ShareStats Item Editor"
+WEBSITE = "https://github.com/essb-mt-section/sharestats-item-editor"
+
+
