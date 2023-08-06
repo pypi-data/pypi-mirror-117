1f21acff94f96909b1bd8500c9a38fe92147e9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:37 +0000

    Add package bybitWrapper/0.0.2/bybitWrapper-0.0.2.tar.gz

---
+++ packages/bybitWrapper/bybitWrapper-0.0.2.tar.gz/bybitWrapper-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='bybitWrapper',
+  version='0.0.2',
+  description='A simple wrapper for the Bybit API',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='bybit', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
