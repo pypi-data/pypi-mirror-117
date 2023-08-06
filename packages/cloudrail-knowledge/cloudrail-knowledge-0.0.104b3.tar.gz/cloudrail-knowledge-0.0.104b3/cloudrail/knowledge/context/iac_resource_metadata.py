1594b7edbe16d17db088c2ace51b48e5581d72d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:48 +0000

    Add package cloudrail-knowledge/0.0.104b3/cloudrail-knowledge-0.0.104b3.tar.gz

---
+++ packages/cloudrail-knowledge/cloudrail-knowledge-0.0.104b3.tar.gz/cloudrail-knowledge-0.0.104b3/cloudrail/knowledge/context/iac_resource_metadata.py
@@ -0,0 +1,18 @@
+import uuid
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class IacResourceMetadata:
+    iac_entity_id: str
+    file_name: str
+    start_line: int
+    end_line: int
+    module_metadata: Optional['IacResourceMetadata'] = None
+    id: Optional[str] = None
+    resource_type: Optional[str] = None
+    run_execution_id: str = None
+
+    def __post_init__(self):
+        self.id = self.id or str(uuid.uuid4())
