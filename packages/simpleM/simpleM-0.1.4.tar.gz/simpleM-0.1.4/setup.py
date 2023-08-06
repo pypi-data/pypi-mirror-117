4e752ad69840d16602f86eb3554f83438a1dec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package simpleM/0.1.4/simpleM-0.1.4.tar.gz

---
+++ packages/simpleM/simpleM-0.1.4.tar.gz/simpleM-0.1.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+
+VERSION = '0.1.4' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="simpleM", 
+        version=VERSION,
+        author="qiujingyu",
+        author_email="<youremail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'first package'],
+        data_files=[('.simpleM', ['doc/a/b.txt'])],
+        package_data={'': ['1234.txt']},
+
+        entry_points = {
+            'console_scripts': ['simpleM=simpleM.display:f'],
+        }
+
+)
