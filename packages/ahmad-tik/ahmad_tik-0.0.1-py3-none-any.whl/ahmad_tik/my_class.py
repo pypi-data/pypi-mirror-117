a16249f427c368ef22ba385eddf51531668cbbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:39 +0000

    Add package ahmad-tik/0.0.1/ahmad_tik-0.0.1-py3-none-any.whl

---
+++ packages/ahmad-tik/ahmad_tik-0.0.1-py3-none-any.whl/ahmad_tik/my_class.py
@@ -0,0 +1,128 @@
+class myclass:
+	def ahmadtik():
+		import requests
+		from threading import Thread
+		import time 
+		print("SNAPCHAT ::: ahmad20l")
+		time.sleep(2)
+		
+		print(" /||_||\/| /||\.")
+		print("/-|| ||  |/-||/")
+		
+		print(""""""""""""""""
+		       ╟▓▓█▌,»▒░▄╣▓▓▓╣▒
+		       ▐█████░eç██████▌
+		       ]█████░║╠▐█████.``
+		       `████▌║▒▒/████▌ⁿ```
+		        ▐███░║▒▒▒████ⁿⁿ.```
+		   «``` ╙███╠║▒▒▒▐██░`{░;`
+		    »`` `██▌║║▒▒▒░█▌ⁿ╝@╘░`
+		     %```/█▒╠║║▒▒░█ⁿ`'ⁿⁿ░
+		      %` `╟░╚╠╠║╠░░``ⁿⁿ░``
+		       `` ` «╠╠╠▒░``ⁿ«```
+		        `   `=╠░░``ⁿ« ░``
+		             «=░``ⁿⁿ«ⁿⁿ``
+		              ░``` «```
+		               `   ``
+		 
+		 
+		             ╓#
+		              "
+		 """"""""""""""""""""")
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		time.sleep(2)
+		 
+		user = input("+++++ ادخل اسم لستت اليوزرات +++++ : ")
+		userList = open(user).read().splitlines()
+		n = len(userList) // 2
+		x = userList[:n]
+		y = userList[n:]
+		
+		def ahmad(alist):
+			for user in alist:
+				
+				burp0_url = f"https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/unique/id/check/?residence=SA&device_id=6964739416456824322&os_version=14.2.1&iid=6975586247558596357&app_name=musical_ly&locale=ar&ac=mobile&sys_region=SA&js_sdk_version=1.77.0.2&version_code=19.5.1&vid=2D85DC48-9130-4B7E-B421-520D8F480FA4&channel=App%20Store&op_region=US-PR&tma_jssdk_version=1.77.0.2&os_api=18&idfa=D30F1D71-0811-4836-BAEA-8364430A94A3&device_platform=iphone&device_type=iPhone13,4&carrier_region1=US-PR&tz_name=Asia/Riyadh&account_region=&build_number=195106&tz_offset=10800&app_language=ar&carrier_region=US-PR&current_region=SA&aid=1233&mcc_mnc=42004&screen_width=1284&uoo=0&content_language=&language=ar&cdid=23101553-AC7B-4EA3-99B1-DF426D0EA4C7&openudid=ecf8bb45108368149adc7ad945b631488bc7be04&app_version=19.5.1&unique_id={user}"
+				burp0_cookies = {
+					"residence":"SA",
+					"device_id":"6964739416456824322",
+					"os_version":"14.2.1",
+					"iid":"6975586247558596357",
+					"app_name":"musical_ly",
+					"cdid":"23101553-AC7B-4EA3-99B1-DF426D0EA4C7",
+					
+					"odin_tt":"ecf6df99e116be53a2e89afb443b1ef72c56d555ec17cb4ac93645e58b18f49cadb6aee0e2ce7881a90183428006f632037cd1c62c3f5e9bd0df800960205b8b0f4aa45e452b622b326477e173de98c8",
+			"cmpl_token":"AgQQAPO_F-RMpbCZVw6aO507-qAY8N4RP4A0YP38pA",
+			"sessionid":"8757999e80ce400d47adaa45844e31cb",
+			"sessionid_ssf":"057e16038a7c2065bc1d37481159571",
+			"sid_guard":"f057e16038a7c2065bc1d37481159571%7C1624062448%7C5184000%7CWed%2C+18-Aug-2021+00%3A27%3A28+GMT",
+			"sid_tt":"f057e16038a7c2065bc1d37481159571",
+			"store-country-code":"ca",
+			"store-idc":"maliva",
+			"uid_tt":"32e685ff0371f6dc34c388359296ff4c0e1688880bd60facfe8a63a0bf0e68e8",
+			"uid_tt_ss":"32e685ff0371f6dc34c388359296ff4c0e1688880bd60facfe8a63a0bf0e68e8",
+			"MONITOR_WEB_ID":"verify_kq30pky0_jmphCyNX_hRWn_42Xr_9Amn_f0DMQszH3Oh0",
+			"ttwid":"1%7CkfMIQA2j3otw9Q6KWrnLrilSpHxy0YwhiTQPhPzE_kU%7C1624062317%7C7e380bf91d21a968f4b15a3560c8942450de8095b32286a80c55604ef8f4d582",
+			"csrf_session_id":"7c37dccdaac744c5bb860edc90d919af",
+			"bm_sv":"A124C2E1D225C4493CDF7737550A0154~uF+kFPdRUchiO9LSFAv21HwAtmgi2S8ye1wXv9PkWEn+pzDWau9BzO6/xH1CPF4jJVB8ijrM69cCvmvQm5qU3Mf6PZ1BxTtrP1/9EjwmLEmN7kWVAL54PmJGgv6aMD2DPxXNX2jXR1WcIp+CNNFwb0+CH09fsLcjb8x2EnATWuM=",
+			"_abck":"59F8F8E729DE36361D753234059ACD41~-1~YAAQZCHfFxujkxJ6AQAAEcSmIQZ9zGUWH55SvM7MrUbBNtZpsFKjb/hUAgdyH1IxuFKdcAx4TWTfrDVyNnqdP3/bipn4Y9SjbfFF06auZRvKDREJQMD2P8+qFs0jSbkMEj4acFtQBtxNXUY0pbA6LHuiPuoJadNuUQ7iPAZs63mgNEm7Z/Slx7Q5ggbwnELa3a0iDFkKvEwyj1NlDqbzYIm2voHj5d8n/vYMKKKSLADtfZfF6u6An+HbePnomp271OJ9gub1yGmRlPaO32BL/+E0xZg839Sr0pewgOFS+tyFznKL33f3/LIniWLvRt0MjPokFGI57wb3chzxG2IOhtUxP9BvPZoD0Mk27lrsFDfQmiwwfAvOOC79AvjrRYMn2XuLtiGfHbo/ATc=~-1~-1~-1",
+			"ak_bmsc":"E99F777564B58BB95A705D72F0BCC21B~000000000000000000000000000000~YAAQZCHfFxyjkxJ6AQAAEcSmIQz7JE6NApp9gJk8lphWp+wtzFaMG/XMTRxiuqHPYYUq2cu38vbLU45IXpZm68SE/euXFqTbZwIeB0qm9onXvR7cGsBg9u9b3qWn2Mv57WB6Mx37rPBBsuQbwl+6irMFcWkNFoiDTzkfBI1eumX25U16IymPc9B8gdvySgKdcl5cye4P8KuOz1tdb5GUVlSPEfbtWajbAU8gZ1/Hr+gwbJVWaNR36cbKjBa5aJxS8Q4AqcTrOLlkBrONm16rVbeuRKDIkAiu4FzXSSfPibf84sODwdqO9kEFNTumEMpsh5Di2LvroWXuOHLa3u059kGIXwqj8iZGsOvFg5er/gC6lvxd9am3X5EM/nd+pg==",
+			"bm_sz":"3D8771567477178767828DB21C907352~YAAQZCHfFxqjkxJ6AQAAEcSmIQzZZ5g1MdmfcLCwpxfKGIbxm4OFYpMgc58JDnCkaGqjVCpaR4zcM3tZy5xJt+9PkqVOSP5I9yaS7CVaw7U2OiLQ8L9EQT1BKGWzssBee0n0N16lJtAvbCIKLXmNeRzu2/oS4i1fMN2NyI6N3XYA+SJEHgHUpsM8Spz0rHnr",
+			"csrf_session_id":"7c37dccdaac744c5bb860edc90d919af",
+			"s_v_web_id":"verify_kq30pky0_jmphCyNX_hRWn_42Xr_9Amn_f0DMQszH3Oh0",
+			"R6kq3TV7":"AKG8piF6AQAAj8mY-QnOA54bpWH0X_Ixwkbw6Fntho7-grZ9KGcPHROZA_U0|1|0|1c54166a25d64699e465fae9ed79b1cb703a9f72",
+			"tt_csrf_token":"T7VOkBPIIRWcVlRCN2f_SRVv",
+			"passport_auth_status":"861e1816d3f56578411186685d551c3c%2C0f1a97fc3a18265faa776377c3d662a2",
+			"passport_auth_status_ss":"861e1816d3f56578411186685d551c3c%2C0f1a97fc3a18265faa776377c3d662a2",
+			"passport_csrf_token":"a25f0e44181d47d42dfc1ec3c3e12af1;passport_csrf_token_default=a25f0e44181d47d42dfc1ec3c3e12af1",
+			"tt_webid":"6971994816205981185",
+			"tt_webid_v2":"6971994816205981185"
+					
+					}
+				burp0_headers = {
+				
+				"User-Agent": "5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1",
+				"Accept": "*/*",
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Referer": "https://www.tiktok.com/signup/create-username?lang=ar",
+				"x-secsdk-csrf-token": "0001000000018cec2e857dc66b2dcbe5458a73757ec7ed82fed8dce08c5cf6b9f864e3a94f6e1689d3fd70c81014",
+				"X-Mssdk-Info": "r9v8VJUSMoA3gyowcyGEzYdExCsDAmN2ZK0JD3vVbKWslWqFt6fW8aFZqq-KVqu-EdSHh6bcPV9pxALNjGlNVS5np8zosdRWJAgBughxrNT-go86N96we9h97R32wqtrDTl-1UfiiZyz6Ya00OmPKTfM9sJkKJ7xDuxqeHQa-JCmalbG1SgFI13EzsHfJcXK37QT7XpbuSp4PtD9WiUPfjtMc954pRmKjmMshdFuURH3Fvop-fcsP-M9yeF-8E9PSxN9dYAWetktIKIIqKtC86XXef2.QJBv8RKt0COVmvky.eXsmT7q3GEHXIUzO18LaObzooY9K5KbTbVntahG2qg4QI8OFPcLzcDLTDKNPKI-N57L8nj8EhbV6.kQRtIZMqE8JcdEwChUYq-B0teQpHAueuASzMU43Juh5hXG1anr0Y8U.0BsTEc1z6-lLBa9HRAlBC4C6RJ63khjCjAo8EwoAXB5Y8jlLMJHrSU1k3Ma47WeCPwSFpipeMMO9FD34jrIM29ZHw5uaUhREZE7fk2bCh82H9aPjOJT4Fl7fxWWRZfPOA1XnkjV42DjZDJ1eCnGkJ7dWKFcUeD9MdwwIUUzVKJMn5pyc-Bmgj1y8rSZD7IgaLFCqIAXFY8CNp3CFEZFaA0L3ZeFf6m2snJSg3drQm6v3oSCpUUeKsnD7B47g9XrLtO68tO4qTGp.wa1iJfA3c1f9zIsM73XiJzSJxSdtewIPR4VoBsNVCmiXI4uW9RvP55p13IQyoMWQkaIQxmZDBiEkO0XHkcwLM7yrc4sq9M0r.GHvyun.l1TbxAcwJEAA0ZzJDRp5qLXePpCX-7P1tpZKTTCOPQ2g5q20M5b260hgU85Db3oCP0U2zSIl7TLLjm4QvxD0d.bgg2Hft8v3z5XtFqkwEf2iU6QxGZQKq4rq3gzzdtheBdkc7rjEhvtqzP4qgLpDvTCj8KpNsPHbLTkkcTag1MMIv.k3IAAcKaC8m8eaFn8VOkK.rJOA0Taory6k30wspiH0bHbKKfbs1Zwdr2oWNJjOYC2vsCrKePc88jRb6vKLh-cc9WGLWYW3hA0.JHTGzIkcNakR3i6mlJuSNLUGrus4Z9u403rRm6pA.oEVT8id0YnpsdvTfUMVMCkBWevLu84YYcLV89rCaCEy9rRR2rUxCOP0nc-NwxlImUGU18aONHT382OP2edWMlcOEIqwBEXcjWC9IkBa0W7ukeZPkfWHGhKhrvspc5m56-0oWYIUIn0LUyPYYZEVZyz0j3-DmxMG0JVPktYWHwjes09Ba6eF.zsMear80HZSd2rAKKEuyaWXF8XaFMM0YdJLgAsaXc68u-wsHbFzPx96Ig0BJxesKCln4.b5KUBnuF7DgFB3qYkjjQbvqqpAApoYoE7VCyl6eKkv6yQNLdlLfZEZDwoIAeNBgnEdRmIM6Qck7cmRrUOrJaGU7vkwjGGe29rA6piQGUKYs5iABKPQqozhCgXYotzM9iocap78Uh5jgB6xYssAgn4scv3Qhx4.ZPsMSAdG6DaN.RXf2oyIqfZ1J87q19L3WT.iR-rAt8arxVB.9jVtffIXJy3LUqCRN2RKGhlQGjGEWIs18Zo.vQQX8IMtTuRCN.JOKtpkEy2eKOm2oLcbMr0xmCzj4k7zNktfMlnbmUPbDBHO2Vr0k02HrwUqPWqfcGkvKK2KoYmoRS6cdOdu9KkQFgofESK0YC7mzR25i5LC07ywr2i67rbglOpc79GG60XuRbb-d6hcxdL9Cnv1huH3H6UYbGYk4hna9Pa.maW5i23YRSsIB97jwK0jeKOenLMihHlqGelnX0l5X67lo-zrYNYyTUcXbBXMG8DJ3GHPc.tXnvZrYmm4iLnkZrpcsAUp8afbgAgR.K6OrpHD.q6q3xjaXpv49pdiuntHtT0UOBXpw5jNZWANZ157SFjSxlgGRQ3Oejnlna.Wul6np4rxMOIylnnZJ5UpJQkG6wfzbAWNLtXP19HYdMD9qfd6JJaEdRAeJTIFQJ6hvtuDIbX7635UttcT-y5rfAdUWPfHFdLAE6AKgfKZNehues.lNz7iTwi3WU3a8vzNFol6CW5s5v8Rix9NTO8qX1uu7obLr3-BvOFzumtn3yN9236O4o0PCX4AMJ8binewzVsm4d0SmMgCFkmBPDKMq2Gyzi6rKHLmo7oMQ3zRcjnpfq-2fcwfj8CCn-oCyHeIfsaKQoPd7wdeRdu5nD9uSOcrsKd6YrAgXHsr0qOdwohpKiM6W0G7NDajenLRa4tpvMwgVNnHIBezRu15kTmNQr6xE2Su8gLSqd16jQvWgWFTPk33GPmUDhq40L0SDnV9f6N0sqtVwCtFBfoS0UKsey3DqUrvt8xtyArRB3XUxbIIqT4nFPLAqb2vcGObJJ-kgpVLBfJYalpoTMHWLV8.TUthSOAK3aFmw8U015x21D2zeKrQ-BfMwWkqlWX5Ny8v4wCty04SUaQ1vDAGd.mprgRwAkblcjjjAzyh2DLa.aeNqfNwiwUhg=="
+			}
+			
+				r = requests.get(burp0_url,headers=burp0_headers, cookies=burp0_cookies, )
+				if r.text.find("true") >= 0:
+					print(" ***-----مــتـاح-----***")
+					with open('goodtiktok.txt', 'a')as x:
+						x.write(user +'\n')
+				elif  r.text.find("false") >= 0:
+					print("xxxxxغــير مــتـاحxxxxx") 
+				
+				
+					
+				else:
+					print("<<<<<سكر الاداه وارجع شغلها >>>>>")
+					
+					
+				#print(r.text)		
+				print(user)
+			
+			
+			
+		t = Thread(target=ahmad, args=(x,))
+		t2 = Thread(target=ahmad, args=(y,))
+		
+		t.start()
+		t2.start()
+		
+		t.join()
+		t2.join()
+	ahmadtik()
