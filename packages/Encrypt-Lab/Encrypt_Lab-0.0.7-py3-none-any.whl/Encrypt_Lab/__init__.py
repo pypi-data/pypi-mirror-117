4ee6dcb3a14bc6973b02c42d7707fe5d4b2bea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:14 +0000

    Add package Encrypt-Lab/0.0.7/Encrypt_Lab-0.0.7-py3-none-any.whl

---
+++ packages/Encrypt-Lab/Encrypt_Lab-0.0.7-py3-none-any.whl/Encrypt_Lab/__init__.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QLabel, QSizePolicy, QVBoxLayout,QHBoxLayout,QGridLayout, QWidget, QScrollArea, QGroupBox, QMainWindow, QScrollBar, QApplication
+import Encrypt_Lab.Sim
+
+import sys
+
+class Simulator:
+    @classmethod
+    def run(cls):
+        app = QApplication(sys.argv)
+
+        m = QMainWindow()
+        m.resize(1000, 1000)
+        w = Sim.Mod(m)
+
+        m.setCentralWidget(w)
+        m.show()
+
+        sys.exit(app.exec_())
+
