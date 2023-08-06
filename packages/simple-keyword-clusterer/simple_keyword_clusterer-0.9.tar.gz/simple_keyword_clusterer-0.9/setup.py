74957fd9584b610eaf75f771380b15a7d16f9e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:09 +0000

    Add package simple-keyword-clusterer/0.9/simple_keyword_clusterer-0.9.tar.gz

---
+++ packages/simple-keyword-clusterer/simple_keyword_clusterer-0.9.tar.gz/simple_keyword_clusterer-0.9/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description=f.read()
+
+
+setup(
+    name="simple_keyword_clusterer",
+    version=0.9,
+    url="https://github.com/Tangelus/simple_keyword_clusterer",
+    author="Andrea D'Agostino",
+    author_email="andrea@andreadagostino.com",
+    description="Extract higher level clusters from keywords",
+    keywords = ['keyword-extraction', 'keyword-clustering'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    py_modules=["simple_keyword_clusterer", "preprocessing", "clustering"],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        "scikit-learn",
+        "tqdm",
+        "seaborn",
+        "numpy",
+        "nltk",
+        "matplotlib",
+        "pandas",
+    ],
+)
