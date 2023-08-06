3f9ae40b3323e117774fd38efa71324da04a8253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:45 +0000

    Add package bokeh/2.4.0.dev6/bokeh-2.4.0.dev6-py3-none-any.whl

---
+++ packages/bokeh/bokeh-2.4.0.dev6-py3-none-any.whl/bokeh/server/static/js/types/models/math_text/mathjax/mathjax.d.ts
@@ -0,0 +1,6 @@
+export declare function tex2svg(formula: string): HTMLElement;
+declare const _default: {
+    tex2svg: typeof tex2svg;
+};
+export default _default;
+//# sourceMappingURL=mathjax.d.ts.map
