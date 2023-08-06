4ea3f4b4e689e4ffbfb7c579a773ad6d77423012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:14 +0000

    Add package aigpytest/2021.8.26.5/aigpytest-2021.8.26.5.tar.gz

---
+++ packages/aigpytest/aigpytest-2021.8.26.5.tar.gz/aigpytest-2021.8.26.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aigpytest',
+    version="2021.8.26.5",
+    license="Apache2",
+    description="",
+
+    author='YaronH',
+    author_email="yaronhuang@foxmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
+    entry_points={'console_scripts': ['aigpytest = aigpytest:main', ]}
+)
