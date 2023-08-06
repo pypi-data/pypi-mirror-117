6a466446dffdbc662f9d196cba1daa29340db2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:19 +0000

    Add package seckerwiki/2.0.0/seckerwiki-2.0.0.tar.gz

---
+++ packages/seckerwiki/seckerwiki-2.0.0.tar.gz/seckerwiki-2.0.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['seckerwiki', 'seckerwiki.commands', 'seckerwiki.scripts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyInquirer>=1.0.3,<2.0.0',
+ 'PyYAML>=5.4.1,<6.0.0',
+ 'pdf2image>=1.16.0,<2.0.0',
+ 'requests>=2.26.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['wiki = seckerwiki.wiki:main']}
+
+setup_kwargs = {
+    'name': 'seckerwiki',
+    'version': '2.0.0',
+    'description': 'A collection of scripts used to manage my personal Foam workspace',
+    'long_description': None,
+    'author': 'Benjamin Secker',
+    'author_email': 'benjamin.secker@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
