eda7a420d08b2f672bccec619edae0e8606a227c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package rtg/0.5.2/rtg-0.5.2.tar.gz

---
+++ packages/rtg/rtg-0.5.2.tar.gz/rtg-0.5.2/tests/test_criterions.py
@@ -0,0 +1,19 @@
+import pytest
+from rtg.pipeline import Pipeline, Experiment
+import tempfile
+from rtg.exp import load_conf
+import torch
+import shutil
+from . import sanity_check_experiment
+
+
+def test_macro_cross_entropy():
+    tmp_dir = tempfile.mkdtemp()
+    config = load_conf('experiments/loss/tfmnmt.macro_CE.yml')
+    exp = Experiment(tmp_dir, config=config, read_only=False)
+    exp.config['trainer'].update(dict(steps=50, check_point=25))
+    exp.config['prep']['num_samples'] = 0
+    Pipeline(exp).run(run_tests=False)
+    sanity_check_experiment(exp)
+    print(f"Cleaning up {tmp_dir}")
+    shutil.rmtree(tmp_dir, ignore_errors=True)
