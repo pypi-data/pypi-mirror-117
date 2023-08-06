b83cd49247184cde3b4420c38be6bc3eb1b7a56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:53 +0000

    Add package yuesfpug/1.0/yuesfpug-1.0.tar.gz

---
+++ packages/yuesfpug/yuesfpug-1.0.tar.gz/yuesfpug-1.0/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import  setup
+import setuptools
+
+packages = ['yuesfpug']
+
+setup(name='yuesfpug',version='1.0',author='yuesf', packages=packages, package_dir={'requests': 'requests'},)
