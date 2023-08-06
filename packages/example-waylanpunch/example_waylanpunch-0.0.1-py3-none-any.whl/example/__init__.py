9eb0564b59ff300532b10183ff71ef27a295dbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:48 +0000

    Add package example-waylanpunch/0.0.1/example_waylanpunch-0.0.1-py3-none-any.whl

---
+++ packages/example-waylanpunch/example_waylanpunch-0.0.1-py3-none-any.whl/example/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# coding: utf-8
+# @Author : waylanpunch
+# @Time : 2021/8/25 21:57
+# @File : __init__.py.py
+# @Software : PyCharm
---
+++ packages/example-waylanpunch/example_waylanpunch-0.0.1-py3-none-any.whl/example/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# coding: utf-8
+# @Author : waylanpunch
+# @Time : 2021/8/25 21:58
+# @File : example.py.py
+# @Software : PyCharm
+
+def add_one(number):
+    return number + 1
