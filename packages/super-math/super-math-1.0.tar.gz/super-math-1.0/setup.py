9c4db9c1d33b82811dc6824ce4f1dcee94cc1b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:21 +0000

    Add package super-math/1.0/super-math-1.0.tar.gz

---
+++ packages/super-math/super-math-1.0.tar.gz/super-math-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='super-math',
+    version='1.0',
+    description='第一次发布的模块',
+    author='lele',
+    author_email='3061204366@qq.com',
+    py_modules=['super_Math.demo1', 'super_Math.demo2']
+)
---
+++ packages/super-math/super-math-1.0.tar.gz/super-math-1.0/super_Math/demo1.py
@@ -0,0 +1,10 @@
+import math as m
+a = 666
+print(m.sqrt(a))
+
+
+
+
+
+
+
