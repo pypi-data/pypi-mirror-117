1a89d42124a5d0d2d8c301fe65acbea4b33fd062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:36 +0000

    Add package testp0131/0.1.7/testp0131-0.1.7.tar.gz

---
+++ packages/testp0131/testp0131-0.1.7.tar.gz/testp0131-0.1.7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testp0131']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'testp0131',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'qiujingyu',
+    'author_email': 'qiujingyu@momenta.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
