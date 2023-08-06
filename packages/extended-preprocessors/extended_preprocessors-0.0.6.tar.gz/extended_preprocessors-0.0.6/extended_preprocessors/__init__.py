9b2e04a0c6a80496c26e4e7084ddd0980bb931cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:04 +0000

    Add package extended-preprocessors/0.0.6/extended_preprocessors-0.0.6.tar.gz

---
+++ packages/extended-preprocessors/extended_preprocessors-0.0.6.tar.gz/extended_preprocessors-0.0.6/extended_preprocessors/__init__.py
@@ -0,0 +1,8 @@
+from extended_preprocessors.bins_linearizer import BinsLinearizer
+from extended_preprocessors.coef_maker import CoefficientMaker
+from extended_preprocessors.numeric_discretizer import NumericalFeatureDiscretizer
+from extended_preprocessors.categorical_discretizer import CategoricalFeatureDiscretizer
+from extended_preprocessors.feature_selector import FeatureSelector
+from extended_preprocessors.dataset_linearizer import DatasetLinearizer
+from extended_preprocessors.dataset_binarizer import DatasetBinarizer
+
