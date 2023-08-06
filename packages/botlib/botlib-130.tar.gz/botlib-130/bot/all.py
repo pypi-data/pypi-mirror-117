b417f1960d3006cd958bf6d1abb5f3c1824e02af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:12 +0000

    Add package botlib/130/botlib-130.tar.gz

---
+++ packages/botlib/botlib-130.tar.gz/botlib-130/bot/all.py
@@ -0,0 +1,25 @@
+# This file is in the Public Domain.
+
+"all bot modules."
+
+import bot.adm
+import bot.bus
+import bot.evt
+import bot.fnd
+import bot.hdl
+import bot.hlp
+import bot.hlp
+import bot.irc
+import bot.log
+import bot.obj
+import bot.opt
+import bot.prs
+import bot.pwd
+import bot.rss
+import bot.run
+import bot.tbl	
+import bot.thr
+import bot.tms
+import bot.trc
+import bot.udp
+import bot.ver
