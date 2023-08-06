adef144a78ae339c5216f29965dda5bc9e530941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:34 +0000

    Add package aws-xray-lambda-segment-shim/0.5.0/aws_xray_lambda_segment_shim-0.5.0.tar.gz

---
+++ packages/aws-xray-lambda-segment-shim/aws_xray_lambda_segment_shim-0.5.0.tar.gz/aws_xray_lambda_segment_shim-0.5.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Setup aws_xray_lambda_segment_shim package."""
+import re
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = re.sub(r"..\s*doctest\s*::", ".. code-block ::", f.read())
+
+setup(
+    version="0.5.0",
+    python_requires=">=3.6.0",
+    name="aws_xray_lambda_segment_shim",
+    packages=find_packages(include=["aws_xray_lambda_segment_shim"]),
+    description="An unofficial shim which faciliates the creation of "
+    "AWS Xray Segments in Lambda functions triggered by unsupported means.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author="Sam Martin",
+    author_email="samjackmartin+aws-xray-lambda-segment-shim@gmail.com",
+    url="https://github.com/sam-martin/aws-xray-lambda-segment-shim",
+    install_requires=["aws-xray-sdk", "jmespath"],
+    package_data={
+        "": ["py.typed"],
+    },
+)
