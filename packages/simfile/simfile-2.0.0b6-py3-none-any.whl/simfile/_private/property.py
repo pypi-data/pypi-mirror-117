732ec427a59dc6e33fac97184f6d67b89846dcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:40 +0000

    Add package simfile/2.0.0b6/simfile-2.0.0b6-py3-none-any.whl

---
+++ packages/simfile/simfile-2.0.0b6-py3-none-any.whl/simfile/_private/property.py
@@ -0,0 +1,18 @@
+from typing import MutableMapping, Optional
+
+
+def item_property(name):
+
+    @property
+    def item_property(self: MutableMapping[str, str]) -> Optional[str]:
+        return self.get(name)
+
+    @item_property.setter
+    def item_property(self: MutableMapping[str, str], value: str) -> None:
+        self[name] = value
+    
+    @item_property.deleter
+    def item_property(self: MutableMapping[str, str]) -> None:
+        del self[name]
+
+    return item_property
