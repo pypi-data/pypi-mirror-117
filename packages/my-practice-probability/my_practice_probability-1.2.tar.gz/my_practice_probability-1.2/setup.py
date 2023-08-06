bd94c9a7a4b258f11e9d87e7fc0ec6a388c493e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:55 +0000

    Add package my-practice-probability/1.2/my_practice_probability-1.2.tar.gz

---
+++ packages/my-practice-probability/my_practice_probability-1.2.tar.gz/my_practice_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my_practice_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['my_practice_probability'],
+      author = 'Andrew Paster',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
