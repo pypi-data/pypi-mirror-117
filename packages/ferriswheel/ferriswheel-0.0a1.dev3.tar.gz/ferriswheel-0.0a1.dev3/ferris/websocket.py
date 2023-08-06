c2fcb161895fc7291c0ef6675830793777034aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package ferriswheel/0.0a1.dev3/ferriswheel-0.0a1.dev3.tar.gz

---
+++ packages/ferriswheel/ferriswheel-0.0a1.dev3.tar.gz/ferriswheel-0.0a1.dev3/ferris/websocket.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import aiohttp
+from typing import TYPE_CHECKING, Union
+
+from .utils import to_json
+from .errors import WebsocketException, Reconnect
+
+if TYPE_CHECKING:
+    from .http import HTTPClient
+    from .types import Data
+
+
+class Websocket:
+    """The class that interfaces with FerrisChat's websockets."""
+
+    def __init__(self, http: HTTPClient) -> None:
+        self.ws: aiohttp.ClientWebSocketResponse = None
+
+        self._http: HTTPClient = http
+        self._ws_url: str = None
+
+    async def prepare(self) -> None:
+        """Retrieves the URL needed for websocket connection."""
+        response = await self._http.api.ws.info.get()
+        self._ws_url = response['url']
+
+    async def handle(self, data: Data) -> None:
+        """Handles a message received from the websocket."""
+        ...  # not implemented
+
+    async def _parse_and_handle(self, data: Union[str, bytes]) -> None:
+        if isinstance(data, (str, bytes)):
+            data = to_json(data)
+            await self.handle(data)
+
+    async def connect(self) -> None:
+        """Establishes a websocket connection with FerrisChat."""
+        if not self._ws_url:
+            await self.prepare()
+
+        self.ws = await self._http.session.ws_connect(self._ws_url, heartbeat=45)
+
+        async for message in self.ws:
+            if message.type in {aiohttp.WSMsgType.TEXT, aiohttp.WSMsgType.BINARY}:
+                await self._parse_and_handle(message.data)
+            elif message.type is aiohttp.WSMsgType.ERROR:
+                raise WebsocketException(message.data)
+            elif message.type in (
+                aiohttp.WSMsgType.CLOSED,
+                aiohttp.WSMsgType.CLOSING,
+                aiohttp.WSMsgType.CLOSE,
+            ):
+                raise Reconnect  # TODO: Reconnect here
+
+    async def close(self) -> None:
+        """Closes the current websocket connection."""
+        await self.ws.close()
---
+++ packages/ferriswheel/ferriswheel-0.0a1.dev3.tar.gz/ferriswheel-0.0a1.dev3/setup.py
@@ -0,0 +1,65 @@
+import re
+
+from setuptools import setup
+
+with open('ferris/__init__.py', 'r') as f:
+    content = f.read()
+    try:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)  # type: ignore
+    except AttributeError:
+        raise RuntimeError('Unable to find version string')
+
+    try:
+        author = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)  # type: ignore
+    except AttributeError:
+        author = 'Cryptex & jay3332'
+
+# with open('requirements.txt', 'r') as f:
+#     requirements = f.readlines()
+
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="ferriswheel",
+    author=author,
+    url="https://github.com/FerrisChat/ferriswheel",
+    project_urls={
+        "Issue tracker": "https://github.com/FerrisChat/ferriswheel/issues/new",
+    },
+    version=version,
+    packages=[
+        "ferris",
+    ],
+    license="MIT",
+    description="An asynchronous python wrapper for ferris.chat api",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=["aiohttp"],
+    extras_require={
+        "docs": [
+            "sphinx>=4.1.1",
+            "sphinx-material",
+            'sphinx-copybutton',
+            'readthedocs-sphinx-search',
+        ],
+        "performance": ["orjson>=1.3.0", "aiohttp[speedups]"],
+    },
+    python_requires=">=3.8.0",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+)
