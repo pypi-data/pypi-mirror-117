b72234e6356884f476238c46d42e66eec3441aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:38 +0000

    Add package mais-distributions/0.1/mais_distributions-0.1.tar.gz

---
+++ packages/mais-distributions/mais_distributions-0.1.tar.gz/mais_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mais_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mais_distributions'],
+      zip_safe=False)
