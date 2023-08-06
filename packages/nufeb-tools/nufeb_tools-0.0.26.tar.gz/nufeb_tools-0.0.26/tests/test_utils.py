41737d156d7e653b5d024a4644d0018249101fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:57 +0000

    Add package nufeb-tools/0.0.26/nufeb_tools-0.0.26.tar.gz

---
+++ packages/nufeb-tools/nufeb_tools-0.0.26.tar.gz/nufeb_tools-0.0.26/tests/test_utils.py
@@ -0,0 +1,15 @@
+def test_github_import():
+    from nufeb_tools import utils
+    x = utils.get_data(directory = None,test=True)
+
+def test_local_import():
+    from nufeb_tools import utils
+    from pathlib import Path
+    import os
+    if os.path.isdir(str((Path.home()) / '.nufeb_tools' / 'data' / 'Run_26_90_83_1')):
+        x = utils.get_data(directory=str((Path.home()) / '.nufeb_tools' / 'data' / 'Run_26_90_83_1'))
+def test_single_cell_biomass():
+    from nufeb_tools import utils
+    x = utils.get_data(test=True)
+    x.single_cell_growth()
+
