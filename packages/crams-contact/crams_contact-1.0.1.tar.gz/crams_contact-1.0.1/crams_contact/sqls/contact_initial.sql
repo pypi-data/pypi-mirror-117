ffdfa352e1bce38f54e06714de2a83efe914b018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:45 +0000

    Add package crams-contact/1.0.1/crams_contact-1.0.1.tar.gz

---
+++ packages/crams-contact/crams_contact-1.0.1.tar.gz/crams_contact-1.0.1/crams_contact/sqls/contact_initial.sql
@@ -0,0 +1,8 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+INSERT INTO crams_contact_contactrole (`name`, project_leader, read_only, support_notification) VALUES ('E_RESEARCH_BODY Admin', false, false, false);
+INSERT INTO crams_contact_contactrole (`name`, project_leader, read_only, support_notification) VALUES ('E_RESEARCH_BODY_SYSTEM Admin', false, false, false);
+INSERT INTO crams_contact_contactrole (`name`, project_leader, read_only, support_notification) VALUES ('E_RESEARCH_SYSTEM_DELEGATE', false, false, false);
+INSERT INTO crams_contact_contactrole (`name`, project_leader, read_only, support_notification) VALUES ('E_RESEARCH_BODY_SYSTEM_Approver', false, false, false);
+INSERT INTO crams_contact_contactrole (`name`, project_leader, read_only, support_notification) VALUES ('E_RESEARCH_BODY_SYSTEM_Provisioner', false, false, false);
+
