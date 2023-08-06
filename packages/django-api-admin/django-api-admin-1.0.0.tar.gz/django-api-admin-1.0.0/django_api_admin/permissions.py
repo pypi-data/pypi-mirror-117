ee3887248a86c1e02cc1052e08c8029825320a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:56 +0000

    Add package django-api-admin/1.0.0/django-api-admin-1.0.0.tar.gz

---
+++ packages/django-api-admin/django-api-admin-1.0.0.tar.gz/django-api-admin-1.0.0/django_api_admin/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+
+class IsAdminUser(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if view.get_view_name() == 'Login':
+            return True
+        return self.is_superuser(request)
+
+    def is_superuser(self, request):
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff and request.user.is_active)
