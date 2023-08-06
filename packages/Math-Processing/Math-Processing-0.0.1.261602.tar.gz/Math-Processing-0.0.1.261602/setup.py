e50fb86034355e28f602ec799bd1894a9954db95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:50 +0000

    Add package Math-Processing/0.0.1.261602/Math-Processing-0.0.1.261602.tar.gz

---
+++ packages/Math-Processing/Math-Processing-0.0.1.261602.tar.gz/Math-Processing-0.0.1.261602/setup.py
@@ -0,0 +1,12 @@
+from setuptools import *
+
+setup(
+    name = 'Math-Processing',
+    version = '0.0.1.261602',
+    description = 'Basic Digital Processing',
+    packages = find_packages(),
+    author = 'Yile Wang',
+    author_email = '36210@qq.com',
+    license = 'GPL',
+    python_requires = '>=3.0'
+    )
