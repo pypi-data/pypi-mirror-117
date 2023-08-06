a8d63c3d3a0f888b446d98065704d79965af651e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package fem-probability/0.1/fem_probability-0.1.tar.gz

---
+++ packages/fem-probability/fem_probability-0.1.tar.gz/fem_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fem_probability',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['fem_probability'],
+      author = 'Femi Tofade',
+      author_email = 'tolatof@gmail.com',
+      zip_safe=False)
