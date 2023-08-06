8b86d36e96d91bc802b6d5f607c9e426f6bfbe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package zunis/0.2rc4/zunis-0.2rc4.tar.gz

---
+++ packages/zunis/zunis-0.2rc4.tar.gz/zunis-0.2rc4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zunis',
+    packages=find_packages(),
+    install_requires=[
+        "numpy == 1.19.1",
+        "pandas == 1.1.0",
+        "torch == 1.6.0",
+        "abc_property == 1.0",
+        "dictwrapper == 1.3",
+        "ruamel.yaml <= 0.16.12"
+    ],
+    version='0.2rc4',
+    description='Neural Importance Sampling',
+    long_description=open("../README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Nicolas Deutschmann',
+    author_email="nicolas.deutschmann@gmail.com",
+    url="https://zunis.readthedocs.io",
+    download_url="https://github.com/ndetschmann/zunis",
+    license='MIT',
+    include_package_data = True
+)
