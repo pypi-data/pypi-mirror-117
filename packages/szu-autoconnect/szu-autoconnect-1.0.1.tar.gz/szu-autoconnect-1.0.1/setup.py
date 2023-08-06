790525b3791a17032abc3468d643da538d62cd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:55 +0000

    Add package szu-autoconnect/1.0.1/szu-autoconnect-1.0.1.tar.gz

---
+++ packages/szu-autoconnect/szu-autoconnect-1.0.1.tar.gz/szu-autoconnect-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='szu-autoconnect',
+    packages=find_packages(include=['szu-autoconnect'], exclude=['build', 'dist']),
+    package_dir={'szu-autoconnect': 'szu-autoconnect'},
+    version='1.0.1',
+    license='MIT',
+    description='A simple way to get different DEXs abis for block chains.',
+    author='Yong',
+    author_email='ackness8@gmail.com',
+    url='https://github.com/ackness/szu-autoconnect',
+    keywords=['drcom', 'auto', 'SZU'],
+    install_requires=[
+        'PySimpleGUI',
+        'apscheduler',
+        'loguru',
+        'retrying',
+        'requests'
+    ]
+)
