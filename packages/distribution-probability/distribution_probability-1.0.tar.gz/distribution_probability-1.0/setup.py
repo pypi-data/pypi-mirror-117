d56ca792e2a2b34b3dcb2f29f752c63d473cb0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:11 +0000

    Add package distribution-probability/1.0/distribution_probability-1.0.tar.gz

---
+++ packages/distribution-probability/distribution_probability-1.0.tar.gz/distribution_probability-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distribution_probability',
+      version='1.0',
+      description='Gaussian and Binomial Distribution',
+      description_file='Readme.md',
+      packages=['distribution_probability'],
+      author='Iromi Paranavithana',
+      author_email='iromirandika@gmail.com',
+      zip_safe=False)
