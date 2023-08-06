bdeefed7560688ee07e20c0194648b9198f0241e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:36 +0000

    Add package froModuleDrivers/1.0.1/froModuleDrivers-1.0.1-py3-none-any.whl

---
+++ packages/froModuleDrivers/froModuleDrivers-1.0.1-py3-none-any.whl/froModuleDrivers/roboticArmDriver.py
@@ -0,0 +1,52 @@
+# -*- encoding=utf-8 -*-
+
+from .baseDriver import BaseDriver
+from .protocolHandler import *
+from . import LOGGER
+import time
+
+
+class RoboticArmDriver(BaseDriver):
+    def __init__(self, host="localhost", port=4001):
+        super().__init__(host, port)
+        self.status = {}
+
+    # 此处result为按协议定义的字段解析过的dict
+    # 可根据具体设备进行自定义 状态信息维护、状态查询结果返回等操作。
+    # 根据不同的协议返回，包装返回值，使其包含"orderName", 并被查询指令接收。
+    def doHandleResult(self, result):
+        if result is None or result.get("data") is None:
+            return None, False
+        else:
+            LOGGER.logger.debug("doHandleResult:%s" % result)
+            return result, True
+
+    # 查询指令样本，需要等待返回状态值
+    def queryOrder(self, *args):
+        argList = list(args)
+        timeout = argList.pop(0)
+        orderName = argList.pop(0)
+        data = self.parseOrder(orderName, *argList)
+        if data is None:
+            return
+        self.connector.waitResponse(self.getClientId(), orderName)
+        self.connector.sendData(self.getClientId(), data, prior=False)
+        time.sleep(timeout)
+        return self.connector.getResponse(self.getClientId(), orderName)
+
+    # 不等待返回值，比如上位机软件更新内部设备状态信息
+    def excuteOrderRightAway(self, *args):
+        argList = list(args)
+        orderName = argList.pop(0)
+        data = self.parseOrder(orderName, *argList)
+        if data is None:
+            return
+        self.connector.sendData(self.getClientId(), data, prior=False)
+
+    # 以下是机械臂控制
+    def startAction(self, actionId):
+        my_order = [0x55, 0x55, 0x05, 0x06, 0x00, 0x01, 0x00]
+        my_order[4] = actionId
+        self.connector.sendData(self.getClientId(),
+                                bytes(bytearray(my_order)),
+                                prior=False)
