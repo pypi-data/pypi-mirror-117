d8c408d9b49dc980c9ff3cc56c4a4f29fc3dc4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:41 +0000

    Add package py-Ultroid/54.3b0/py_Ultroid-54.3b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-54.3b0-py3-none-any.whl/pyUltroid/functions/username_db.py
@@ -0,0 +1,25 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+from .. import udB
+
+try:
+    eval(udB["USERNAME_DB"])
+except BaseException:
+    udB.set("USERNAME_DB", "{}")
+
+
+def update_username(id, uname):
+    ok = eval(udB["USERNAME_DB"])
+    ok.update({id: uname})
+
+
+def get_username(id):
+    ok = eval(udB["USERNAME_DB"])
+    if ok.get(id):
+        return ok[id]
+    return None
