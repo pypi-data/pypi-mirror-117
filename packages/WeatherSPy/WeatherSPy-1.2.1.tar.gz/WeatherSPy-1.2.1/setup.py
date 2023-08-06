813d50a557ebac23c407b68d0c7f3f12b1aba6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:28 +0000

    Add package WeatherSPy/1.2.1/WeatherSPy-1.2.1.tar.gz

---
+++ packages/WeatherSPy/WeatherSPy-1.2.1.tar.gz/WeatherSPy-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='WeatherSPy',
+    version='1.2.1',
+    author='stripe-python',
+    author_email='13513519246@139.com',
+    py_modules=['WeatherSPy'],
+    description='a weather library',
+)
