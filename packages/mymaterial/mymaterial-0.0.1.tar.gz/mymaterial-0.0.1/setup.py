6e86916e0ce7fd7a7fe2553385622bd36ab9029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:32 +0000

    Add package mymaterial/0.0.1/mymaterial-0.0.1.tar.gz

---
+++ packages/mymaterial/mymaterial-0.0.1.tar.gz/mymaterial-0.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='mymaterial',
+    version='0.0.1',
+    author='zhouzi',
+    author_email='3222676446@qq.com',
+    url='https://github.com/zhijiuxing-alt/mkdocs-material.git',
+    description='用于测试的',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+           
+        ]
+    }
+)
