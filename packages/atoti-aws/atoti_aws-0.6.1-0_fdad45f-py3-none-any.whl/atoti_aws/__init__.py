bc13955daa59c406858cf4a744e9fee10ba7b4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:21 +0000

    Add package atoti-aws/0.6.1/atoti_aws-0.6.1-0_fdad45f-py3-none-any.whl

---
+++ packages/atoti-aws/atoti_aws-0.6.1-0_fdad45f-py3-none-any.whl/atoti_aws/__init__.py
@@ -0,0 +1,12 @@
+"""Plugin to load CSV and Parquet files from AWS S3 into atoti tables.
+
+This package is required to load files with paths starting with ``s3://``.
+
+Authentication is handled by the underlying AWS SDK for Java library.
+Refer to their `documentation <https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html#setup-credentials-setting>`__  for the available options.
+
+See Also:
+    :class:`~atoti.config.aws.AwsConfig` to configure how atoti interacts with AWS.
+"""
+
+from ._version import VERSION as __version__
