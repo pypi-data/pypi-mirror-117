6293a088d31b7368801654ab9bce86c5fa9ba42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:53 +0000

    Add package django-politico-minutes-editor/1.0.6/django-politico-minutes-editor-1.0.6.tar.gz

---
+++ packages/django-politico-minutes-editor/django-politico-minutes-editor-1.0.6.tar.gz/django-politico-minutes-editor-1.0.6/minutes/api/common/serializers/native_ad.py
@@ -0,0 +1,25 @@
+from minutes.models import NativeAd
+from rest_framework import serializers
+
+PIXEL_DELIMETER = '>|<'
+
+
+class NativeAdSerializer(serializers.ModelSerializer):
+    pixels = serializers.SerializerMethodField()
+
+    def get_pixels(self, obj):
+        pixel_list = obj.pixels.split(PIXEL_DELIMETER)
+        return pixel_list
+
+    class Meta:
+        model = NativeAd
+        fields = (
+            "id",
+            "handle",
+            "sponsor",
+            "lede",
+            "body",
+            "campaign_title",
+            "campaign_link",
+            "pixels"
+        )
