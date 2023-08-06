73be9e481ea53061fd5adbbf174ea3f349e82a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:15 +0000

    Add package tweetkit/0.0.3/tweetkit-0.0.3.tar.gz

---
+++ packages/tweetkit/tweetkit-0.0.3.tar.gz/tweetkit-0.0.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='tweetkit',
+    version='0.0.3',
+    packages=setuptools.find_packages(),
+    description='TweetKit library',
+    install_requires=[],
+)
