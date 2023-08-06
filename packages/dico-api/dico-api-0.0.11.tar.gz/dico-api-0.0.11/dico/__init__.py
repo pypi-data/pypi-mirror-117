c2b38abcb04395d17945c453c67b37c898b9dd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:02 +0000

    Add package dico-api/0.0.11/dico-api-0.0.11.tar.gz

---
+++ packages/dico-api/dico-api-0.0.11.tar.gz/dico-api-0.0.11/dico/__init__.py
@@ -0,0 +1,15 @@
+"""
+dico
+~~~~~~~~~~~~~~~~~~~~~~~~
+Yet another Discord API wrapper for Python.
+:copyright: (c) 2021 eunwoo1104
+:license: MIT
+"""
+
+__version__ = "0.0.11"
+
+from .client import Client
+from .model.channel import *
+from .model.event import *
+from .model.gateway import Intents, Activity, ActivityTypes
+from .model.interactions import *
