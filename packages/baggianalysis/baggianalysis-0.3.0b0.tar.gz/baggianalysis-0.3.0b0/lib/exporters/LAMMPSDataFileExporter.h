f7e2ddb727f16d347d72722922b31bda48e81017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:30 +0000

    Add package baggianalysis/0.3.0b0/baggianalysis-0.3.0b0.tar.gz

---
+++ packages/baggianalysis/baggianalysis-0.3.0b0.tar.gz/baggianalysis-0.3.0b0/lib/exporters/LAMMPSDataFileExporter.h
@@ -0,0 +1,40 @@
+/*
+ * LAMMPSDataFileExporter.h
+ *
+ *  Created on: Mar 1, 2020
+ *      Author: lorenzo
+ */
+
+#ifndef EXPORTERS_LAMMPSDATAFILEEXPORTER_H_
+#define EXPORTERS_LAMMPSDATAFILEEXPORTER_H_
+
+#include "BaseExporter.h"
+
+namespace ba {
+
+class Particle;
+
+class LAMMPSDataFileExporter: public BaseExporter {
+public:
+	LAMMPSDataFileExporter() = delete;
+	LAMMPSDataFileExporter(std::string atom_style);
+	virtual ~LAMMPSDataFileExporter();
+
+private:
+	void _write_system_to_stream(std::shared_ptr<System> system, std::ostream &output) override;
+
+	std::string _particle_line(std::shared_ptr<Particle> p);
+
+	std::string _atom_style;
+	std::vector<std::string> _supported_atom_styles = {"atomic", "bond", "full", "molecular"};
+};
+
+#ifdef PYTHON_BINDINGS
+
+void export_LAMMPSDataFileExporter(py::module &m);
+
+#endif
+
+} /* namespace ba */
+
+#endif /* EXPORTERS_LAMMPSDATAFILEEXPORTER_H_ */
