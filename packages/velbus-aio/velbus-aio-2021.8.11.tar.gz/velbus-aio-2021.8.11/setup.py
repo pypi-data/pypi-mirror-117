5ad5c50001ed9ee01bca954134bf9906b2c8e970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:21 +0000

    Add package velbus-aio/2021.8.11/velbus-aio-2021.8.11.tar.gz

---
+++ packages/velbus-aio/velbus-aio-2021.8.11.tar.gz/velbus-aio-2021.8.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="velbus-aio",
+    version="2021.8.11",
+    url="https://github.com/Cereal2nd/velbus-aio",
+    license="MIT",
+    author="Maikel Punie",
+    install_requires=["pyserial-asyncio"],
+    author_email="maikel.punie@gmail.com",
+    packages=["velbusaio", "velbusaio.messages"],
+    include_package_data=True,
+    platforms="any",
+)
