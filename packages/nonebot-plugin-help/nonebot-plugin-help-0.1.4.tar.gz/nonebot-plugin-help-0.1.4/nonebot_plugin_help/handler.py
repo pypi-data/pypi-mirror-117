1cd8725352008cd901184eb9511d9fecd69faa2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:31 +0000

    Add package nonebot-plugin-help/0.1.4/nonebot-plugin-help-0.1.4.tar.gz

---
+++ packages/nonebot-plugin-help/nonebot-plugin-help-0.1.4.tar.gz/nonebot-plugin-help-0.1.4/nonebot_plugin_help/handler.py
@@ -0,0 +1,67 @@
+import nonebot.plugin
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.cqhttp.message import Message, MessageSegment
+from nonebot.log import logger
+
+
+helper = on_command("help", priority=1, aliases={"帮助"})
+default_start = list(nonebot.get_driver().config.command_start)[0]
+
+
+@helper.handle()
+async def handle_first_receive(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).strip()
+    logger.debug(args)
+    if args:
+        state["content"] = args
+    else:
+        state["content"] = ""
+
+
+@helper.got("content")
+async def get_result(bot: Bot, event: Event, state: T_State):
+    at = MessageSegment.at(event.get_user_id())
+    if not state.get("content"):
+        result = await get_help()
+    elif str(state.get("content")).lower() == "list":
+        plugin_set = nonebot.plugin.get_loaded_plugins()
+        plugin_names = []
+        for plugin in plugin_set:
+            try:
+                name = f'{plugin.name} | ' \
+                    f'{plugin.module.__getattribute__("__help_plugin_name__")}'
+            except:
+                name = f'{plugin.name}'
+            try:
+                version = plugin.module.__getattribute__("__help_version__")
+            except:
+                version = ""
+            plugin_names.append(f'{name} {version}')
+        plugin_names.sort()
+        newline_char = '\n'
+        result = f'已加载插件：\n{newline_char.join(plugin_names)}'
+    else:
+        try:
+            plugin = nonebot.plugin.get_plugin(state.get("content"))
+        except AttributeError:
+            plugin = None
+        try:
+            result = plugin.module.__getattribute__("__usage__")
+        except:
+            try:
+                result = plugin.module.__doc__
+            except AttributeError:
+                result = f'{state.get("content")}插件不存在或未加载'
+    await helper.finish(Message().append(at).append(
+        MessageSegment.text(result)))
+
+
+async def get_help():
+    return f'''欢迎使用Nonebot 2 Help Plugin
+支持使用的前缀：{" ".join(list(nonebot.get_driver().config.command_start))}
+{default_start}help  # 获取本插件帮助
+{default_start}help list  # 展示已加载插件列表
+{default_start}help <plugin_name>  # 调取目标插件帮助信息
+'''
---
+++ packages/nonebot-plugin-help/nonebot-plugin-help-0.1.4.tar.gz/nonebot-plugin-help-0.1.4/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_help']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nonebot-adapter-cqhttp>=2.0.0a11.post2,<3.0.0',
+ 'nonebot2>=2.0.0-alpha.10,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-help',
+    'version': '0.1.4',
+    'description': 'A general help lister for nonebot2 plugins',
+    'long_description': '# nonebot-plugin-help\nA general help plugin for nonebot2\n\n为nonebot2插件提供泛用的帮助列表\n\n## 开发者接入此插件列表方法\n\n使用python包形态的插件（已发布/自行开发皆可），并在插件包的__init__.py文件内增加如下代码：\n```python\n# 若此文本不存在，将显示包的__doc__\n__usage__ = \'您想在使用命令/help <your plugin package name>时提供的帮助文本\'\n\n# 您的插件版本号，将在/help list中显示\n__help__version__ = \'0.1.4\' \n\n# 此名称有助于美化您的插件在/help list中的显示\n# 但使用/help xxx查询插件用途时仍必须使用包名\n__help__plugin_name__ = "您的插件名称（有别于nonebot-plugin-xxx的包名）" \n```\n\n## 实际使用\n### 查看已加载插件列表\n指令：/help list\n\n返回示例：\n```\n@<user_who_send_command> 已加载插件：\ndice: XZhouQD\'s Roll 0.1.0\nnonebot_plugin_help: XZhouQD\'s Help Menu 0.1.0\nnonebot_plugin_apscheduler \nnonebot_plugin_status \n```\n\n### 查看已加载某一插件用途\n指令：/help <plugin_package_name>\n示例：\n```\n/help help\n\n@<user_who_send_command> 欢迎使用Nonebot 2 Help Plugin，请输入/help 获取使用方法\n```\n\n若插件未提供__usage__，则会显示__doc__，示例：\n```\n/help nonebot_plugin_status\n\n@<user_who_send_command>\n@Author         : yanyongyu\n@Date           : 2020-09-18 00:00:13\n@LastEditors    : yanyongyu\n@LastEditTime   : 2021-03-16 17:05:58\n@Description    : None\n@GitHub         : https://github.com/yanyongyu\n```',
+    'author': 'XZhouQD',
+    'author_email': 'X.Zhou.QD@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/XZhouQD/nonebot-plugin-help',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
