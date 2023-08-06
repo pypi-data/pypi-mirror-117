b7f7ac1eab2882360e6d2509d2835267b36b60ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package kubric/0.1/kubric-0.1-py3-none-any.whl

---
+++ packages/kubric/kubric-0.1-py3-none-any.whl/kubric/post_processing.py
@@ -0,0 +1,35 @@
+# Copyright 2021 The Kubric Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+
+def compute_bboxes(segmentation):
+  instances = []
+  for k in range(1, np.max(segmentation)+1):
+    obj = {
+        "bboxes": [],
+        "bbox_frames": [],
+    }
+    for t in range(segmentation.shape[0]):
+      seg = segmentation[t, ..., 0]
+      idxs = np.array(np.where(seg == k), dtype=np.float32)
+      if idxs.size > 0:
+        idxs /= np.array(seg.shape)[:, np.newaxis]
+        obj["bboxes"].append((float(idxs[0].min()), float(idxs[1].min()),
+                              float(idxs[0].max()), float(idxs[1].max())))
+        obj["bbox_frames"].append(t)
+
+    instances.append(obj)
+  return instances
