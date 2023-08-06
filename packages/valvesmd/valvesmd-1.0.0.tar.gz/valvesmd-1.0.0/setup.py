032141b700a40d194088a4471fc5c55983e11173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:07 +0000

    Add package valvesmd/1.0.0/valvesmd-1.0.0.tar.gz

---
+++ packages/valvesmd/valvesmd-1.0.0.tar.gz/valvesmd-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name='valvesmd',
+    packages=['valvesmd'],
+    version='1.0.0',
+    license='gpl-3.0',
+    description='A library to parse .smd files used in the source engine.',
+    author='Maxime Dupuis',
+    author_email='mdupuis@hotmail.ca',
+    url='https://github.com/pySourceSDK/ValveSMD',
+    download_url='https://github.com/pySourceSDK/ValveSMD/archive/v1.0.0.tar.gz',
+    keywords=['smd', 'source', 'sourcesdk', 'hammer', 'valve'],
+    install_requires=['pyparsing', 'future'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
