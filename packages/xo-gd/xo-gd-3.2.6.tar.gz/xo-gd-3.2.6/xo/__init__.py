e9ddfb0f11cf4b1f8f24492f0a005a9f3826a1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:23 +0000

    Add package xo-gd/3.2.6/xo-gd-3.2.6.tar.gz

---
+++ packages/xo-gd/xo-gd-3.2.6.tar.gz/xo-gd-3.2.6/xo/__init__.py
@@ -0,0 +1,17 @@
+#__init__.py
+#XObject + GlobalData v3.1.3 Starlight (08.2021)
+
+# from xo import *
+# print()
+# print(" ::: Please use ")
+# print(" :::    from xo import *")
+# print(" ::: or        ")
+# print(" :::    import xo; xo = ok()")
+# print()
+
+from .gd import *
+# xo = xo.ok()
+
+
+# meet xo. your new friend
+# :) enjoy
