4c7380817e0a065eb7bef6bdee72fb7765c03e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:16 +0000

    Add package pyHanko/0.8.0/pyHanko-0.8.0.tar.gz

---
+++ packages/pyHanko/pyHanko-0.8.0.tar.gz/pyHanko-0.8.0/pyhanko/pdf_utils/font/__init__.py
@@ -0,0 +1,7 @@
+from .api import ShapeResult, FontEngine, FontEngineFactory
+from .basic import SimpleFontEngine, SimpleFontEngineFactory
+
+__all__ = [
+    'ShapeResult', 'FontEngine', 'SimpleFontEngine', 'SimpleFontEngineFactory',
+    'FontEngineFactory'
+]
