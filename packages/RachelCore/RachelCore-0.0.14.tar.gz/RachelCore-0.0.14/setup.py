3b7908398c8eea194c59662244dbbd0b64f0d621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:41 +0000

    Add package RachelCore/0.0.14/RachelCore-0.0.14.tar.gz

---
+++ packages/RachelCore/RachelCore-0.0.14.tar.gz/RachelCore-0.0.14/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+# def parse_requirements(filename):
+#     """ load requirements from a pip requirements file """
+#     lineiter = (line.strip() for line in open(filename))
+#     return [line for line in lineiter if line and not line.startswith("#")]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='RachelCore',
+    packages=find_packages(),
+    version='0.0.14',
+    author='Amirhossein Mohammadi',
+    license='MIT',
+    author_email="amirhosseinmohammadi1380@yahoo.com",
+    description="Rachel assistant core.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackIQ/RachelCore",
+    install_requires=[
+        "requests",
+        "googlesearch-python"
+    ]
+)
