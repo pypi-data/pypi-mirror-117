e86f3d996fb87aae18adbf90f3561cec886585cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:41 +0000

    Add package geoapidata/0.0.14/geoapidata-0.0.14.tar.gz

---
+++ packages/geoapidata/geoapidata-0.0.14.tar.gz/geoapidata-0.0.14/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    'geopandas',
+    'unidecode',
+    'sparse_dot_topn',
+    'dash',
+]
+
+setup(
+    name='geoapidata',
+    packages=find_packages(exclude=["*test*"]),
+    version='0.0.14',
+    description='GeoAPI do TerraLab',
+    author='TerraLab',
+    install_requires= INSTALL_REQUIRES,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    package_data = {"GeoAPI": ['Data/*.csv', 'Data/*.xls'] },
+)
