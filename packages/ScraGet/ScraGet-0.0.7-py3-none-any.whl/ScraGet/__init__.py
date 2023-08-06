1138bf435873ef2a61aa57961102aff8e0725c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:45 +0000

    Add package ScraGet/0.0.7/ScraGet-0.0.7-py3-none-any.whl

---
+++ packages/ScraGet/ScraGet-0.0.7-py3-none-any.whl/ScraGet/__init__.py
@@ -0,0 +1,11 @@
+from ScraGet.forum import get_post, get_topic
+from ScraGet.project import get_project
+from ScraGet.studios import get_studio
+from ScraGet.user import get_user, get_user_extra
+from ScraGet.frontpage import get_frontpage
+
+#from ScraGet import frontpage
+
+#import ScraGet
+
+print("ScraGet: https://github.com/Quantum-Codes/ScraGet")
