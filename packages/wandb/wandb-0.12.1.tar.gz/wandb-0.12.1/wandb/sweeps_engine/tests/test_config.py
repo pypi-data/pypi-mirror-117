c74f458e19e265601f20be991eb453b5f05c373c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:53 +0000

    Add package wandb/0.12.1/wandb-0.12.1.tar.gz

---
+++ packages/wandb/wandb-0.12.1.tar.gz/wandb-0.12.1/wandb/sweeps_engine/tests/test_config.py
@@ -0,0 +1,49 @@
+import pytest
+import jsonschema
+from .. import config
+
+
+def test_invalid_sweep_config_nonuniform_array_elements_categorical():
+
+    valid_config = {
+        "method": "grid",
+        "parameters": {
+            "v1": {"values": [None, 2, 3, "a", (2, 3)]},
+        },
+    }
+
+    # doesn't raise
+    _ = config.SweepConfig(valid_config)
+
+
+def test_min_max_validation():
+    invalid_config = {
+        "method": "random",
+        "parameters": {
+            "v1": {"max": 3, "min": 5},
+            "v2": {"min": 5, "max": 6},
+        },
+    }
+
+    with pytest.raises(jsonschema.ValidationError):
+        _ = config.SweepConfig(invalid_config)
+
+
+def test_negative_sigma_validation():
+    invalid_config = {
+        "method": "random",
+        "parameters": {
+            "v1": {"mu": 0.1, "sigma": -0.1, "distribution": "normal"},
+        },
+    }
+    with pytest.raises(jsonschema.ValidationError):
+        _ = config.SweepConfig(invalid_config)
+
+
+def test_missing_parameters_section():
+    invalid_config = {
+        "method": "random",
+    }
+
+    warnings = config.schema_violations_from_proposed_config(invalid_config)
+    assert len(warnings) == 1
