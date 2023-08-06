da829018fba6b8bb863cf21ed468f1468e0efd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:18 +0000

    Add package embed-templator/1.0.4/embed_templator-1.0.4-py3-none-any.whl

---
+++ packages/embed-templator/embed_templator-1.0.4-py3-none-any.whl/embed_templator/__init__.py
@@ -0,0 +1,7 @@
+"""A Python Template manager for your discord bot to keep your embeds simple
+& consistent. """
+
+from .embed_templator import Embed
+
+__version__: str = '1.0.4'
+__all__ = (__version__, Embed)
