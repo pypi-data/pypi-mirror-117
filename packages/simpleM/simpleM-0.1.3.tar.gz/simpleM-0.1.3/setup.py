0c4954f3343b1f8b5a463af11890a07e1e35c767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package simpleM/0.1.3/simpleM-0.1.3.tar.gz

---
+++ packages/simpleM/simpleM-0.1.3.tar.gz/simpleM-0.1.3/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        print('post install dev hello')
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        print('post install hello')
+
+VERSION = '0.1.3' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="simpleM", 
+        version=VERSION,
+        author="qiujingyu",
+        author_email="<youremail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        data_files=[('.simpleM', ['doc/a/b.txt'])],
+        package_data={'': ['doc/a/b.txt']},
+
+        cmdclass={
+          'develop': PostDevelopCommand,
+          'install': PostInstallCommand,
+        },
+        entry_points = {
+            'console_scripts': ['simpleM=simpleM.display:f'],
+        }
+
+)
