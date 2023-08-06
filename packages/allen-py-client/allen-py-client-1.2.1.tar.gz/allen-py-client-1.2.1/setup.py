d6586876fcf1c7f7da394bd69faecfe47f32c426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:51 +0000

    Add package allen-py-client/1.2.1/allen-py-client-1.2.1.tar.gz

---
+++ packages/allen-py-client/allen-py-client-1.2.1.tar.gz/allen-py-client-1.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='allen-py-client',
+    version='1.2.1',
+    description='Unofficial python wrapper for Allen\'s web API.',
+    long_description=open('README.rst', encoding="utf8").read(),
+    author='Harsh Patil',
+    author_email='ifung230@gmail.com',
+    url='https://github.com/lamergameryt/allen-py-client',
+    download_url='https://github.com/lamergameryt/allen-py-client',
+    keywords=['python', 'allen'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=['requests', 'termcolor', 'stdiomask'],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'allen=allen.command_line:main'
+        ]
+    }
+)
