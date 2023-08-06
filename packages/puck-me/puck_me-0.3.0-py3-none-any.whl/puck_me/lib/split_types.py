adfcf9a610c7c793593e88831ecbaa7318992e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:30 +0000

    Add package puck-me/0.3.0/puck_me-0.3.0-py3-none-any.whl

---
+++ packages/puck-me/puck_me-0.3.0-py3-none-any.whl/puck_me/lib/split_types.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class SplitType(Enum):
+    SEASON = "Season"
+    ARENA = "Place"
+    ALL_STAR = "All_Star"
+    RESULT = "Result"
+    MONTH = "Month"
+    CONFERENCE = "Conference"
+    DIVISION = "Division"
+    OPPONENT = "Opponent"
